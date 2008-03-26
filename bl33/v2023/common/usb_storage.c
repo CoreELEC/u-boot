CoreELEC/u-boot

commit bdacb1625550aff1412557965983b3a015a9e7b4

USB Storage, add meaningful return value
This patch changes the "usb storage" command to return success if it
finds a USB storage device, otherwise it returns error.

Signed-off-by: Markus Klotzbuecher <mk@denx.de>

bl33/v2023/common/usb_storage.c
@@ -188,17 +188,20 @@ void usb_show_progress(void)
  * show info on storage devices; 'usb start/init' must be invoked earlier
  * as we only retrieve structures populated during devices initialization
  */
-void usb_stor_info(void)
+int usb_stor_info(void)
 {
 	int i;
 
-	if (usb_max_devs > 0)
+	if (usb_max_devs > 0) {
 		for (i = 0; i < usb_max_devs; i++) {
 			printf ("  Device %d: ", i);
 			dev_print(&usb_dev_desc[i]);
+			return 0;
 		}
-	else
+	} else {
 		printf("No storage devices, perhaps not 'usb start'ed..?\n");
+		return 1;
+	}
 }
 
 /*********************************************************************************

bl33/v2023/common/cmd_usb.c
@@ -529,8 +529,7 @@ int do_usb (cmd_tbl_t *cmdtp, int flag, int argc, char *argv[])
 	}
 
 	if (strncmp(argv[1], "stor", 4) == 0) {
-		usb_stor_info();
-		return 0;
+		return usb_stor_info();
 	}
 
 	if (strncmp(argv[1],"part",4) == 0) {

commit e45052a80e980625b4a323eccc8c695f357b9201

USB, Storage: fix a bug introduced in commit bdacb1625550aff1412557965983b3a015a9e7b4 that will cause usb_stor_info to only print only information on one storage device, but not for multiple.
Signed-off-by: Markus Klotzbuecher <mk@denx.de>

bl33/v2023/common/usb_storage.c
@@ -196,12 +196,12 @@ int usb_stor_info(void)
 		for (i = 0; i < usb_max_devs; i++) {
 			printf ("  Device %d: ", i);
 			dev_print(&usb_dev_desc[i]);
-			return 0;
 		}
-	} else {
-		printf("No storage devices, perhaps not 'usb start'ed..?\n");
-		return 1;
+		return 0;
 	}
+	
+	printf("No storage devices, perhaps not 'usb start'ed..?\n");
+	return 1;
 }
 
 /*********************************************************************************
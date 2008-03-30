CoreELEC/u-boot

commit 6ffa7ee0583a1a8ff95ee530579b6b057b4b085d

Add CONFIG_MII_INIT in cmd_mii.c
Provide common configuration in do_mii() to execute mii_init()
for all cpu architectures

Signed-off-by: TsiChung Liew <Tsi-Chung.Liew@freescale.com>
Signed-off-by: Ben Warren <biggerbadderben@gmail.com>

bl33/v2023/common/cmd_mii.c
@@ -306,7 +306,7 @@ int do_mii (cmd_tbl_t * cmdtp, int flag, int argc, char *argv[])
 		return 1;
 	}
 
-#if defined(CONFIG_8xx) || defined(CONFIG_MCF532x)
+#if defined(CONFIG_8xx) || defined(CONFIG_MCF532x || defined(CONFIG_MII_INIT))
 	mii_init ();
 #endif
 

commit 3413314bd7a085ae730feeb6654aabcc3dc50171

Fix macro typo in common/cmd_mii.c
This typo was introduced in commit 6ffa7ee0583a1a8ff95ee530579b6b057b4b085d.  I
actually applied the wrong patch.

Signed-off-by: Ben Warren <biggerbadderben@gmail.com>

bl33/v2023/common/cmd_mii.c
@@ -306,7 +306,7 @@ int do_mii (cmd_tbl_t * cmdtp, int flag, int argc, char *argv[])
 		return 1;
 	}
 
-#if defined(CONFIG_8xx) || defined(CONFIG_MCF532x || defined(CONFIG_MII_INIT))
+#if defined(CONFIG_8xx) || defined(CONFIG_MCF532x) || defined(CONFIG_MII_INIT)
 	mii_init ();
 #endif
 
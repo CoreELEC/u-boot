CoreELEC/u-boot

commit ceced00cf336e4198c1c9821a1fd110529129f5b

environment: cleanup prototype declarations of env functions.
Signed-off-by: Wolfgang Denk <wd@denx.de>

bl33/v2023/include/environment.h
@@ -107,4 +107,14 @@ typedef	struct environment_s {
 	unsigned char	data[ENV_SIZE]; /* Environment data		*/
 } env_t;
 
+/* Function that returns a character from the environment */
+unsigned char (*env_get_char)(int);
+
+/* Function that returns a pointer to a value from the environment */
+unsigned char *env_get_addr(int);
+unsigned char env_get_char_memory (int index);
+
+/* Function that updates CRC of the enironment */
+void env_crc_update (void);
+
 #endif	/* _ENVIRONMENT_H_ */

bl33/v2023/api/api.c
@@ -30,6 +30,7 @@
 #include <command.h>
 #include <common.h>
 #include <malloc.h>
+#include <environment.h>
 #include <linux/types.h>
 #include <api_public.h>
 
@@ -40,8 +41,6 @@
 
 /* U-Boot routines needed */
 extern int do_reset (cmd_tbl_t *cmdtp, int flag, int argc, char *argv[]);
-extern uchar (*env_get_char)(int);
-extern uchar *env_get_addr(int);
 
 /*****************************************************************************
  *

bl33/v2023/common/cmd_nvedit.c
@@ -68,19 +68,6 @@ DECLARE_GLOBAL_DATA_PTR;
 /************************************************************************
 ************************************************************************/
 
-/* Function that returns a character from the environment */
-extern uchar (*env_get_char)(int);
-
-/* Function that returns a pointer to a value from the environment */
-/* (Only memory version supported / needed). */
-extern uchar *env_get_addr(int);
-
-/* Function that updates CRC of the enironment */
-extern void env_crc_update (void);
-
-/************************************************************************
-************************************************************************/
-
 /*
  * Table with supported baudrates (defined in config_xyz.h)
  */

bl33/v2023/common/env_eeprom.c
@@ -38,10 +38,6 @@ env_t *env_ptr = NULL;
 
 char * env_name_spec = "EEPROM";
 
-extern uchar (*env_get_char)(int);
-extern uchar env_get_char_memory (int index);
-
-
 uchar env_get_char_spec (int index)
 {
 	uchar c;

bl33/v2023/common/env_nvram.c
@@ -63,9 +63,6 @@ char * env_name_spec = "NVRAM";
 extern uchar default_environment[];
 extern int default_environment_size;
 
-extern uchar (*env_get_char)(int);
-extern uchar env_get_char_memory (int index);
-
 #ifdef CONFIG_AMIGAONEG3SE
 uchar env_get_char_spec (int index)
 {

bl33/v2023/common/ft_build.c
@@ -396,9 +396,6 @@ void *ft_get_prop(void *bphp, const char *propname, int *szp)
 
 /********************************************************************/
 
-/* Function that returns a character from the environment */
-extern uchar(*env_get_char) (int);
-
 void ft_setup(void *blob, bd_t * bd, ulong initrd_start, ulong initrd_end)
 {
 	u32 *p;

commit 52edc5c43053936aec27b85f8a135351ab4f2d06

environment: fix bug introduced by commit ceced00cf336
env_get_char is not a function, but a pointer to one.

Signed-off-by: Wolfgang Denk <wd@denx.de>

bl33/v2023/include/environment.h
@@ -107,8 +107,8 @@ typedef	struct environment_s {
 	unsigned char	data[ENV_SIZE]; /* Environment data		*/
 } env_t;
 
-/* Function that returns a character from the environment */
-unsigned char (*env_get_char)(int);
+/* Pointer to function that returns a character from the environment */
+extern unsigned char (*env_get_char)(int);
 
 /* Function that returns a pointer to a value from the environment */
 unsigned char *env_get_addr(int);
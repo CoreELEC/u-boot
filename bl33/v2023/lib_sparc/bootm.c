CoreELEC/u-boot

commit b3d584aeb3ff0b8b4105091e6003dd3a294e3169

bootm: refactor entry point code
Move entry point code out of each arch and into common code.
Keep the entry point in the bootm_headers_t images struct.

Signed-off-by: Kumar Gala <galak@kernel.crashing.org>

bl33/v2023/lib_sparc/bootm.c
@@ -87,7 +87,7 @@ void do_bootm_linux(cmd_tbl_t * cmdtp, int flag, int argc, char *argv[],
 		    bootm_headers_t * images)
 {
 	char *bootargs;
-	ulong ep, load;
+	ulong load;
 	ulong initrd_start, initrd_end;
 	ulong rd_data_start, rd_data_end, rd_len;
 	unsigned int data, len, checksum;
@@ -97,17 +97,9 @@ void do_bootm_linux(cmd_tbl_t * cmdtp, int flag, int argc, char *argv[],
 	int ret;
 
 	if (images->legacy_hdr_valid) {
-		ep = image_get_ep(images->legacy_hdr_os);
 		load = image_get_load(images->legacy_hdr_os);
 #if defined(CONFIG_FIT)
 	} else if (images->fit_uname_os) {
-		int ret = fit_image_get_entry(images->fit_hdr_os,
-					      images->fit_noffset_os, &ep);
-		if (ret) {
-			puts("Can't get entry point property!\n");
-			goto error;
-		}
-
 		ret = fit_image_get_load(images->fit_hdr_os,
 					 images->fit_noffset_os, &load);
 		if (ret) {
@@ -124,7 +116,7 @@ void do_bootm_linux(cmd_tbl_t * cmdtp, int flag, int argc, char *argv[],
 	linux_hdr = (void *)load;
 
 	/* */
-	kernel = (void (*)(struct linux_romvec *, void *))ep;
+	kernel = (void (*)(struct linux_romvec *, void *))images->ep;
 
 	/* check for a SPARC kernel */
 	if ((linux_hdr->hdr[0] != 'H') ||

bl33/v2023/lib_arm/bootm.c
@@ -62,7 +62,6 @@ void do_bootm_linux (cmd_tbl_t *cmdtp, int flag, int argc, char *argv[],
 		     bootm_headers_t *images)
 {
 	ulong	initrd_start, initrd_end;
-	ulong	ep = 0;
 	bd_t	*bd = gd->bd;
 	char	*s;
 	int	machid = bd->bi_arch_number;
@@ -73,23 +72,7 @@ void do_bootm_linux (cmd_tbl_t *cmdtp, int flag, int argc, char *argv[],
 	char *commandline = getenv ("bootargs");
 #endif
 
-	/* find kernel entry point */
-	if (images->legacy_hdr_valid) {
-		ep = image_get_ep (&images->legacy_hdr_os_copy);
-#if defined(CONFIG_FIT)
-	} else if (images->fit_uname_os) {
-		ret = fit_image_get_entry (images->fit_hdr_os,
-					images->fit_noffset_os, &ep);
-		if (ret) {
-			puts ("Can't get entry point property!\n");
-			goto error;
-		}
-#endif
-	} else {
-		puts ("Could not find kernel entry point!\n");
-		goto error;
-	}
-	theKernel = (void (*)(int, int, uint))ep;
+	theKernel = (void (*)(int, int, uint))images->ep;
 
 	s = getenv ("machid");
 	if (s) {

bl33/v2023/lib_avr32/bootm.c
@@ -177,29 +177,12 @@ void do_bootm_linux(cmd_tbl_t *cmdtp, int flag, int argc, char *argv[],
 		    bootm_headers_t *images)
 {
 	ulong	initrd_start, initrd_end;
-	ulong	ep = 0;
 	void	(*theKernel)(int magic, void *tagtable);
 	struct	tag *params, *params_start;
 	char	*commandline = getenv("bootargs");
 	int	ret;
 
-	/* find kernel entry point */
-	if (images->legacy_hdr_valid) {
-		ep = image_get_ep (&images->legacy_hdr_os_copy);
-#if defined(CONFIG_FIT)
-	} else if (images->fit_uname_os) {
-		ret = fit_image_get_entry (images->fit_hdr_os,
-				images->fit_noffset_os, &ep);
-		if (ret) {
-			puts ("Can't get entry point property!\n");
-			goto error;
-		}
-#endif
-	} else {
-		puts ("Could not find kernel entry point!\n");
-		goto error;
-	}
-	theKernel = (void *)ep;
+	theKernel = (void *)images->ep;
 
 	ret = boot_get_ramdisk (argc, argv, images, IH_ARCH_AVR32,
 			&initrd_start, &initrd_end);

bl33/v2023/lib_blackfin/bootm.c
@@ -38,29 +38,12 @@ void do_bootm_linux(cmd_tbl_t *cmdtp, int flag, int argc, char *argv[],
 {
 	int	(*appl) (char *cmdline);
 	char	*cmdline;
-	ulong	ep = 0;
 
 #ifdef SHARED_RESOURCES
 	swap_to(FLASH);
 #endif
 
-	/* find kernel entry point */
-	if (images->legacy_hdr_valid) {
-		ep = image_get_ep (&images->legacy_hdr_os_copy);
-#if defined(CONFIG_FIT)
-	} else if (images->fit_uname_os) {
-		int ret = fit_image_get_entry (images->fit_hdr_os,
-				images->fit_noffset_os, &ep);
-		if (ret) {
-			puts ("Can't get entry point property!\n");
-			goto error;
-		}
-#endif
-	} else {
-		puts ("Could not find kernel entry point!\n");
-		goto error;
-	}
-	appl = (int (*)(char *))ep;
+	appl = (int (*)(char *))images->ep;
 
 	printf("Starting Kernel at = %x\n", appl);
 	cmdline = make_command_line();

bl33/v2023/lib_m68k/bootm.c
@@ -57,7 +57,6 @@ void do_bootm_linux(cmd_tbl_t * cmdtp, int flag,
 	ulong cmd_start, cmd_end;
 	ulong bootmap_base;
 	bd_t  *kbd;
-	ulong ep = 0;
 	void  (*kernel) (bd_t *, ulong, ulong, ulong, ulong);
 	struct lmb *lmb = images->lmb;
 
@@ -94,23 +93,7 @@ void do_bootm_linux(cmd_tbl_t * cmdtp, int flag,
 	}
 	set_clocks_in_mhz(kbd);
 
-	/* find kernel entry point */
-	if (images->legacy_hdr_valid) {
-		ep = image_get_ep (&images->legacy_hdr_os_copy);
-#if defined(CONFIG_FIT)
-	} else if (images->fit_uname_os) {
-		ret = fit_image_get_entry (images->fit_hdr_os,
-				images->fit_noffset_os, &ep);
-		if (ret) {
-			puts ("Can't get entry point property!\n");
-			goto error;
-		}
-#endif
-	} else {
-		puts ("Could not find kernel entry point!\n");
-		goto error;
-	}
-	kernel = (void (*)(bd_t *, ulong, ulong, ulong, ulong))ep;
+	kernel = (void (*)(bd_t *, ulong, ulong, ulong, ulong))images->ep;
 
 	/* find ramdisk */
 	ret = boot_get_ramdisk (argc, argv, images, IH_ARCH_M68K,

bl33/v2023/lib_microblaze/bootm.c
@@ -40,25 +40,8 @@ void do_bootm_linux (cmd_tbl_t * cmdtp, int flag, int argc, char *argv[],
 	/* First parameter is mapped to $r5 for kernel boot args */
 	void	(*theKernel) (char *);
 	char	*commandline = getenv ("bootargs");
-	ulong	ep = 0;
 
-	/* find kernel entry point */
-	if (images->legacy_hdr_valid) {
-		ep = image_get_ep (&images->legacy_hdr_os_copy);
-#if defined(CONFIG_FIT)
-	} else if (images->fit_uname_os) {
-		int ret = fit_image_get_entry (images->fit_hdr_os,
-				images->fit_noffset_os, &ep);
-		if (ret) {
-			puts ("Can't get entry point property!\n");
-			goto error;
-		}
-#endif
-	} else {
-		puts ("Could not find kernel entry point!\n");
-		goto error;
-	}
-	theKernel = (void (*)(char *))ep;
+	theKernel = (void (*)(char *))images->ep;
 
 	show_boot_progress (15);
 

bl33/v2023/lib_mips/bootm.c
@@ -49,7 +49,6 @@ void do_bootm_linux (cmd_tbl_t * cmdtp, int flag, int argc, char *argv[],
 		     bootm_headers_t *images)
 {
 	ulong	initrd_start, initrd_end;
-	ulong	ep = 0;
 	void	(*theKernel) (int, char **, char **, int *);
 	char	*commandline = getenv ("bootargs");
 	char	env_buf[12];
@@ -57,22 +56,7 @@ void do_bootm_linux (cmd_tbl_t * cmdtp, int flag, int argc, char *argv[],
 	char	*cp;
 
 	/* find kernel entry point */
-	if (images->legacy_hdr_valid) {
-		ep = image_get_ep (&images->legacy_hdr_os_copy);
-#if defined(CONFIG_FIT)
-	} else if (images->fit_uname_os) {
-		ret = fit_image_get_entry (images->fit_hdr_os,
-				images->fit_noffset_os, &ep);
-		if (ret) {
-			puts ("Can't get entry point property!\n");
-			goto error;
-		}
-#endif
-	} else {
-		puts ("Could not find kernel entry point!\n");
-		goto error;
-	}
-	theKernel = (void (*)(int, char **, char **, int *))ep;
+	theKernel = (void (*)(int, char **, char **, int *))images->ep;
 
 	ret = boot_get_ramdisk (argc, argv, images, IH_ARCH_MIPS,
 			&initrd_start, &initrd_end);

bl33/v2023/lib_nios2/bootm.c
@@ -30,25 +30,7 @@ extern int do_reset (cmd_tbl_t *cmdtp, int flag, int argc, char *argv[]);
 void do_bootm_linux(cmd_tbl_t *cmdtp, int flag, int argc, char *argv[],
 		bootm_headers_t *images)
 {
-	ulong	ep = 0;
-
-	/* find kernel entry point */
-	if (images->legacy_hdr_valid) {
-		ep = image_get_ep (&images->legacy_hdr_os_copy);
-#if defined(CONFIG_FIT)
-	} else if (images->fit_uname_os) {
-		int ret = fit_image_get_entry (images->fit_hdr_os,
-				images->fit_noffset_os, &ep);
-		if (ret) {
-			puts ("Can't get entry point property!\n");
-			goto error;
-		}
-#endif
-	} else {
-		puts ("Could not find kernel entry point!\n");
-		goto error;
-	}
-	void (*kernel)(void) = (void (*)(void))ep;
+	void (*kernel)(void) = (void (*)(void))images->ep;
 
 	/* For now we assume the Microtronix linux ... which only
 	 * needs to be called ;-)

commit 4b76c65277c2da71061bd332834610a308ebaac5

SPARC: Fixed compiler error introduced by commit b3d584aeb3ff
This patch fixes a build error for the SPARC platform. It was
introduced by commit b3d584aeb3ff0b8b4105091e6003dd3a294e3169.

Signed-off-by: Daniel Hellstrom <daniel@gaisler.com>

bl33/v2023/lib_sparc/bootm.c
@@ -27,6 +27,7 @@
 #include <asm/byteorder.h>
 #include <asm/prom.h>
 #include <asm/cache.h>
+#include <image.h>
 
 #define PRINT_KERNEL_HEADER
 
@@ -178,7 +179,7 @@ int do_bootm_linux(int flag, int argc, char *argv[], bootm_headers_t * images)
 	 * From now on the only code in u-boot that will be
 	 * executed is the PROM code.
 	 */
-	kernel(kernel_arg_promvec, (void *)ep);
+	kernel(kernel_arg_promvec, (void *)images->ep);
 
 	/* It will never come to this... */
 	while (1) ;
CoreELEC/u-boot

commit f5305a730c670447f043a3cc0ad2e5874fac1ef0

Moved initialization of MPC8XX SCC to cpu_eth_init()
Removed initialization of the driver from net/eth.c

Signed-off-by: Ben Warren <biggerbadderben@gmail.com>

bl33/v2023/cpu/mpc8xx/cpu.c
@@ -37,6 +37,7 @@
 #include <watchdog.h>
 #include <command.h>
 #include <mpc8xx.h>
+#include <commproc.h>
 #include <netdev.h>
 #include <asm/cache.h>
 
@@ -643,6 +644,9 @@ void reset_8xx_watchdog (volatile immap_t * immr)
  */
 int cpu_eth_init(bd_t *bis)
 {
+#if defined(SCC_ENET)
+	scc_initialize(bis);
+#endif
 #if defined(FEC_ENET)
 	fec_initialize(bis);
 #endif

bl33/v2023/include/netdev.h
@@ -64,6 +64,7 @@ int pcnet_initialize(bd_t *bis);
 int plb2800_eth_initialize(bd_t *bis);
 int rtl8139_initialize(bd_t *bis);
 int rtl8169_initialize(bd_t *bis);
+int scc_initialize(bd_t *bis);
 int skge_initialize(bd_t *bis);
 int tsi108_eth_initialize(bd_t *bis);
 int uec_initialize(int index);

bl33/v2023/net/eth.c
@@ -42,7 +42,6 @@ int board_eth_init(bd_t *bis) __attribute((weak, alias("__def_eth_init")));
 extern int mv6436x_eth_initialize(bd_t *);
 extern int mv6446x_eth_initialize(bd_t *);
 extern int ppc_4xx_eth_initialize(bd_t *);
-extern int scc_initialize(bd_t*);
 
 #ifdef CONFIG_API
 extern void (*push_packet)(volatile void *, int);
@@ -156,9 +155,6 @@ int eth_initialize(bd_t *bis)
 #endif
 #if defined(CONFIG_4xx) && !defined(CONFIG_IOP480) && !defined(CONFIG_AP1000)
 	ppc_4xx_eth_initialize(bis);
-#endif
-#ifdef SCC_ENET
-	scc_initialize(bis);
 #endif
 	if (!eth_devices) {
 		puts ("No ethernet found.\n");

commit ca9f960762724285c78951e1f1ad3f8300e1f5fc

Fix compile error in building MBX860T. Bug was introduced in f5305a730c670447f043a3cc0ad2e5874fac1ef0
Signed-off-by: Ben Warren <biggerbadderben@gmail.com>

bl33/v2023/cpu/mpc8xx/cpu.c
@@ -644,7 +644,7 @@ void reset_8xx_watchdog (volatile immap_t * immr)
  */
 int cpu_eth_init(bd_t *bis)
 {
-#if defined(SCC_ENET)
+#if defined(SCC_ENET) && defined(CONFIG_CMD_NET)
 	scc_initialize(bis);
 #endif
 #if defined(FEC_ENET)
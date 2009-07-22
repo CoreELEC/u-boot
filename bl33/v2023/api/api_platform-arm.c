CoreELEC/u-boot

commit c55b7025a1d04f3368fa82908ffa345a088c8abf

api: remove un-needed ifdef CONFIG_API already handle by the Makefile
Signed-off-by: Jean-Christophe PLAGNIOL-VILLARD <plagnioj@jcrosoft.com>

bl33/v2023/api/api_platform-arm.c
@@ -28,9 +28,6 @@
  */
 
 #include <config.h>
-
-#if defined(CONFIG_API)
-
 #include <linux/types.h>
 #include <api_public.h>
 

bl33/v2023/api/api.c
@@ -24,9 +24,6 @@
  */
 
 #include <config.h>
-
-#if defined(CONFIG_API)
-
 #include <command.h>
 #include <common.h>
 #include <malloc.h>
@@ -665,5 +662,3 @@ void platform_set_mr(struct sys_info *si, unsigned long start, unsigned long siz
 			return;
 		}
 }
-
-#endif /* CONFIG_API */

bl33/v2023/api/api_net.c
@@ -24,9 +24,6 @@
  */
 
 #include <config.h>
-
-#if defined(CONFIG_API)
-
 #include <common.h>
 #include <net.h>
 #include <linux/types.h>
@@ -109,5 +106,3 @@ int dev_read_net(void *cookie, void *buf, int len)
 
 	return eth_receive(buf, len);
 }
-
-#endif /* CONFIG_API */

bl33/v2023/api/api_platform-ppc.c
@@ -28,9 +28,6 @@
  */
 
 #include <config.h>
-
-#if defined(CONFIG_API)
-
 #include <linux/types.h>
 #include <api_public.h>
 
@@ -75,5 +72,3 @@ int platform_sys_info(struct sys_info *si)
 
 	return 1;
 }
-
-#endif /* CONFIG_API */

bl33/v2023/api/api_storage.c
@@ -24,9 +24,6 @@
  */
 
 #include <config.h>
-
-#if defined(CONFIG_API)
-
 #include <common.h>
 #include <api_public.h>
 
@@ -391,5 +388,3 @@ lbasize_t dev_read_stor(void *cookie, void *buf, lbasize_t len, lbastart_t start
 
 	return (dd->block_read(dev_stor_index(dd), start, len, buf));
 }
-
-#endif /* CONFIG_API */

commit 9e903c03422a0f9d9c7c5f73ffc5e465bd870a17

api: Fix broken build on ARM.
This patch fixes broken build introduced by commit
c55b7025a1d04f3368fa82908ffa345a088c8abf (api: remove un-needed
ifdef CONFIG_API already handle by the Makefile).

Signed-off-by: Piotr Ziecik <kosmo@semihalf.com>

bl33/v2023/api/api_platform-arm.c
@@ -53,5 +53,3 @@ int platform_sys_info(struct sys_info *si)
 
 	return 1;
 }
-
-#endif /* CONFIG_API */
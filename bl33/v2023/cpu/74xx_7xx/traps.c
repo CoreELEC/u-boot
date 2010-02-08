CoreELEC/u-boot

commit f9adb31bd3e0c9792354df21956e5b02f392fc46

kgdb: drop duplicate debugger_exception_handler
The debugger_exception_handler definition is the same for everyone, so use
the common one now.

Signed-off-by: Mike Frysinger <vapier@gentoo.org>

bl33/v2023/cpu/74xx_7xx/traps.c
@@ -40,10 +40,6 @@
 DECLARE_GLOBAL_DATA_PTR;
 #endif
 
-#if defined(CONFIG_CMD_KGDB)
-int (*debugger_exception_handler)(struct pt_regs *) = 0;
-#endif
-
 /* Returns 0 if exception not found and fixup otherwise.  */
 extern unsigned long search_exception_table(unsigned long);
 

bl33/v2023/cpu/mpc5xx/traps.c
@@ -36,10 +36,6 @@
 #include <command.h>
 #include <asm/processor.h>
 
-#if defined(CONFIG_CMD_KGDB)
-int (*debugger_exception_handler)(struct pt_regs *) = 0;
-#endif
-
 #if defined(CONFIG_CMD_BEDBUG)
 extern void do_bedbug_breakpoint(struct pt_regs *);
 #endif

bl33/v2023/cpu/mpc5xxx/traps.c
@@ -37,10 +37,6 @@
 #include <command.h>
 #include <asm/processor.h>
 
-#if defined(CONFIG_CMD_KGDB)
-int (*debugger_exception_handler)(struct pt_regs *) = 0;
-#endif
-
 /* Returns 0 if exception not found and fixup otherwise.  */
 extern unsigned long search_exception_table(unsigned long);
 

bl33/v2023/cpu/mpc8220/traps.c
@@ -37,10 +37,6 @@
 #include <command.h>
 #include <asm/processor.h>
 
-#if defined(CONFIG_CMD_KGDB)
-int (*debugger_exception_handler) (struct pt_regs *) = 0;
-#endif
-
 /* Returns 0 if exception not found and fixup otherwise.  */
 extern unsigned long search_exception_table (unsigned long);
 

bl33/v2023/cpu/mpc8260/traps.c
@@ -37,10 +37,6 @@
 #include <asm/processor.h>
 #include <asm/m8260_pci.h>
 
-#if defined(CONFIG_CMD_KGDB)
-int (*debugger_exception_handler)(struct pt_regs *) = 0;
-#endif
-
 /* Returns 0 if exception not found and fixup otherwise.  */
 extern unsigned long search_exception_table(unsigned long);
 

bl33/v2023/cpu/mpc85xx/traps.c
@@ -42,10 +42,6 @@
 
 DECLARE_GLOBAL_DATA_PTR;
 
-#if defined(CONFIG_CMD_KGDB)
-int (*debugger_exception_handler)(struct pt_regs *) = 0;
-#endif
-
 /* Returns 0 if exception not found and fixup otherwise.  */
 extern unsigned long search_exception_table(unsigned long);
 

bl33/v2023/cpu/mpc86xx/traps.c
@@ -36,10 +36,6 @@
 
 DECLARE_GLOBAL_DATA_PTR;
 
-#if defined(CONFIG_CMD_KGDB)
-int (*debugger_exception_handler)(struct pt_regs *) = 0;
-#endif
-
 /* Returns 0 if exception not found and fixup otherwise.  */
 extern unsigned long search_exception_table(unsigned long);
 

bl33/v2023/cpu/mpc8xx/traps.c
@@ -36,10 +36,6 @@
 #include <command.h>
 #include <asm/processor.h>
 
-#if defined(CONFIG_CMD_KGDB)
-int (*debugger_exception_handler)(struct pt_regs *) = 0;
-#endif
-
 #if defined(CONFIG_CMD_BEDBUG)
 extern void do_bedbug_breakpoint(struct pt_regs *);
 #endif

commit 8d3aace130c98c46e138347adf4e6c302cb10c79

kgdb: cpu/mpc* cpu/74xx: include kgdb.h when needed
Commit f9adb31bd3e0c979 broke some platforms which used kgdb code but
didn't actually include kgdb.h.  So include kgdb.h in all the relevant
traps code.

Signed-off-by: Mike Frysinger <vapier@gentoo.org>

bl33/v2023/cpu/74xx_7xx/traps.c
@@ -34,6 +34,7 @@
 
 #include <common.h>
 #include <command.h>
+#include <kgdb.h>
 #include <asm/processor.h>
 
 #ifdef CONFIG_AMIGAONEG3SE

bl33/v2023/cpu/mpc512x/traps.c
@@ -28,6 +28,7 @@
  */
 
 #include <common.h>
+#include <kgdb.h>
 #include <asm/processor.h>
 
 DECLARE_GLOBAL_DATA_PTR;

bl33/v2023/cpu/mpc5xx/traps.c
@@ -34,6 +34,7 @@
 
 #include <common.h>
 #include <command.h>
+#include <kgdb.h>
 #include <asm/processor.h>
 
 #if defined(CONFIG_CMD_BEDBUG)

bl33/v2023/cpu/mpc5xxx/traps.c
@@ -35,6 +35,7 @@
 
 #include <common.h>
 #include <command.h>
+#include <kgdb.h>
 #include <asm/processor.h>
 
 /* Returns 0 if exception not found and fixup otherwise.  */

bl33/v2023/cpu/mpc8220/traps.c
@@ -35,6 +35,7 @@
 
 #include <common.h>
 #include <command.h>
+#include <kgdb.h>
 #include <asm/processor.h>
 
 /* Returns 0 if exception not found and fixup otherwise.  */

bl33/v2023/cpu/mpc8260/traps.c
@@ -34,6 +34,7 @@
 
 #include <common.h>
 #include <command.h>
+#include <kgdb.h>
 #include <asm/processor.h>
 #include <asm/m8260_pci.h>
 

bl33/v2023/cpu/mpc83xx/traps.c
@@ -27,6 +27,7 @@
 
 #include <common.h>
 #include <command.h>
+#include <kgdb.h>
 #include <asm/processor.h>
 #include <asm/mpc8349_pci.h>
 

bl33/v2023/cpu/mpc85xx/traps.c
@@ -38,6 +38,7 @@
 
 #include <common.h>
 #include <command.h>
+#include <kgdb.h>
 #include <asm/processor.h>
 
 DECLARE_GLOBAL_DATA_PTR;

bl33/v2023/cpu/mpc86xx/traps.c
@@ -32,6 +32,7 @@
 
 #include <common.h>
 #include <command.h>
+#include <kgdb.h>
 #include <asm/processor.h>
 
 DECLARE_GLOBAL_DATA_PTR;

bl33/v2023/cpu/mpc8xx/traps.c
@@ -34,6 +34,7 @@
 
 #include <common.h>
 #include <command.h>
+#include <kgdb.h>
 #include <asm/processor.h>
 
 #if defined(CONFIG_CMD_BEDBUG)
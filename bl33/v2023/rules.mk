CoreELEC/u-boot

commit 0da6d6fff7f8766a196176108add2b1b0325f3bd

Adjust dependency rules to permit per-file flags
The dependency rules are currently done in a shell 'for' loop. This does not
permit Makefile variables to adjust preprocessor flags as is done with normal
compile flags, using the CFLAGS_path/file.o syntax.

This change moves the dependency generation into the Makefile itself, and
permits a CPPFLAGS_path/file.o to adjust preprocessor flags on a file or
directory basis.

The CPPFLAGS_... variable is also folded into CFLAGS during the build.

Signed-off-by: Simon Glass <sjg@chromium.org>

bl33/v2023/rules.mk
@@ -25,17 +25,39 @@
 
 _depend:	$(obj).depend
 
-$(obj).depend:	$(src)Makefile $(TOPDIR)/config.mk $(SRCS) $(HOSTSRCS)
-		@rm -f $@
-		@touch $@
-		@for f in $(SRCS); do \
-			g=`basename $$f | sed -e 's/\(.*\)\.[[:alnum:]_]/\1.o/'`; \
-			$(CC) -M $(CPPFLAGS) -MQ $(obj)$$g $$f >> $@ ; \
-		done
-		@for f in $(HOSTSRCS); do \
-			g=`basename $$f | sed -e 's/\(.*\)\.[[:alnum:]_]/\1.o/'`; \
-			$(HOSTCC) -M $(HOSTCPPFLAGS) -MQ $(obj)$$g $$f >> $@ ; \
-		done
+# Split the source files into two camps: those in the current directory, and
+# those somewhere else. For the first camp we want to support CPPFLAGS_<fname>
+# and for the second we don't / can't.
+PWD_SRCS := $(filter $(notdir $(SRCS)),$(SRCS))
+OTHER_SRCS := $(filter-out $(notdir $(SRCS)),$(SRCS))
+
+# This is a list of dependency files to generate
+DEPS := $(basename $(patsubst %,$(obj).depend.%,$(PWD_SRCS)))
+
+# Join all the dependencies into a single file, in three parts
+#	1 .Concatenate all the generated depend files together
+#	2. Add in the deps from OTHER_SRCS which we couldn't process
+#	3. Add in the HOSTSRCS
+$(obj).depend:	$(src)Makefile $(TOPDIR)/config.mk $(DEPS) $(HOSTSRCS)
+	cat /dev/null $(DEPS) >$@
+	@for f in $(OTHER_SRCS); do \
+		g=`basename $$f | sed -e 's/\(.*\)\.[[:alnum:]_]/\1.o/'`; \
+		$(CC) -M $(CPPFLAGS) -MQ $(obj)$$g $$f >> $@ ; \
+	done
+	@for f in $(HOSTSRCS); do \
+		g=`basename $$f | sed -e 's/\(.*\)\.[[:alnum:]_]/\1.o/'`; \
+		$(HOSTCC) -M $(HOSTCPPFLAGS) -MQ $(obj)$$g $$f >> $@ ; \
+	done
+
+MAKE_DEPEND = $(CC) -M $(CPPFLAGS) $(EXTRA_CPPFLAGS_DEP) \
+		-MQ $(addsuffix .o,$(obj)$(basename $<)) $< >$@
+
+
+$(obj).depend.%:	%.c
+	$(MAKE_DEPEND)
+
+$(obj).depend.%:	%.S
+	$(MAKE_DEPEND)
 
 $(HOSTOBJS): $(obj)%.o: %.c
 	$(HOSTCC) $(HOSTCFLAGS) $(HOSTCFLAGS_$(@F)) $(HOSTCFLAGS_$(BCURDIR)) -o $@ $< -c

bl33/v2023/.gitignore
@@ -40,7 +40,7 @@
 # Generated files
 #
 
-*.depend
+*.depend*
 /LOG
 /errlog
 /reloc_off

bl33/v2023/Makefile
@@ -955,7 +955,7 @@ clean:
 		| xargs rm -f
 
 clobber:	clean
-	@find $(OBJTREE) -type f \( -name '*.depend' \
+	@find $(OBJTREE) -type f \( -name '*.depend*' \
 		-o -name '*.srec' -o -name '*.bin' -o -name u-boot.img \) \
 		-print0 \
 		| xargs -0 rm -f

bl33/v2023/config.mk
@@ -277,6 +277,13 @@ export	CONFIG_SYS_TEXT_BASE PLATFORM_CPPFLAGS PLATFORM_RELFLAGS CPPFLAGS CFLAGS 
 BCURDIR = $(subst $(SRCTREE)/,,$(CURDIR:$(obj)%=%))
 ALL_AFLAGS = $(AFLAGS) $(AFLAGS_$(BCURDIR)/$(@F)) $(AFLAGS_$(BCURDIR))
 ALL_CFLAGS = $(CFLAGS) $(CFLAGS_$(BCURDIR)/$(@F)) $(CFLAGS_$(BCURDIR))
+EXTRA_CPPFLAGS = $(CPPFLAGS_$(BCURDIR)/$(@F)) $(CPPFLAGS_$(BCURDIR))
+ALL_CFLAGS += $(EXTRA_CPPFLAGS)
+
+# The _DEP version uses the $< file target (for dependency generation)
+# See rules.mk
+EXTRA_CPPFLAGS_DEP = $(CPPFLAGS_$(BCURDIR)/$(addsuffix .o,$(basename $<))) \
+		$(CPPFLAGS_$(BCURDIR))
 $(obj)%.s:	%.S
 	$(CPP) $(ALL_AFLAGS) -o $@ $<
 $(obj)%.o:	%.S

commit 0d586306127482a8fc0b8c0ac044e5a07b1ea643

Correct dependency rule to fix SPL build
Commit 0da6d6f introduced a change in the dependency generation which
breaks SPL, because the source files being built are not initially present
and are symlinked as part of the build.

The .depend file must depend not only on the files in the DEPS list but
also on the sources which did not contribute files to the DEPS list, since
these sources will otherwise not get a dependency and will not be built.

Signed-off-by: Simon Glass <sjg@chromium.org>
Tested-by: Wolfgang Denk <wd@denx.de>

bl33/v2023/rules.mk
@@ -38,7 +38,8 @@ DEPS := $(basename $(patsubst %,$(obj).depend.%,$(PWD_SRCS)))
 #	1 .Concatenate all the generated depend files together
 #	2. Add in the deps from OTHER_SRCS which we couldn't process
 #	3. Add in the HOSTSRCS
-$(obj).depend:	$(src)Makefile $(TOPDIR)/config.mk $(DEPS) $(HOSTSRCS)
+$(obj).depend:	$(src)Makefile $(TOPDIR)/config.mk $(DEPS) $(OTHER_SRCS) \
+		$(HOSTSRCS)
 	cat /dev/null $(DEPS) >$@
 	@for f in $(OTHER_SRCS); do \
 		g=`basename $$f | sed -e 's/\(.*\)\.[[:alnum:]_]/\1.o/'`; \
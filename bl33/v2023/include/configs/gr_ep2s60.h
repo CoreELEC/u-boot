CoreELEC/u-boot

commit 4a3142dd1cb60a57b25a8e632de2194ec673c9ca

net: Remove all references to CONFIG_ETHADDR and friends
We really don't want boards defining fixed MAC addresses in their config
so we just remove the option to set it in a fixed way. If you must have
a MAC address that was not provisioned, then use the random MAC address
functionality.

Signed-off-by: Joe Hershberger <joe.hershberger@ni.com>
Reviewed-by: Simon Glass <sjg@chromium.org>

bl33/v2023/include/configs/gr_ep2s60.h
@@ -260,16 +260,6 @@
 /* USE GRETH Ethernet Driver */
 #define CONFIG_GRETH	1
 
-/* Default GRETH Ethernet HARDWARE address */
-#define GRETH_HWADDR_0 0x00
-#define GRETH_HWADDR_1 0x00
-#define GRETH_HWADDR_2 0x7a
-#define GRETH_HWADDR_3 0xcc
-#define GRETH_HWADDR_4 0x00
-#define GRETH_HWADDR_5 0x13
-#endif
-
-#define CONFIG_ETHADDR   00:00:7a:cc:00:13
 #define CONFIG_PHY_ADDR	 0x00
 
 /*

commit eab8a6f556d2e3dfcb6330ec4a0a9ca098f20397

sparc: fix build error on gr_ep2s60 board
Commit 4a3142dd1cb6 (net: Remove all references to CONFIG_ETHADDR
and friends) accidentally dropped #endif.  Since then, gr_ep2s60
could not build.

scripts/kconfig/conf --silentoldconfig Kconfig
  CHK     include/config.h
  GEN     include/autoconf.mk
In file included from include/config.h:5:0,
                 from ./include/common.h:18:
include/configs/gr_ep2s60.h:15:0: error: unterminated #ifndef
 #ifndef __CONFIG_H__
 ^
make[1]: *** [include/autoconf.mk] Error 1

Signed-off-by: Masahiro Yamada <yamada.masahiro@socionext.com>
Acked-by: Joe Hershberger <joe.hershberger@ni.com>

bl33/v2023/include/configs/gr_ep2s60.h
@@ -259,6 +259,7 @@
 
 /* USE GRETH Ethernet Driver */
 #define CONFIG_GRETH	1
+#endif
 
 #define CONFIG_PHY_ADDR	 0x00
 
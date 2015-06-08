CoreELEC/u-boot

commit 7b1fa7076ed355e169cf01f7745ba41a49e1078e

imx6: centralise common boot options in mx6_common.h
Define common LOADADDR and BOOTDELAY to ensure a consistent experience
across mx6 boards

Signed-off-by: Peter Robinson <pbrobinson@gmail.com>

bl33/v2023/include/configs/mx6_common.h
@@ -52,6 +52,16 @@
 #define CONFIG_INITRD_TAG
 #define CONFIG_REVISION_TAG
 
+/* Boot options */
+#define CONFIG_LOADADDR		0x12000000
+#define CONFIG_SYS_LOAD_ADDR	CONFIG_LOADADDR
+#ifndef CONFIG_SYS_TEXT_BASE
+#define CONFIG_SYS_TEXT_BASE	0x17800000
+#endif
+#ifndef CONFIG_BOOTDELAY
+#define CONFIG_BOOTDELAY	3
+#endif
+
 /* GPIO */
 #define CONFIG_MXC_GPIO
 #define CONFIG_CMD_GPIO

bl33/v2023/include/configs/tqma6.h
@@ -138,10 +138,6 @@
 #define CONFIG_CMD_ITEST
 #define CONFIG_CMD_SETEXPR
 
-#define CONFIG_BOOTDELAY		3
-
-#define CONFIG_LOADADDR			0x12000000
-
 /* place code in last 4 MiB of RAM */
 #if defined(CONFIG_MX6DL) || defined(CONFIG_MX6S)
 #define CONFIG_SYS_TEXT_BASE		0x2fc00000
@@ -406,8 +402,6 @@
 					 sizeof(CONFIG_SYS_PROMPT) + 16)
 #define CONFIG_SYS_MAXARGS		16
 #define CONFIG_SYS_BARGSIZE		CONFIG_SYS_CBSIZE
-
-#define CONFIG_SYS_LOAD_ADDR		CONFIG_LOADADDR
 
 #define CONFIG_CMDLINE_EDITING
 #define CONFIG_STACKSIZE		(128u * SZ_1K)

bl33/v2023/include/configs/aristainetos-common.h
@@ -75,11 +75,6 @@
 #define CONFIG_CMD_BOOTZ
 #define CONFIG_CMD_SETEXPR
 
-#define CONFIG_BOOTDELAY		3
-
-#define CONFIG_LOADADDR			0x12000000
-#define CONFIG_SYS_TEXT_BASE		0x17800000
-
 #define CONFIG_EXTRA_ENV_SETTINGS \
 	"script=u-boot.scr\0" \
 	"fit_file=/boot/system.itb\0" \
@@ -195,8 +190,6 @@
 #define CONFIG_SYS_MEMTEST_START	PHYS_SDRAM
 #define CONFIG_SYS_MEMTEST_END		(CONFIG_SYS_MEMTEST_START + 0x100000)
 #define CONFIG_SYS_MEMTEST_SCRATCH	0x10800000
-
-#define CONFIG_SYS_LOAD_ADDR		CONFIG_LOADADDR
 
 #define CONFIG_CMDLINE_EDITING
 #define CONFIG_STACKSIZE		(128 * 1024)

bl33/v2023/include/configs/cgtqmx6eval.h
@@ -49,11 +49,6 @@
 
 /* Command definition */
 
-#define CONFIG_BOOTDELAY	       3
-
-#define CONFIG_LOADADDR			       0x12000000
-#define CONFIG_SYS_TEXT_BASE	       0x17800000
-
 #define CONFIG_DEFAULT_FDT_FILE "imx6q-congatec.dtb"
 
 #define CONFIG_EXTRA_ENV_SETTINGS \
@@ -124,8 +119,6 @@
 #define CONFIG_SYS_MEMTEST_START       0x10000000
 #define CONFIG_SYS_MEMTEST_END	       0x10010000
 #define CONFIG_SYS_MEMTEST_SCRATCH     0x10800000
-
-#define CONFIG_SYS_LOAD_ADDR	       CONFIG_LOADADDR
 
 #define CONFIG_CMDLINE_EDITING
 

bl33/v2023/include/configs/cm_fx6.h
@@ -247,9 +247,6 @@
 #define CONFIG_DWC_AHSATA_BASE_ADDR	SATA_ARB_BASE_ADDR
 
 /* Boot */
-#define CONFIG_ZERO_BOOTDELAY_CHECK
-#define CONFIG_LOADADDR			0x10800000
-#define CONFIG_SYS_LOAD_ADDR		CONFIG_LOADADDR
 #define CONFIG_SYS_BOOTMAPSZ	        (8 << 20)
 #define CONFIG_SERIAL_TAG
 

bl33/v2023/include/configs/embestmx6boards.h
@@ -97,9 +97,6 @@
 #define CONFIG_CMD_BMODE
 #define CONFIG_CMD_SETEXPR
 
-#define CONFIG_LOADADDR                        0x12000000
-#define CONFIG_SYS_TEXT_BASE           0x17800000
-
 #define CONFIG_ARP_TIMEOUT     200UL
 
 /* Miscellaneous configurable options */
@@ -114,8 +111,6 @@
 #define CONFIG_SYS_MEMTEST_START       0x10000000
 #define CONFIG_SYS_MEMTEST_END         0x10010000
 #define CONFIG_SYS_MEMTEST_SCRATCH     0x10800000
-
-#define CONFIG_SYS_LOAD_ADDR           CONFIG_LOADADDR
 
 #define CONFIG_STACKSIZE               (128 * 1024)
 

bl33/v2023/include/configs/gw_ventana.h
@@ -270,8 +270,6 @@
 #define CONFIG_SYS_MEMTEST_START       0x10000000
 #define CONFIG_SYS_MEMTEST_END	       0x10010000
 #define CONFIG_SYS_MEMTEST_SCRATCH     0x10800000
-#define CONFIG_SYS_TEXT_BASE	         0x17800000
-#define CONFIG_SYS_LOAD_ADDR           0x12000000
 
 /* Physical Memory Map */
 #define CONFIG_NR_DRAM_BANKS           1
@@ -329,8 +327,6 @@
 #endif
 
 /* Environment */
-#define CONFIG_BOOTDELAY          3
-#define CONFIG_LOADADDR           CONFIG_SYS_LOAD_ADDR
 #define CONFIG_IPADDR             192.168.1.1
 #define CONFIG_SERVERIP           192.168.1.146
 #define HWCONFIG_DEFAULT \

bl33/v2023/include/configs/mx6cuboxi.h
@@ -99,11 +99,6 @@
 #define CONFIG_CMD_BOOTZ
 #define CONFIG_CMD_SETEXPR
 
-#define CONFIG_BOOTDELAY		1
-
-#define CONFIG_LOADADDR			0x12000000
-#define CONFIG_SYS_TEXT_BASE		0x17800000
-
 #define CONFIG_MXC_UART_BASE	UART1_BASE
 #define CONFIG_CONSOLE_DEV	"ttymxc0"
 #define CONFIG_MMCROOT		"/dev/mmcblk0p2"
@@ -216,8 +211,6 @@
 #define CONFIG_SYS_CBSIZE              256
 #define CONFIG_SYS_MAXARGS             16
 #define CONFIG_SYS_BARGSIZE CONFIG_SYS_CBSIZE
-
-#define CONFIG_SYS_LOAD_ADDR           CONFIG_LOADADDR
 
 #define CONFIG_CMDLINE_EDITING
 

commit d8ec98c679536970ba4b8b1d9552fa2c8ed520a3

mx6_common: Fix LOADADDR and SYS_TEXT_BASE for MX6SL and MX6SX
Commit 7b1fa7076ed355e16 ("imx6: centralise common boot options in
mx6_common.h") broke boot on mx6sl and mx6sx by assuming that all mx6
SoCs use the same LOADADDR/SYS_TEXT_BASE range, which is not correct.

DDR on mx6sx/mx6sl starts at 0x80000000.

Adjust LOADADDR/SYS_TEXT_BASE to the proper values for mx6sx/mx6sl,
so that these SoCs can boot again.

Also, TQMA6 requires a custom CONFIG_SYS_TEXT_BASE value, so move
its setting prior to the inclusion of mx6_common.h.

Signed-off-by: Fabio Estevam <fabio.estevam at freescale.com>

bl33/v2023/include/configs/mx6_common.h
@@ -53,11 +53,19 @@
 #define CONFIG_REVISION_TAG
 
 /* Boot options */
+#if (defined(CONFIG_MX6SX) || defined(CONFIG_MX6SL))
+#define CONFIG_LOADADDR		0x82000000
+#ifndef CONFIG_SYS_TEXT_BASE
+#define CONFIG_SYS_TEXT_BASE	0x87800000
+#endif
+#else
 #define CONFIG_LOADADDR		0x12000000
-#define CONFIG_SYS_LOAD_ADDR	CONFIG_LOADADDR
 #ifndef CONFIG_SYS_TEXT_BASE
 #define CONFIG_SYS_TEXT_BASE	0x17800000
 #endif
+#endif
+#define CONFIG_SYS_LOAD_ADDR	CONFIG_LOADADDR
+
 #ifndef CONFIG_BOOTDELAY
 #define CONFIG_BOOTDELAY	3
 #endif

bl33/v2023/include/configs/tqma6.h
@@ -22,6 +22,13 @@
 
 /* #endif */
 
+/* place code in last 4 MiB of RAM */
+#if defined(CONFIG_MX6DL) || defined(CONFIG_MX6S)
+#define CONFIG_SYS_TEXT_BASE		0x2fc00000
+#elif defined(CONFIG_MX6Q) || defined(CONFIG_MX6D)
+#define CONFIG_SYS_TEXT_BASE		0x4fc00000
+#endif
+
 #include "mx6_common.h"
 
 #if defined(CONFIG_MX6DL) || defined(CONFIG_MX6S)
@@ -117,13 +124,6 @@
 #define CONFIG_CMD_BMODE
 #define CONFIG_CMD_ITEST
 #define CONFIG_CMD_SETEXPR
-
-/* place code in last 4 MiB of RAM */
-#if defined(CONFIG_MX6DL) || defined(CONFIG_MX6S)
-#define CONFIG_SYS_TEXT_BASE		0x2fc00000
-#elif defined(CONFIG_MX6Q) || defined(CONFIG_MX6D)
-#define CONFIG_SYS_TEXT_BASE		0x4fc00000
-#endif
 
 #define CONFIG_ENV_SIZE			(SZ_8K)
 /* Size of malloc() pool */
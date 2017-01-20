46206d4cb26d8cd57b39fe7074ed12940dd719ba
Xiaobo Gu <xiaobo.gu@amlogic.com>
2017-01-20 15:50:05 +0800

    Bootloader tools/scripts v3
    a). for 2016.11.01 later code use.
    b). normal boot test on gxb/gxtvbb/gxl/txl pass
    c). secure boot test on gxb/gxtvbb/gxl pass
    
    Change-Id: Ie8812d70cd65202647572aa5ceb860ce41595b6d

--- fip/mk_script.sh
+++ fip/mk_script.sh
@@ -356,8 +356,8 @@ function fix_blx() {
 
   #$7:name flag
   if [ "$7" = "bl30" ]; then
-    declare blx_bin_limit=41984   # PD#132613 2016-10-31 update, 41984->40960
-    declare blx01_bin_limit=12288 # PD#132613 2016-10-31 update, 12288->13312
+    declare blx_bin_limit=40960   # PD#132613 2016-10-31 update, 41984->40960
+    declare blx01_bin_limit=13312 # PD#132613 2016-10-31 update, 12288->13312
   elif [ "$7" = "bl2" ]; then
     declare blx_bin_limit=41984
     declare blx01_bin_limit=7168

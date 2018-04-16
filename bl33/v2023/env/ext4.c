CoreELEC/u-boot

commit 63776d00fd0020d8c165257f242e0274b33cde16

env: mmc/fat/ext4: make sure that the MMC sub-system is initialized before using it
When booting from a non-MMC device, the MMC sub-system may not be
initialized when the environment is first accessed.
We need to make sure that the MMC sub-system is ready in even a non-MMC
boot case.

Therefore, initialize mmc before loading environment from it.

Signed-off-by: Faiz Abbas <faiz_abbas@ti.com>

bl33/v2023/env/ext4.c
@@ -87,6 +87,9 @@ static int env_ext4_load(void)
 	int err;
 	loff_t off;
 
+	if (!strcmp(CONFIG_ENV_EXT4_INTERFACE, "mmc"))
+		mmc_initialize(NULL);
+
 	part = blk_get_device_part_str(CONFIG_ENV_EXT4_INTERFACE,
 				       CONFIG_ENV_EXT4_DEVICE_AND_PART,
 				       &dev_desc, &info, 1);

bl33/v2023/env/fat.c
@@ -89,6 +89,9 @@ static int env_fat_load(void)
 	int dev, part;
 	int err;
 
+	if (!strcmp(CONFIG_ENV_FAT_INTERFACE, "mmc"))
+		mmc_initialize(NULL);
+
 	part = blk_get_device_part_str(CONFIG_ENV_FAT_INTERFACE,
 					CONFIG_ENV_FAT_DEVICE_AND_PART,
 					&dev_desc, &info, 1);

bl33/v2023/env/mmc.c
@@ -273,6 +273,8 @@ static int env_mmc_load(void)
 	ALLOC_CACHE_ALIGN_BUFFER(env_t, tmp_env1, 1);
 	ALLOC_CACHE_ALIGN_BUFFER(env_t, tmp_env2, 1);
 
+	mmc_initialize(NULL);
+
 	mmc = find_mmc_device(dev);
 
 	errmsg = init_mmc_for_env(mmc);

commit 5ab7f85abcc0051c8b077300f3c5ff26aab6612a

env: mmc/fat/ext4: undefined reference to `mmc_initialize'
For CONFIG_ENV_FAT_INTERFACE != 'mmc' a link error
env/fat.c:93: undefined reference to `mmc_initialize'
occurs if CONFIG_MMC_SUPPORT is not enabled.

Fixes: 63776d00fd00 ("env: mmc/fat/ext4: make sure that the MMC sub-system
is initialized before using it")
Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

bl33/v2023/env/ext4.c
@@ -87,8 +87,10 @@ static int env_ext4_load(void)
 	int err;
 	loff_t off;
 
+#ifdef CONFIG_MMC
 	if (!strcmp(CONFIG_ENV_EXT4_INTERFACE, "mmc"))
 		mmc_initialize(NULL);
+#endif
 
 	part = blk_get_device_part_str(CONFIG_ENV_EXT4_INTERFACE,
 				       CONFIG_ENV_EXT4_DEVICE_AND_PART,

bl33/v2023/env/fat.c
@@ -89,8 +89,10 @@ static int env_fat_load(void)
 	int dev, part;
 	int err;
 
+#ifdef CONFIG_MMC
 	if (!strcmp(CONFIG_ENV_FAT_INTERFACE, "mmc"))
 		mmc_initialize(NULL);
+#endif
 
 	part = blk_get_device_part_str(CONFIG_ENV_FAT_INTERFACE,
 					CONFIG_ENV_FAT_DEVICE_AND_PART,
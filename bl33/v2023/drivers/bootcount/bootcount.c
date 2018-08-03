CoreELEC/u-boot

commit 0d81039bc7b5170af9cbb96ae052d4c2b1156ecf

bootcount: flush after storing the bootcounter
If the bootcounter address is in a cached memory,
a flush of dcache must occur after updateing the bootcounter.

Issue found on i.MX6 where bootcounter is put into the internal
(cached) IRAM.

Signed-off-by: Stefano Babic <sbabic@denx.de>

bl33/v2023/drivers/bootcount/bootcount.c
@@ -18,6 +18,9 @@ __weak void bootcount_store(ulong a)
 	raw_bootcount_store(reg, a);
 	raw_bootcount_store(reg + 4, BOOTCOUNT_MAGIC);
 #endif /* defined(CONFIG_SYS_BOOTCOUNT_SINGLEWORD */
+	flush_dcache_range(CONFIG_SYS_BOOTCOUNT_ADDR,
+				CONFIG_SYS_BOOTCOUNT_ADDR +
+				CONFIG_SYS_CACHELINE_SIZE);
 }
 
 __weak ulong bootcount_load(void)

commit 37bcfe29d90b2c215ce3ed1994235dc9a3be3f45

bootcount: Fix misaligned cache operation
0d81039bc7 introduces cache line flushes for the bootcounter, but if
the start address is not aligned then the flush causes warnings of
the form:

  CACHE: Misaligned operation at range [4030b7fc, 4030b83c]

Align both the start and end of the buffer (possibly crossing multiple
lines).

Fixes: 0d81039bc7 ("bootcount: flush after storing the bootcounter")
Signed-off-by: Alex Kiernan <alex.kiernan@gmail.com>
Acked-by: Stefano Babic <sbabic@denx.de>

bl33/v2023/drivers/bootcount/bootcount.c
@@ -11,16 +11,23 @@
 __weak void bootcount_store(ulong a)
 {
 	void *reg = (void *)CONFIG_SYS_BOOTCOUNT_ADDR;
+	uintptr_t flush_start = rounddown(CONFIG_SYS_BOOTCOUNT_ADDR,
+					  CONFIG_SYS_CACHELINE_SIZE);
+	uintptr_t flush_end;
 
 #if defined(CONFIG_SYS_BOOTCOUNT_SINGLEWORD)
 	raw_bootcount_store(reg, (BOOTCOUNT_MAGIC & 0xffff0000) | a);
+
+	flush_end = roundup(CONFIG_SYS_BOOTCOUNT_ADDR + 4,
+			    CONFIG_SYS_CACHELINE_SIZE);
 #else
 	raw_bootcount_store(reg, a);
 	raw_bootcount_store(reg + 4, BOOTCOUNT_MAGIC);
+
+	flush_end = roundup(CONFIG_SYS_BOOTCOUNT_ADDR + 8,
+			    CONFIG_SYS_CACHELINE_SIZE);
 #endif /* defined(CONFIG_SYS_BOOTCOUNT_SINGLEWORD */
-	flush_dcache_range(CONFIG_SYS_BOOTCOUNT_ADDR,
-				CONFIG_SYS_BOOTCOUNT_ADDR +
-				CONFIG_SYS_CACHELINE_SIZE);
+	flush_dcache_range(flush_start, flush_end);
 }
 
 __weak ulong bootcount_load(void)
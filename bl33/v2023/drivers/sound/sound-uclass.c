CoreELEC/u-boot

commit e0ebd7cd5bbc334b4707b595b7ec44f9d071df9b

sound: Correct data output in sound_create_square_wave()
This function currently outputs twice as much data as it should and
overwrites its buffer as a result. Fix it.

Signed-off-by: Simon Glass <sjg@chromium.org>

bl33/v2023/drivers/sound/sound.c
@@ -25,11 +25,9 @@ void sound_create_square_wave(unsigned short *data, int size, uint32_t freq)
 		for (i = 0; size && i < half; i++) {
 			size -= 2;
 			*data++ = amplitude;
-			*data++ = amplitude;
 		}
 		for (i = 0; size && i < period - half; i++) {
 			size -= 2;
-			*data++ = -amplitude;
 			*data++ = -amplitude;
 		}
 	}

commit 307979741e4e38ab2788aa4a0fe5b6e22b16fdf0

dm: sound: Use the correct number of channels for sound
At present the 'beep' sound generates a waveform for only one channel even
if two are being used. This means that the beep is twice the frequency it
should be. Correct this by making it a parameter.

The fix in a previous commit was correct for sandbox but not for other
boards.

Fixes: e0ebd7cd5b ("sound: Correct data output in sound_create_square_wave()")
Signed-off-by: Simon Glass <sjg@chromium.org>

bl33/v2023/drivers/sound/sound-uclass.c
@@ -53,7 +53,7 @@ int sound_beep(struct udevice *dev, int msecs, int frequency_hz)
 	}
 
 	sound_create_square_wave(i2s_uc_priv->samplingrate, data, data_size,
-				 frequency_hz);
+				 frequency_hz, i2s_uc_priv->channels);
 
 	while (msecs >= 1000) {
 		ret = sound_play(dev, data, data_size);

bl33/v2023/drivers/sound/sound.c
@@ -8,7 +8,7 @@
 #include <sound.h>
 
 void sound_create_square_wave(uint sample_rate, unsigned short *data, int size,
-			      uint freq)
+			      uint freq, uint channels)
 {
 	const unsigned short amplitude = 16000; /* between 1 and 32767 */
 	const int period = freq ? sample_rate / freq : 0;
@@ -21,14 +21,17 @@ void sound_create_square_wave(uint sample_rate, unsigned short *data, int size,
 		size--;
 
 	while (size) {
-		int i;
+		int i, j;
+
 		for (i = 0; size && i < half; i++) {
 			size -= 2;
-			*data++ = amplitude;
+			for (j = 0; j < channels; j++)
+				*data++ = amplitude;
 		}
 		for (i = 0; size && i < period - half; i++) {
 			size -= 2;
-			*data++ = -amplitude;
+			for (j = 0; j < channels; j++)
+				*data++ = -amplitude;
 		}
 	}
 }

bl33/v2023/include/sound.h
@@ -37,13 +37,14 @@ struct sound_uc_priv {
 /**
  * Generates square wave sound data for 1 second
  *
- * @param sample_rate   Sample rate in Hz
- * @param data          data buffer pointer
- * @param size          size of the buffer in bytes
- * @param freq          frequency of the wave
+ * @sample_rate: Sample rate in Hz
+ * @data: data buffer pointer
+ * @size: size of the buffer in bytes
+ * @freq: frequency of the wave
+ * @channels: Number of channels to use
  */
 void sound_create_square_wave(uint sample_rate, unsigned short *data, int size,
-			      uint freq);
+			      uint freq, uint channels);
 
 /*
  * The sound uclass brings together a data transport (currently only I2C) and a
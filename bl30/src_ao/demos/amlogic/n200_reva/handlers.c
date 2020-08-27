3c3c2acb54cf2881e75ebfce5bb7b286a0cdb973
Jianxiong Pan <jianxiong.pan@amlogic.com>
2020-08-27 15:14:40 +0800

    t5: t5 pxp bringup. [1/1]
    PD#SWPL-32229
    
    Problem:
    bringup t5 aocpu on pxp.
    
    Solution:
    bringup t5 aocpu on pxp.
    
    Verify:
    build pass.
    
    Change-Id: Ifacc987e7db8af9a8238056ce0d20b3612191ac4
    Signed-off-by: Jianxiong Pan <jianxiong.pan@amlogic.com>

---
+++ bl30/src_ao/demos/amlogic/n200_reva/handlers.c
@@ -0,0 +1,42 @@
+//See LICENSE for license details.
+#include <stdint.h>
+#include <stdio.h>
+#include <unistd.h>
+#include "riscv_encoding.h"
+#include "n200_func.h"
+
+uintptr_t handle_trap(uintptr_t mcause, uintptr_t sp);
+__attribute__((weak)) uintptr_t handle_nmi(void);
+
+__attribute__((weak)) uintptr_t handle_nmi(void)
+{
+  //_exit(1);
+
+	  printf("\nhandle_nmi");
+
+
+  do {}while(1);
+  return 0;
+}
+
+
+__attribute__((weak)) uintptr_t handle_trap(uintptr_t mcause, uintptr_t sp)
+{
+  sp = sp;
+  if ((mcause&0xFFF) == 0xFFF) {
+      handle_nmi();
+  }
+  //write(1, "trap\n", 5);
+  printf("In trap handler, the mcause is %d\n", mcause);
+  printf("In trap handler, the mepc is 0x%lx\n", read_csr(mepc));
+  printf("In trap handler, the mtval is 0x%lx\n", read_csr(mbadaddr));
+  //_exit(mcause);
+  printf("\nhandle_trap");
+
+  do {}while(1);
+  return 0;
+}
+
+
+
+

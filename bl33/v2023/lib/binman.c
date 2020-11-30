CoreELEC/u-boot

commit 4cd025b51486896b24ebd46668a9ce18a66a8254

binman: Call helper function binman_set_rom_offset() to fill offset
There is prepared function for filing rom_offset. That's why use it instead
of copying content of it.

Signed-off-by: Michal Simek <michal.simek@xilinx.com>
Reviewed-by: Simon Glass <sjg@chromium.org>
Reviewed-by: Simon Glass <sjg@chromium.org>

bl33/v2023/lib/binman.c
@@ -103,7 +103,7 @@ int binman_init(void)
 			return log_msg_ret("first image", -ENOENT);
 		binman->image = node;
 	}
-	binman->rom_offset = ROM_OFFSET_NONE;
-
+	binman_set_rom_offset(ROM_OFFSET_NONE);
+\
 	return 0;
 }

commit 63cad596ea32018bf6f3efd000f745639b2bfcbb

binman: Remove additional backslash
The origin patch didn't have this change and it was caused by manual
resolution where additional backslash was added.

Fixes: 4cd025b51486 ("binman: Call helper function binman_set_rom_offset() to fill offset")
Signed-off-by: Michal Simek <michal.simek@xilinx.com>
Reviewed-by: Simon Glass <sjg@chromium.org>

bl33/v2023/lib/binman.c
@@ -104,6 +104,6 @@ int binman_init(void)
 		binman->image = node;
 	}
 	binman_set_rom_offset(ROM_OFFSET_NONE);
-\
+
 	return 0;
 }
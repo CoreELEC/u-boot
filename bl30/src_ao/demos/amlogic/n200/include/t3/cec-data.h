6e278447bc111d2a4f5a7038f4bcb8fe7e6fb837
Jianxiong Pan <jianxiong.pan@amlogic.com>
2021-01-22 19:53:22 +0800

    T3: BL30: init bringup version for pxp. [1/1]
    PD#SWPL-41815
    
    Problem:
    aocpu bringup for t3.
    
    Solution:
    init it.
    
    Verify:
    pxp.
    
    Change-Id: If3178851ec7f6b2d3939417cedeb3dbbe4997862
    Signed-off-by: Jianxiong Pan <jianxiong.pan@amlogic.com>

---
+++ bl30/src_ao/demos/amlogic/n200/include/t3/cec-data.h
@@ -0,0 +1,45 @@
+#ifndef __CEC_SC2_DATA_H__
+#define __CEC_SC2_DATA_H__
+
+#include "register.h"
+#include "gpio.h"
+/*SC2 data define*/
+
+
+/*SC2 register enum cec_reg_idx */
+unsigned int cec_reg_tab[] = {
+	CLKCTRL_CECA_CTRL0,
+	CLKCTRL_CECA_CTRL1,
+	CECA_GEN_CNTL,
+	CECA_RW_REG,
+	CECA_INTR_MASKN,
+	CECA_INTR_CLR,
+	CECA_INTR_STAT,
+
+	CLKCTRL_CECB_CTRL0,
+	CLKCTRL_CECB_CTRL1,
+	CECB_GEN_CNTL,
+	CECB_RW_REG,
+	CECB_INTR_MASKN,
+	CECB_INTR_CLR,
+	CECB_INTR_STAT,
+
+	SYSCTRL_STATUS_REG0,
+	SYSCTRL_STATUS_REG1,
+
+	0xffff,//AO_CEC_STICKY_DATA0,
+	0xffff,//AO_CEC_STICKY_DATA1,
+	0xffff,//AO_CEC_STICKY_DATA2,
+	0xffff,//AO_CEC_STICKY_DATA3,
+	0xffff,//AO_CEC_STICKY_DATA4,
+	0xffff,//AO_CEC_STICKY_DATA5,
+	0xffff,//AO_CEC_STICKY_DATA6,
+	0xffff,//AO_CEC_STICKY_DATA7,
+};
+
+/*GPIOH_3 FUNC4:ceca, FUNC5:cecb*/
+#define CEC_PIN_MX	GPIOH_3
+#define CEC_PIN_FUNC	PIN_FUNC5
+
+#endif
+

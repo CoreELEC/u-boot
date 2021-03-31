221a8b5ad3d8446327279e2fba1a4ece1cd1b3ab
Zelong Dong <zelong.dong@amlogic.com>
2021-03-31 14:14:39 +0800

    keypad: s4 aocpu add keypad support [1/1]
    PD#SWPL-46802
    
    Problem:
    aocpu need add keypad support
    
    Solution:
    add keypad support for s4
    
    Verify:
    s4_ap229
    
    Change-Id: I1a68f5be65e433138596a1b1e00017033e52b782
    Signed-off-by: Zelong Dong <zelong.dong@amlogic.com>

--- bl30/src_ao/demos/amlogic/n200/s4/s4_ap229/keypad.c
+++ bl30/src_ao/demos/amlogic/n200/s4/s4_ap229/keypad.c
@@ -34,6 +34,17 @@ static void vGpioKeyCallBack(struct xReportEvent event)
 
 static void vAdcKeyCallBack(struct xReportEvent event)
 {
+	uint32_t buf[4] = {0};
+
+	switch (event.ulCode) {
+	case ADC_KEY_ID_HOME:
+		buf[0] = POWER_KEY_WAKEUP;
+		STR_Wakeup_src_Queue_Send_FromISR(buf);
+		break;
+	default:
+		break;
+	}
+
 	printf("ADC key event 0x%x, key code %d, responseTime %d\n",
 		event.event, event.ulCode, event.responseTime);
 }
@@ -44,32 +55,32 @@ struct xGpioKeyInfo gpioKeyInfo[] = {
 };
 
 struct xAdcKeyInfo adcKeyInfo[] = {
-	ADC_KEY_INFO(ADC_KEY_ID_MENU, 0, SARADC_CH2,
+	ADC_KEY_INFO(ADC_KEY_ID_MENU, 0, SARADC_CH0,
 		     EVENT_SHORT,
 		     vAdcKeyCallBack, NULL),
-	ADC_KEY_INFO(ADC_KEY_ID_VOL_DEC, 574, SARADC_CH2,
+	ADC_KEY_INFO(ADC_KEY_ID_VOL_DEC, 574, SARADC_CH0,
 		     EVENT_SHORT,
 		     vAdcKeyCallBack, NULL),
-	ADC_KEY_INFO(ADC_KEY_ID_VOL_INC, 1065, SARADC_CH2,
+	ADC_KEY_INFO(ADC_KEY_ID_VOL_INC, 1065, SARADC_CH0,
 		     EVENT_SHORT,
 		     vAdcKeyCallBack, NULL),
-	ADC_KEY_INFO(ADC_KEY_ID_ESC, 1557, SARADC_CH2,
+	ADC_KEY_INFO(ADC_KEY_ID_ESC, 1557, SARADC_CH0,
 		     EVENT_SHORT | EVENT_LONG,
 		     vAdcKeyCallBack, NULL),
-	ADC_KEY_INFO(ADC_KEY_ID_HOME, 2048, SARADC_CH2,
+	ADC_KEY_INFO(ADC_KEY_ID_HOME, 2048, SARADC_CH0,
 		     EVENT_SHORT,
 		     vAdcKeyCallBack, NULL)
 };
 
 void vKeyPadInit(void)
 {
-	vCreateGpioKey(gpioKeyInfo,
-			sizeof(gpioKeyInfo)/sizeof(struct xGpioKeyInfo));
-	vGpioKeyEnable();
+	vCreateAdcKey(adcKeyInfo,
+			sizeof(adcKeyInfo)/sizeof(struct xAdcKeyInfo));
+	vAdcKeyEnable();
 }
 
 void vKeyPadDeinit(void)
 {
-	vGpioKeyDisable();
-	vDestoryGpioKey();
+	vAdcKeyDisable();
+	vDestoryAdcKey();
 }

CoreELEC/u-boot

commit c46c3d32187fe036f71a90fac0a2918ee8ab7eab

pinctrl: single: get register area size by device API
Use dev_read_addr_size to get size of the controller's register area.

Signed-off-by: Dario Binacchi <dariobin@libero.it>
Reviewed-by: Pratyush Yadav <p.yadav@ti.com>

bl33/v2023/drivers/pinctrl/pinctrl-single.c
@@ -182,17 +182,19 @@ static int single_set_state(struct udevice *dev,
 static int single_of_to_plat(struct udevice *dev)
 {
 	fdt_addr_t addr;
-	u32 of_reg[2];
-	int res;
+	fdt_size_t size;
 	struct single_pdata *pdata = dev_get_plat(dev);
 
 	pdata->width =
 		dev_read_u32_default(dev, "pinctrl-single,register-width", 0);
 
-	res = dev_read_u32_array(dev, "reg", of_reg, 2);
-	if (res)
-		return res;
-	pdata->offset = of_reg[1] - pdata->width / 8;
+	addr = dev_read_addr_size(dev, "reg", &size);
+	if (addr == FDT_ADDR_T_NONE) {
+		dev_err(dev, "failed to get base register size\n");
+		return -EINVAL;
+	}
+
+	pdata->offset = size - pdata->width / BITS_PER_BYTE;
 
 	addr = dev_read_addr(dev);
 	if (addr == FDT_ADDR_T_NONE) {

commit d2e4ce5facd2d7bcf8224ac64e316e181462cb25

pinctrl: single: Fix probe failure getting register area size
If reg property of pinctrl-single node requires address translation then
probe fails with following message:

single-pinctrl pinctrl@4301c000: failed to get base register size

This is because driver uses dev_read_addr_size() to get size which also
tries to fetch untranslated addr and fails.
Fix this by using dev_read_addr_size_index() which takes care of address
translation and also makes following dev_read_addr() call redundant.

This fixes Ethernet failures on TI's AM654 based EVMs due to lack of
pinmux configuration.

Fixes: c46c3d3218 ("pinctrl: single: get register area size by device API")
Signed-off-by: Vignesh Raghavendra <vigneshr@ti.com>

bl33/v2023/drivers/pinctrl/pinctrl-single.c
@@ -509,19 +509,13 @@ static int single_of_to_plat(struct udevice *dev)
 		return -EINVAL;
 	}
 
-	addr = dev_read_addr_size(dev, "reg", &size);
+	addr = dev_read_addr_size_index(dev, 0, &size);
 	if (addr == FDT_ADDR_T_NONE) {
-		dev_err(dev, "failed to get base register size\n");
+		dev_err(dev, "failed to get base register address\n");
 		return -EINVAL;
 	}
 
 	pdata->offset = size - pdata->width / BITS_PER_BYTE;
-
-	addr = dev_read_addr(dev);
-	if (addr == FDT_ADDR_T_NONE) {
-		dev_dbg(dev, "no valid base register address\n");
-		return -EINVAL;
-	}
 	pdata->base = addr;
 
 	ret = dev_read_u32(dev, "pinctrl-single,function-mask", &pdata->mask);
267e8743ba631d71b0c0e7df08b9651ed1ddc2e2
Qianggui Song <qianggui.song@amlogic.com>
2021-06-02 13:14:46 +0800

    gpio: update p1 gpio data [1/1]
    PD#SWPL-52297
    
    Problem:
    gpio data need update
    
    Solution:
    update p1 gpio data
    
    Verify:
    p1_ptm
    
    Change-Id: I4c4a7157b132e52c00a7190caf984e0c4e422b02
    Signed-off-by: Qianggui Song <qianggui.song@amlogic.com>

--- bl30/src_ao/demos/amlogic/driver/gpio/gpio_p1_plat.c
+++ bl30/src_ao/demos/amlogic/driver/gpio/gpio_p1_plat.c
@@ -38,60 +38,73 @@
 #include "portmacro.h"
 
 /* gpio irq controller */
-#define IRQ_GPIO0_NUM 10
-#define IRQ_GPIO1_NUM 11
-#define IRQ_GPIO2_NUM 12
-#define IRQ_GPIO3_NUM 13
-#define IRQ_GPIO4_NUM 14
-#define IRQ_GPIO5_NUM 15
-#define IRQ_GPIO6_NUM 16
-#define IRQ_GPIO7_NUM 17
+#define IRQ_GPIO0_NUM 228
+#define IRQ_GPIO1_NUM 229
+#define IRQ_GPIO2_NUM 230
+#define IRQ_GPIO3_NUM 231
+#define IRQ_GPIO4_NUM 232
+#define IRQ_GPIO5_NUM 233
+#define IRQ_GPIO6_NUM 234
+#define IRQ_GPIO7_NUM 235
 
-#define REG_PIN_SC2_SEL			0x04
-#define REG_EDGE_POL_EXTR		0x1c
-#define REG_EDGE_POL_MASK_SC2(x)			\
-	({typeof(x) _x = (x); BIT(_x) | BIT(12 + (_x)); })
+#define REG_PIN_P1_SEL			0x10
+#define REG_EDGE_SINGLE			0x04
+#define REG_POL_LOW			0x08
+#define REG_EDGE_BOTH			0x0c
 #define GPIO_IRQ_FILTER_SHIFT(x)	(((x) % 2 == 0) ? 8 : 24)
 #define GPIO_IRQ_POL_SHIFT(x)		(BIT(0 + (x)))
-#define GPIO_IRQ_EDGE_SHIFT(x)		(BIT(12 + (x)))
+#define GPIO_IRQ_EDGE_SHIFT(x)		(BIT(0 + (x)))
 #define GPIO_IRQ_BOTH_SHIFT(x)		(BIT(0 + (x)))
+
+static const GpioDomain_t stDomain = {
+	.name = "ST",
+	.rPullen = PADCTRL_GPIOB_I,
+	.rPull = PADCTRL_GPIOB_I,
+	.rGpio = PADCTRL_GPIOB_I,
+	.rMux = PADCTRL_PIN_MUX_REG10,
+	.rDrv = PADCTRL_GPIOB_I,
+};
 
 static const GpioDomain_t eeDomain = {
 	.name = "EE",
-	.rPullen = PADCTRL_GPIOD_I,
-	.rPull = PADCTRL_GPIOD_I,
-	.rGpio = PADCTRL_GPIOD_I,
-	.rMux = PADCTRL_PIN_MUX_REG0,
-	.rDrv = PADCTRL_GPIOD_I,
+	.rPullen = PADCTRL_TESTN_I,
+	.rPull = PADCTRL_TESTN_I,
+	.rGpio = PADCTRL_TESTN_I,
+	.rMux = PADCTRL_PIN_MUX_REG2,
+	.rDrv = PADCTRL_TESTN_I,
 };
 
 static const GpioBank_t gpioBanks[BANK_NUM_MAX] = {
-			  /* pullen   pull    dir       out      in      mux */
-	BANK("D", &eeDomain, 0x03, 0, 0x04, 0, 0x02, 0, 0x01, 0, 0x00, 0, 0x0a,
-		/* drv */
-	     0, 0x07, 0),
-	BANK("E", &eeDomain, 0x0b, 0, 0x0c, 0, 0x0a, 0, 0x09, 0, 0x08, 0, 0x0c,
-	     0, 0x0f, 0),
-	BANK("Z", &eeDomain, 0x13, 0, 0x14, 0, 0x12, 0, 0x11, 0, 0x10, 0, 0x05,
+			/* pullen   pull    dir       out      in      mux */
+	BANK("D", &eeDomain, 0x13, 0, 0x14, 0, 0x12, 0, 0x11, 0, 0x10, 0, 0x00,
+	        /* drv */
 	     0, 0x17, 0),
-	BANK("H", &eeDomain, 0x1b, 0, 0x1c, 0, 0x1a, 0, 0x19, 0, 0x18, 0, 0x08,
-	     0, 0x1f, 0),
-	BANK("C", &eeDomain, 0x23, 0, 0x24, 0, 0x22, 0, 0x21, 0, 0x20, 0, 0x07,
+	BANK("E", &eeDomain, 0x23, 0, 0x24, 0, 0x22, 0, 0x21, 0, 0x20, 0, 0x02,
 	     0, 0x27, 0),
-	BANK("B", &eeDomain, 0x2b, 0, 0x2c, 0, 0x2a, 0, 0x29, 0, 0x28, 0, 0x00,
-	     0, 0x2f, 0),
-	BANK("X", &eeDomain, 0x33, 0, 0x34, 0, 0x32, 0, 0x31, 0, 0x30, 0, 0x02,
+	BANK("Z", &eeDomain, 0x33, 0, 0x34, 0, 0x32, 0, 0x31, 0, 0x30, 0, 0x1d,
 	     0, 0x37, 0),
-	BANK("T", &eeDomain, 0x43, 0, 0x44, 0, 0x42, 0, 0x41, 0, 0x40, 0, 0x0f,
+	BANK("H", &eeDomain, 0x43, 0, 0x44, 0, 0x42, 0, 0x41, 0, 0x40, 0, 0x0d,
 	     0, 0x47, 0),
-	BANK("Y", &eeDomain, 0x53, 0, 0x54, 0, 0x52, 0, 0x51, 0, 0x50, 0, 0x13,
+	BANK("C", &eeDomain, 0x53, 0, 0x54, 0, 0x52, 0, 0x51, 0, 0x50, 0, 0x03,
 	     0, 0x57, 0),
-	BANK("W", &eeDomain, 0x63, 0, 0x64, 0, 0x62, 0, 0x61, 0, 0x60, 0, 0x16,
-	     0, 0x67, 0),
-	BANK("M", &eeDomain, 0x73, 0, 0x74, 0, 0x72, 0, 0x71, 0, 0x70, 0, 0x0d,
+	BANK("B", &stDomain, 0x03, 0, 0x04, 0, 0x02, 0, 0x01, 0, 0x00, 0, 0x00,
+	     0, 0x07, 0),
+	BANK("X", &eeDomain, 0x73, 0, 0x74, 0, 0x72, 0, 0x71, 0, 0x70, 0, 0x17,
 	     0, 0x77, 0),
-	BANK("TEST_N", &eeDomain, 0x83, 0, 0x84, 0, 0x82, 0, 0x81, 0, 0x80, 0,
-	     0x09, 0, 0x87, 0),
+	BANK("T", &eeDomain, 0x83, 0, 0x84, 0, 0x82, 0, 0x81, 0, 0x80, 0, 0x06,
+	     0, 0x87, 0),
+	BANK("K", &eeDomain, 0x93, 0, 0x94, 0, 0x92, 0, 0x91, 0, 0x90, 0, 0x10,
+	     0, 0x97, 0),
+	BANK("W", &eeDomain, 0xa3, 0, 0xa4, 0, 0xa2, 0, 0xa1, 0, 0xa0, 0, 0x14,
+	     0, 0xa7, 0),
+	BANK("M", &eeDomain, 0xb3, 0, 0xb4, 0, 0xb2, 0, 0xb1, 0, 0xb0, 0, 0x0a,
+	     0, 0xb7, 0),
+	BANK("Y", &eeDomain, 0xc3, 0, 0xc4, 0, 0xc2, 0, 0xc1, 0, 0xc0, 0, 0x1b,
+	     0, 0xc7, 0),
+	BANK("N", &eeDomain, 0xd3, 0, 0xd4, 0, 0xd2, 0, 0xd1, 0, 0xd0, 0, 0x0b,
+	     0, 0xd7, 0),
+	BANK("TEST_N", &eeDomain, 0x03, 0, 0x04, 0, 0x02, 0, 0x01, 0, 0x00, 0, 0x0a,
+	0, 0x07, 0),
 };
 
 static ParentIRQDesc_t eeIRQs[] = {
@@ -105,20 +118,21 @@ static ParentIRQDesc_t eeIRQs[] = {
 	[GPIO_EE_IRQ_L7] = PARENT_IRQ_BK(NULL, 0, GPIO_INVALID, IRQ_GPIO7_NUM),
 };
 
-/* need real gpio irq number */
 static const GpioIRQBank_t irqBanks[BANK_NUM_MAX] = {
-	GPIO_IRQ_BK("D", 57,  eeIRQs, ARRAY_SIZE(eeIRQs)),
-	GPIO_IRQ_BK("E", 70,  eeIRQs, ARRAY_SIZE(eeIRQs)),
-	GPIO_IRQ_BK("Z", 77,  eeIRQs, ARRAY_SIZE(eeIRQs)),
-	GPIO_IRQ_BK("H", 148, eeIRQs, ARRAY_SIZE(eeIRQs)),
-	GPIO_IRQ_BK("C", 13,  eeIRQs, ARRAY_SIZE(eeIRQs)),
+	GPIO_IRQ_BK("D", 13,  eeIRQs, ARRAY_SIZE(eeIRQs)),
+	GPIO_IRQ_BK("E", 26,  eeIRQs, ARRAY_SIZE(eeIRQs)),
+	GPIO_IRQ_BK("Z", 222,  eeIRQs, ARRAY_SIZE(eeIRQs)),
+	GPIO_IRQ_BK("H", 102, eeIRQs, ARRAY_SIZE(eeIRQs)),
+	GPIO_IRQ_BK("C", 32,  eeIRQs, ARRAY_SIZE(eeIRQs)),
 	GPIO_IRQ_BK("B", 0,   eeIRQs, ARRAY_SIZE(eeIRQs)),
-	GPIO_IRQ_BK("X", 20,  eeIRQs, ARRAY_SIZE(eeIRQs)),
-	GPIO_IRQ_BK("T", 91,  eeIRQs, ARRAY_SIZE(eeIRQs)),
-	GPIO_IRQ_BK("Y", 129, eeIRQs, ARRAY_SIZE(eeIRQs)),
-	GPIO_IRQ_BK("W", 40,  eeIRQs, ARRAY_SIZE(eeIRQs)),
-	GPIO_IRQ_BK("M", 115, eeIRQs, ARRAY_SIZE(eeIRQs)),
-	GPIO_IRQ_BK("TEST_N", 156, eeIRQs, ARRAY_SIZE(eeIRQs)),
+	GPIO_IRQ_BK("X", 175,  eeIRQs, ARRAY_SIZE(eeIRQs)),
+	GPIO_IRQ_BK("T", 50,  eeIRQs, ARRAY_SIZE(eeIRQs)),
+	GPIO_IRQ_BK("K", 126,  eeIRQs, ARRAY_SIZE(eeIRQs)),
+	GPIO_IRQ_BK("W", 157,  eeIRQs, ARRAY_SIZE(eeIRQs)),
+	GPIO_IRQ_BK("M", 82, eeIRQs, ARRAY_SIZE(eeIRQs)),
+	GPIO_IRQ_BK("Y", 207, eeIRQs, ARRAY_SIZE(eeIRQs)),
+	GPIO_IRQ_BK("N", 88, eeIRQs, ARRAY_SIZE(eeIRQs)),
+	GPIO_IRQ_BK("TEST_N", 230, eeIRQs, ARRAY_SIZE(eeIRQs)),
 };
 
 const GpioBank_t *pGetGpioBank(void)
@@ -138,12 +152,11 @@ void prvGpioPlatIrqSetup(uint16_t irqNum, uint8_t line, uint32_t flags)
 	uint16_t bit_offset = 0;
 
 	bit_offset = ((line % 2) == 0) ? 0 : 16;
-	reg_offset = REG_PIN_SC2_SEL + ((line / 2) << 2);
+	reg_offset = REG_PIN_P1_SEL + ((line / 2) << 2);
 
 	/* clear both edge */
-	REG32_UPDATE_BITS(GPIO_EE_IRQ_BASE + REG_EDGE_POL_EXTR,
+	REG32_UPDATE_BITS(GPIO_EE_IRQ_BASE + REG_EDGE_BOTH,
 			  GPIO_IRQ_BOTH_SHIFT(line), 0);
-
 	/* set filter */
 	REG32_UPDATE_BITS(GPIO_EE_IRQ_BASE + reg_offset,
 			  0x7 << GPIO_IRQ_FILTER_SHIFT(line),
@@ -156,19 +169,21 @@ void prvGpioPlatIrqSetup(uint16_t irqNum, uint8_t line, uint32_t flags)
 	/* set trigger both type */
 	if (flags & IRQF_TRIGGER_BOTH) {
 		val |= GPIO_IRQ_BOTH_SHIFT(line);
-		REG32_UPDATE_BITS(GPIO_EE_IRQ_BASE + REG_EDGE_POL_EXTR,
+		REG32_UPDATE_BITS(GPIO_EE_IRQ_BASE + REG_EDGE_BOTH,
 				  GPIO_IRQ_BOTH_SHIFT(line), val);
 		return;
 	}
 
 	/* set trigger single edge or level  type */
-	if (flags & (IRQF_TRIGGER_LOW | IRQF_TRIGGER_FALLING))
+	if (flags & (IRQF_TRIGGER_LOW | IRQF_TRIGGER_FALLING)) {
 		val |= GPIO_IRQ_POL_SHIFT(line);
+		REG32_UPDATE_BITS(GPIO_EE_IRQ_BASE + REG_POL_LOW, GPIO_IRQ_POL_SHIFT(line), val);
+	}
 
-	if (flags & (IRQF_TRIGGER_RISING | IRQF_TRIGGER_FALLING))
+	if (flags & (IRQF_TRIGGER_RISING | IRQF_TRIGGER_FALLING)) {
+		val = 0;
 		val |= GPIO_IRQ_EDGE_SHIFT(line);
-
-	REG32_UPDATE_BITS(GPIO_EE_IRQ_BASE, REG_EDGE_POL_MASK_SC2(line), val);
-
+		REG32_UPDATE_BITS(GPIO_EE_IRQ_BASE + REG_EDGE_SINGLE, GPIO_IRQ_EDGE_SHIFT(line), val);
+	}
 }
 
--- bl30/src_ao/demos/amlogic/n200/include/p1/gpio-data.h
+++ bl30/src_ao/demos/amlogic/n200/include/p1/gpio-data.h
@@ -34,26 +34,26 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-#define	GPIO_NUM_MAX	157 /*actual numbers of pins*/
-#define	BANK_NUM_MAX	12   /*numbers of gpio bank*/
-#define	IRQ_REG_NUM	8  /* numbers of irq relative reg*/
+#define	GPIO_NUM_MAX	231 /*actual numbers of pins*/
+#define	BANK_NUM_MAX	14   /*numbers of gpio bank*/
+#define	IRQ_REG_NUM	20  /* numbers of irq relative reg*/
 
 #define	GPIO_INVALID	0xffff
 
 /*0-31*/
-#define GPIOD_0 	0
-#define GPIOD_1 	1
-#define GPIOD_2 	2
-#define GPIOD_3 	3
-#define GPIOD_4 	4
-#define GPIOD_5 	5
-#define GPIOD_6 	6
-#define GPIOD_7 	7
-#define GPIOD_8 	8
-#define GPIOD_9 	9
-#define GPIOD_10	10
-#define GPIOD_11	11
-#define GPIOD_12	12
+#define	GPIOD_0		0
+#define	GPIOD_1		1
+#define	GPIOD_2		2
+#define	GPIOD_3		3
+#define	GPIOD_4		4
+#define	GPIOD_5		5
+#define	GPIOD_6		6
+#define	GPIOD_7		7
+#define	GPIOD_8		8
+#define	GPIOD_9		9
+#define	GPIOD_10	10
+#define	GPIOD_11	11
+#define	GPIOD_12	12
 
 /*32-63*/
 #define	GPIOE_0		32
@@ -62,33 +62,42 @@ extern "C" {
 #define	GPIOE_3		35
 #define	GPIOE_4		36
 #define	GPIOE_5		37
-#define	GPIOE_6		38
 
 /*64-95*/
-#define	GPIOZ_0 	64
-#define	GPIOZ_1 	65
-#define	GPIOZ_2 	66
-#define	GPIOZ_3 	67
-#define	GPIOZ_4 	68
-#define	GPIOZ_5 	69
-#define	GPIOZ_6 	70
-#define	GPIOZ_7 	71
-#define	GPIOZ_8 	72
-#define	GPIOZ_9 	73
-#define	GPIOZ_10	74
-#define	GPIOZ_11	75
-#define	GPIOZ_12	76
-#define	GPIOZ_13	77
+#define	GPIOZ_0		64
+#define	GPIOZ_1		65
+#define	GPIOZ_2		66
+#define	GPIOZ_3		67
+#define	GPIOZ_4		68
+#define	GPIOZ_5		69
+#define	GPIOZ_6		70
+#define	GPIOZ_7		71
 
 /*96-127 */
-#define	GPIOH_0 	96
-#define	GPIOH_1 	97
-#define	GPIOH_2 	98
-#define	GPIOH_3 	99
-#define	GPIOH_4 	100
-#define	GPIOH_5 	101
-#define	GPIOH_6 	102
+#define	GPIOH_0		96
+#define	GPIOH_1		97
+#define	GPIOH_2		98
+#define	GPIOH_3		99
+#define	GPIOH_4		100
+#define	GPIOH_5		101
+#define	GPIOH_6		102
 #define	GPIOH_7 	103
+#define	GPIOH_8 	104
+#define	GPIOH_9 	105
+#define	GPIOH_10	106
+#define	GPIOH_11	107
+#define	GPIOH_12	108
+#define	GPIOH_13	109
+#define	GPIOH_14	110
+#define	GPIOH_15	111
+#define	GPIOH_16	112
+#define	GPIOH_17	113
+#define	GPIOH_18	114
+#define	GPIOH_19	115
+#define	GPIOH_20	116
+#define	GPIOH_21	117
+#define	GPIOH_22	118
+#define	GPIOH_23	119
 
 /*128-159 */
 #define	GPIOC_0		128
@@ -97,7 +106,18 @@ extern "C" {
 #define	GPIOC_3		131
 #define	GPIOC_4		132
 #define	GPIOC_5		133
-#define	GPIOC_6		134
+#define	GPIOC_6 	134
+#define	GPIOC_7 	135
+#define	GPIOC_8 	136
+#define	GPIOC_9 	137
+#define	GPIOC_10	138
+#define	GPIOC_11	139
+#define	GPIOC_12	140
+#define	GPIOC_13	141
+#define	GPIOC_14	142
+#define	GPIOC_15	143
+#define	GPIOC_16	144
+#define	GPIOC_17	145
 
 /* 160-191 */
 #define	GPIOB_0		160
@@ -106,10 +126,10 @@ extern "C" {
 #define	GPIOB_3		163
 #define	GPIOB_4		164
 #define	GPIOB_5		165
-#define	GPIOB_6 	166
-#define	GPIOB_7 	167
-#define	GPIOB_8 	168
-#define	GPIOB_9 	169
+#define	GPIOB_6		166
+#define	GPIOB_7		167
+#define	GPIOB_8		168
+#define	GPIOB_9		169
 #define	GPIOB_10	170
 #define	GPIOB_11	171
 #define	GPIOB_12	172
@@ -135,6 +155,18 @@ extern "C" {
 #define	GPIOX_17	209
 #define	GPIOX_18	210
 #define	GPIOX_19	211
+#define	GPIOX_20	212
+#define	GPIOX_21	213
+#define	GPIOX_22	214
+#define	GPIOX_23	215
+#define	GPIOX_24	216
+#define	GPIOX_25	217
+#define	GPIOX_26	218
+#define	GPIOX_27	219
+#define	GPIOX_28	220
+#define	GPIOX_29	221
+#define	GPIOX_30	222
+#define	GPIOX_31	223
 
 /* 224- 255*/
 #define	GPIOT_0		224
@@ -161,65 +193,111 @@ extern "C" {
 #define	GPIOT_21	245
 #define	GPIOT_22	246
 #define	GPIOT_23	247
+#define	GPIOT_24	248
+#define	GPIOT_25	249
+#define	GPIOT_26	250
+#define	GPIOT_27	251
+#define	GPIOT_28	252
+#define	GPIOT_29	253
+#define	GPIOT_30	254
+#define	GPIOT_31	255
 
 /* 256-287 */
-#define	GPIOY_0 	256
-#define	GPIOY_1 	257
-#define	GPIOY_2 	258
-#define	GPIOY_3 	259
-#define	GPIOY_4 	260
-#define	GPIOY_5 	261
-#define	GPIOY_6 	262
-#define	GPIOY_7 	263
-#define	GPIOY_8 	264
-#define	GPIOY_9 	265
-#define	GPIOY_10	266
-#define	GPIOY_11	267
-#define	GPIOY_12	268
-#define	GPIOY_13	269
-#define	GPIOY_14	270
-#define	GPIOY_15	271
-#define	GPIOY_16	272
-#define	GPIOY_17	273
-#define	GPIOY_18	274
+#define	GPIOK_0		256
+#define	GPIOK_1		257
+#define	GPIOK_2		258
+#define	GPIOK_3		259
+#define	GPIOK_4		260
+#define	GPIOK_5		261
+#define	GPIOK_6		262
+#define	GPIOK_7		263
+#define	GPIOK_8		264
+#define	GPIOK_9		265
+#define	GPIOK_10	266
+#define	GPIOK_11	267
+#define	GPIOK_12	268
+#define	GPIOK_13	269
+#define	GPIOK_14	270
+#define	GPIOK_15	271
+#define	GPIOK_16	272
+#define	GPIOK_17	273
+#define	GPIOK_18	274
+#define	GPIOK_19	275
+#define	GPIOK_20	276
+#define	GPIOK_21	277
+#define	GPIOK_22	278
+#define	GPIOK_23	279
+#define	GPIOK_24	280
+#define	GPIOK_25	281
+#define	GPIOK_26	282
+#define	GPIOK_27	283
+#define	GPIOK_28	284
+#define	GPIOK_29	285
+#define	GPIOK_30	286
 
 /* 288-319 */
-#define	GPIOW_0  	288
-#define	GPIOW_1  	289
-#define	GPIOW_2  	290
-#define	GPIOW_3  	291
-#define	GPIOW_4  	292
-#define	GPIOW_5  	293
-#define	GPIOW_6  	294
-#define	GPIOW_7  	295
-#define	GPIOW_8  	296
-#define	GPIOW_9  	297
-#define	GPIOW_10 	298
-#define	GPIOW_11 	299
-#define	GPIOW_12 	300
-#define	GPIOW_13 	301
-#define	GPIOW_14 	302
-#define	GPIOW_15 	303
-#define	GPIOW_16 	304
+#define	GPIOW_0		288
+#define	GPIOW_1		289
+#define	GPIOW_2		290
+#define	GPIOW_3		291
+#define	GPIOW_4		292
+#define	GPIOW_5		293
+#define	GPIOW_6		294
+#define	GPIOW_7		295
+#define	GPIOW_8		296
+#define	GPIOW_9		297
+#define	GPIOW_10	298
+#define	GPIOW_11	299
+#define	GPIOW_12	300
+#define	GPIOW_13	301
+#define	GPIOW_14	302
+#define	GPIOW_15	303
+#define	GPIOW_16	304
+#define	GPIOW_17	305
 
 /* 320-351 */
-#define	GPIOM_0 	320
-#define	GPIOM_1 	321
-#define	GPIOM_2 	322
-#define	GPIOM_3 	323
-#define	GPIOM_4 	324
+#define	GPIOM_0		320
+#define	GPIOM_1		321
+#define	GPIOM_2		322
+#define	GPIOM_3		323
+#define	GPIOM_4		324
 #define	GPIOM_5 	325
-#define	GPIOM_6 	326
-#define	GPIOM_7  	327
-#define	GPIOM_8  	328
-#define	GPIOM_9  	329
-#define	GPIOM_10 	330
-#define	GPIOM_11 	331
-#define	GPIOM_12 	332
-#define	GPIOM_13 	333
 
-/* 352-384 */
-#define	GPIO_TEST_N	352
+/* 352-383 */
+#define	GPIOY_0 	352
+#define	GPIOY_1 	353
+#define	GPIOY_2 	354
+#define	GPIOY_3 	355
+#define	GPIOY_4 	356
+#define	GPIOY_5 	357
+#define	GPIOY_6 	358
+#define	GPIOY_7 	359
+#define	GPIOY_8 	360
+#define	GPIOY_9 	361
+#define	GPIOY_10	362
+#define	GPIOY_11	363
+#define	GPIOY_12	364
+#define	GPIOY_13	365
+#define	GPIOY_14	366
+
+/* 384 - 415*/
+#define	GPION_0  	384
+#define	GPION_1  	385
+#define	GPION_2  	386
+#define	GPION_3  	387
+#define	GPION_4  	388
+#define	GPION_5  	389
+#define	GPION_6  	390
+#define	GPION_7  	391
+#define	GPION_8  	392
+#define	GPION_9  	393
+#define	GPION_10 	394
+#define	GPION_11 	395
+#define	GPION_12 	396
+#define	GPION_13 	397
+
+/* 416 -*/
+#define	GPIO_TEST_N	416
 
 #ifdef __cplusplus
 }

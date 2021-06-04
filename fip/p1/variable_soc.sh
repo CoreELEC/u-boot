20fae964f93f7124d8fc7e0b925782a73793cba2
Bo Lv <bo.lv@amlogic.com>
2021-06-04 17:33:54 +0800

    P1: fix bl32 compile [1/1]
    PD#SWPL-50160
    
    Problem:
    fix bl32 compile error
    
    Solution:
    fix it
    
    Verify:
    compile pass
    
    Change-Id: Ie5127d2bc336f9b92a9dea4350c64b6498155f28
    Signed-off-by: Bo Lv <bo.lv@amlogic.com>

--- fip/p1/variable_soc.sh
+++ fip/p1/variable_soc.sh
@@ -10,16 +10,15 @@ elif [ -n "${CONFIG_CHIPSET_VARIANT}" ]; then
 else
 	declare CHIPSET_VARIANT_SUFFIX=""
 fi
-declare -a BLX_NAME=("bl2" "bl2" "bl2e" "bl2e" "bl2x" "bl31" "bl30" "bl32")
-#declare -a BLX_NAME=("bl2"	\
-#		     "bl2"	\
-#		     "bl2e"	\
-#		     "bl2e"	\
-#		     "bl2x"	\
-#		     "bl31"	\
-#		     "bl30"	\
-#		     "bl32"	\
-#		     "bl40")
+declare -a BLX_NAME=("bl2"	\
+		     "bl2"	\
+		     "bl2e"	\
+		     "bl2e"	\
+		     "bl2x"	\
+		     "bl31"	\
+		     "bl32"	\
+		     "bl40"	\
+		     "bl30")
 
 declare -a BLX_SRC_FOLDER=("bl2/core"		\
 			   "bl2/core"		\
@@ -27,9 +26,9 @@ declare -a BLX_SRC_FOLDER=("bl2/core"		\
 			   "bl2/ree"		\
 			   "bl2/tee"		\
 			   "bl31_1.3/src"	\
-			   "bl30/src_ao"	\
 			   "bl32_3.8/src"	\
 			   "NULL"		\
+			   "bl30/src_ao"	\
 			   "bl33")
 
 declare -a BLX_BIN_FOLDER=("bl2/bin"		\
@@ -38,9 +37,9 @@ declare -a BLX_BIN_FOLDER=("bl2/bin"		\
 			   "bl2/bin"		\
 			   "bl2/bin"		\
 			   "bl31_1.3/bin"	\
-			   "bl30/bin_ao"	\
 			   "bl32_3.8/bin"	\
-			   "bl40/bin")
+			   "bl40/bin"		\
+			   "bl30/bin_ao")
 
 #declare -a BLX_BIN_NAME=("bb1st.sto${CHIPSET_VARIANT_SUFFIX}.bin.signed"     \
 #			 "bb1st.usb${CHIPSET_VARIANT_SUFFIX}.bin.signed"     \
@@ -59,9 +58,9 @@ declare -a BLX_BIN_NAME=("bl2.bin.sto"	\
 			    "bl2e.bin.usb"	\
 			    "bl2x.bin"		\
 			    "bl31.bin"		\
-			    "NULL"		\
 			    "bl32.bin"		\
-			    "bl40.bin")
+			    "bl40.bin"		\
+				"NULL")
 
 declare -a BLX_BIN_SIZE=("260096"	\
 			 "260096"	\
@@ -69,9 +68,9 @@ declare -a BLX_BIN_SIZE=("260096"	\
 			 "74864"	\
 			 "66672"	\
 			 "266240"	\
-			 "NULL"		\
 			 "528384"	\
-			 "102400")
+			 "102400"	\
+			 "NULL")
 
 declare BL30_BIN_SIZE="65536"
 declare BL33_BIN_SIZE="1572864"
@@ -82,9 +81,9 @@ declare -a BLX_RAWBIN_NAME=("bl2.bin.sto"	\
 			    "bl2e.bin.usb"	\
 			    "bl2x.bin"		\
 			    "bl31.bin"		\
-			    "NULL"		\
 			    "bl32.bin"		\
-			    "bl40.bin")
+			    "bl40.bin"		\
+			    "NULL")
 
 declare -a BLX_IMG_NAME=("NULL"	\
 			 "NULL"	\
@@ -110,8 +109,8 @@ declare -a BLX_SRC_GIT=("bootloader/amlogic-advanced-bootloader/core" \
 			"bootloader/amlogic-advanced-bootloader/ree" \
 			"bootloader/amlogic-advanced-bootloader/tee" \
 			"ARM-software/arm-trusted-firmware" \
-			"firmware/aocpu" \
 			"OP-TEE/optee_os" \
+			"firmware/aocpu" \
 			"uboot")
 
 declare -a BLX_BIN_GIT=("firmware/bin/bl2" \
@@ -130,9 +129,9 @@ declare -a BIN_PATH=("null"	\
 		     "null"	\
 		     "null"	\
 		     "null"	\
-		     "source"	\
 		     "null"	\
-		     "null")
+		     "null"	\
+		     "source")
 
 # variables
 declare -a CUR_REV # current version of each blx

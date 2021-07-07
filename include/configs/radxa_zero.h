ae31914ca13c69e230f4446e901e5fab4e8b728d
Portisch <hugo.portisch@yahoo.de>
2021-07-07 23:08:12 +0200

    board: add Radxa Zero u223_v1

---
+++ include/configs/radxa_zero.h
@@ -0,0 +1,13 @@
+#ifndef __RADXAZERO_H__
+#define __RADXAZERO_H__
+
+#define CONFIG_DEVICE_PRODUCT		"radxa_zero"
+#define ODROID_BOARD_UUID		"ddb8c3f6-d94d-4394-b633-3134139cc2e0"
+
+/* configs for CEC */
+#define CONFIG_CEC_OSD_NAME		"Radxa Zero"
+#define CONFIG_CEC_WAKEUP
+
+#include "odroid-g12-common.h"
+
+#endif

2447140c66b1b2430f3c06be0a877678ea77b934
kelvin.zhang <kelvin.zhang@amlogic.com>
2021-10-19 18:12:39 +0800

    setup.sh: exclude products dir for CMakeLists.txt [1/1]
    PD#SWPL-61282
    
    Problem:
    Exclude products dir for CMakeLists.txt
    
    Solution:
    Exclude products dir for CMakeLists.txt
    
    Verify:
    ad401_a113l
    
    Signed-off-by: kelvin.zhang <kelvin.zhang@amlogic.com>
    Change-Id: I0cd7398488b6d79e2bce2d2b08668f03547b7b55

--- bl30/rtos_sdk/scripts/setup.sh
+++ bl30/rtos_sdk/scripts/setup.sh
@@ -5,6 +5,7 @@ prj_dir=`pwd`
 input="$prj_dir/.repo/manifests/default.xml"
 cmake_file="$prj_dir/CMakeLists.txt"
 kconfig_file="$prj_dir/Kconfig"
+exclude_dir="products"
 
 cat <<EOF > $cmake_file
 enable_language(C CXX ASM)
@@ -25,14 +26,14 @@ do
 	keyword=`echo "$line" | grep 'path=.* name=' | awk '{print $2}'`
 	if [ $keyword ]; then
 		repo_path=`echo ${keyword#*path=} | sed 's/\"//g'`
+		category=`dirname $repo_path`
 		# Generate root CMakeLists.txt
-		if [ -f $repo_path/CMakeLists.txt ]; then
+		if [ -f $repo_path/CMakeLists.txt ] && [ "$category" != "$exclude_dir" ]; then
 			echo "add_subdirectory($repo_path)" >> $cmake_file
 		fi
 
 		# Generate root Kconfig
 		if [ -f $repo_path/Kconfig ]; then
-			category=`dirname $repo_path`
 			if [ "$last_category" != "$category" ]; then
 				if [ $last_category ]; then
 					echo -e "endmenu\n" >> $kconfig_file

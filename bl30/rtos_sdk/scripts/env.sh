0b7d9c5c3e8f2dff36dfd70982e51ad46168e45c
kelvin.zhang <kelvin.zhang@amlogic.com>
2021-10-25 11:44:04 +0800

    env.sh: refactor env.sh [1/1]
    PD#SWPL-61282
    
    Problem:
    current env.sh only support interactive mode
    
    Solution:
    refactor env.sh to support non-interactive mode
    
    Verify:
    ad401_a113l
    
    Signed-off-by: kelvin.zhang <kelvin.zhang@amlogic.com>
    Change-Id: I0f0cc088153bc786cb77abbc8dc7555f3f3efa6e

--- bl30/rtos_sdk/scripts/env.sh
+++ bl30/rtos_sdk/scripts/env.sh
@@ -1,18 +1,53 @@
 #!/bin/bash
 
 ###############################################################
-# Function: choose board and product.
+# Function: choose board and product, set environment variables.
 ###############################################################
 
-echo "Available boards:"
-find $PWD/boards/ -mindepth 2 -maxdepth 2 -type d ! -name ".*" -exec basename {} \;
-read -p "Choose your board: " BOARD
+usage()
+{
+	echo "Usage: source $BASH_SOURCE [board_name] [product_name]"
+	echo "−h: display help."
+	echo ""
+}
 
-echo ""
+if [ -n "$1" ]; then
+	if [ $1 == "-h" ]; then
+		usage
+		return 0
+	else
+		BOARD=$1
+	fi
+else
+	unset BOARD
+fi
+if [ -z $BOARD ]; then
+	echo "Available boards:"
+	find $PWD/boards/ -mindepth 2 -maxdepth 2 -type d ! -name ".*" -exec basename {} \;
+	read -p "Choose your board: " BOARD
+	# Check BOARD
+	if [ -z "$(find $PWD/boards/ -mindepth 2 -maxdepth 2 -type d -name $BOARD)" ]; then
+		echo "No such board!"
+		return 1
+	fi
+fi
 
-echo "Available products:"
-find $PWD/products/ -mindepth 1 -maxdepth 1 -type d ! -name ".*" -exec basename {} \;
-read -p "Choose your product: " PRODUCT
+if [ -n "$2" ]; then
+	PRODUCT=$2
+else
+	unset PRODUCT
+	echo ""
+fi
+if [ -z $PRODUCT ]; then
+	echo "Available products:"
+	find $PWD/products/ -mindepth 1 -maxdepth 1 -type d ! -name ".*" -exec basename {} \;
+	read -p "Choose your product: " PRODUCT
+	# Check PRODUCT
+	if [ -z "$(find $PWD/products/ -mindepth 1 -maxdepth 1 -type d -name $PRODUCT)" ]; then
+		echo "No such product!"
+		return 1
+	fi
+fi
 
 ARCH=`find -name "*$BOARD" -exec dirname {} \; | xargs basename`
 
@@ -20,7 +55,7 @@ case $ARCH in
 	arm) COMPILER=gcc;TOOLCHAIN_KEYWORD="arm-none-eabi" ;;
 	arm64) COMPILER=gcc;TOOLCHAIN_KEYWORD="aarch64-none-elf" ;;
 	riscv) COMPILER=gcc;TOOLCHAIN_KEYWORD="riscv-none" ;;
-	*) echo "Failed to identify ARCH $ARCH" ;;
+	*) echo "Failed to identify ARCH $ARCH";return 1;;
 esac
 
 export ARCH BOARD COMPILER PRODUCT TOOLCHAIN_KEYWORD

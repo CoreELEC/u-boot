ab87e0d025806595b9a711da6176e2140aa633ae
binbin.wang <binbin.wang@amlogic.com>
2021-11-11 10:55:26 +0800

    bl30: t3: Add the function of adjusting voltage [1/2]
    PD#SWPL-64003
    
    Problem:
    Add the function of adjusting voltage based on corner information
    
    Solution:
    Add function.
    
    Verify:
    T3 pass
    
    Change-Id: Ie7a4fefda0da830958d55c2aa62cf70a3cd81d7d
    Signed-off-by: binbin.wang <binbin.wang@amlogic.com>

--- bl30/src_ao/demos/amlogic/n200/t3/t3_t965d4/power.c
+++ bl30/src_ao/demos/amlogic/n200/t3/t3_t965d4/power.c
@@ -193,11 +193,12 @@ void str_power_on(int shutdown_flag)
 	vTaskDelay(pdMS_TO_TICKS(200));
 }
 
+extern uint32_t get_vddee_voltage(int is_shutdown);
+extern uint32_t get_vddcpu_voltage(int is_shutdown);
 void str_power_off(int shutdown_flag)
 {
 	int ret;
-
-	shutdown_flag = shutdown_flag;
+	uint32_t vol;
 
 #ifndef CONFIG_ETH_WAKEUP
 	/***power off vcc3v3***/
@@ -210,12 +211,13 @@ void str_power_off(int shutdown_flag)
 		printf("vddcpu pwm get fail\n");
 		return;
 	}
-	ret = vPwmMesonsetvoltage(VDDCPU_VOLT,720);
+
+	ret = vPwmMesonsetvoltage(VDDCPU_VOLT,690);
 	if (ret < 0) {
 		printf("vddcpu pwm set fail\n");
 		return;
 	}
-	printf("vddcpu 720mv\n");
+	printf("vddcpu 690mv\n");
 
 	/***set vdd_ee val***/
 	vdd_ee = vPwmMesongetvoltage(VDDEE_VOLT);
@@ -224,12 +226,20 @@ void str_power_off(int shutdown_flag)
 		return;
 	}
 
-	ret = vPwmMesonsetvoltage(VDDEE_VOLT,770);
-	if (ret < 0) {
-		printf("vdd_EE pwm set fail\n");
-		return;
+	vol = get_vddee_voltage(shutdown_flag);
+	if (vol) {
+#ifndef CONFIG_ETH_WAKEUP
+		ret = vPwmMesonsetvoltage(VDDEE_VOLT,vol);
+		if (ret < 0) {
+			printf("vdd_EE pwm set fail\n");
+			return;
+		}
+#endif
+		printf("vddee %dmv\n", vol);
+	} else {
+		printf("No need to adjust voltage, vddee:%dmV\n",
+				vdd_ee);
 	}
-
 	/***power off vcc5v***/
 	power_off_vcc5v();
 }
--- bl30/src_ao/demos/amlogic/n200/t3/t3_t982/power.c
+++ bl30/src_ao/demos/amlogic/n200/t3/t3_t982/power.c
@@ -193,11 +193,13 @@ void str_power_on(int shutdown_flag)
 	vTaskDelay(pdMS_TO_TICKS(200));
 }
 
+extern uint32_t get_vddee_voltage(int is_shutdown);
+extern uint32_t get_vddcpu_voltage(int is_shutdown);
 void str_power_off(int shutdown_flag)
 {
 	int ret;
+	uint32_t vol;
 
-	shutdown_flag = shutdown_flag;
 #ifndef CONFIG_ETH_WAKEUP
 	/***power off vcc3v3***/
 	power_off_vcc3v3();
@@ -208,12 +210,13 @@ void str_power_off(int shutdown_flag)
 		printf("vddcpu pwm get fail\n");
 		return;
 	}
-	ret = vPwmMesonsetvoltage(VDDCPU_VOLT,720);
+
+	ret = vPwmMesonsetvoltage(VDDCPU_VOLT,690);
 	if (ret < 0) {
 		printf("vddcpu pwm set fail\n");
 		return;
 	}
-	printf("vddcpu 720mv\n");
+	printf("vddcpu 690mv\n");
 
 	/***set vdd_ee val***/
 	vdd_ee = vPwmMesongetvoltage(VDDEE_VOLT);
@@ -222,12 +225,21 @@ void str_power_off(int shutdown_flag)
 		return;
 	}
 
-	ret = vPwmMesonsetvoltage(VDDEE_VOLT,770);
-	if (ret < 0) {
-		printf("vdd_EE pwm set fail\n");
-		return;
+	vol = get_vddee_voltage(shutdown_flag);
+	if (vol) {
+#ifndef CONFIG_ETH_WAKEUP
+		ret = vPwmMesonsetvoltage(VDDEE_VOLT,vol);
+		if (ret < 0) {
+			printf("vdd_EE pwm set fail\n");
+			return;
+		}
+#endif
+		printf("vddee %dmv\n", vol);
+	} else {
+		printf("No need to adjust voltage, vddee:%dmV\n",
+				vdd_ee);
 	}
-
 	/***power off vcc5v***/
 	power_off_vcc5v();
 }
+

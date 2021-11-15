9a00590e230179ed30f68fa34248e60c5f170fb0
Xiaohu.Huang <xiaohu.huang@amlogic.com>
2021-11-15 18:18:35 +0800

    T5W/fip: Integrated that to get bl30.bin from rtos_sdk for T5W [2/2]
    PD#SWPL-61630
    
    Problem:
    Integrated the compiling bl30 source code from rtos_sdk.
    
    Solution:
    Modified that branch to compile rtos_sdk then get bl30.bin for t5w_skt.
    
    Verify:
    T5W_AT301/T5W_AT309
    
    Change-Id: Ieec4edc3b6472926e2e50578964624fe1595a5ba
    Signed-off-by: Xiaohu.Huang <xiaohu.huang@amlogic.com>

--- fip/build_bl30.sh
+++ fip/build_bl30.sh
@@ -3,26 +3,47 @@
 function build_bl30() {
 	# $1: src_folder, $2: bin_folder, $3: soc
 	# co-work with bl30 build script under bl30.git
-	echo -n "Build $3 bl30...Please wait..."
-	local target="$1/bl30.bin"
-	cd $1
-	#export CROSS_COMPILE=${AARCH32_TOOL_CHAIN}
-	local soc=$3
-	if [ "$soc" == "gxtvbb" ]; then
-		soc="gxtvb"
-	fi
-	#make clean BOARD=$soc &> /dev/null
-	#make BOARD=$soc &> /dev/null
-	if [[ "${BL30_SELECT}" != "" ]]; then
-		/bin/bash mk ${BL30_SELECT}
+	echo -n "Build $3 bl30 of ${BL30_SELECT}...Please wait..."
+	local tartget
+	if [[ "${BL30_SELECT}" == "t5w_at301" || "${BL30_SELECT}" == "t5w_skt" ]]; then
+		echo "Build bl30 from new RTOS SDK."
+		local current_dir=$(cd $(dirname $0); pwd)
+		local work_dir=${current_dir}/bl30/rtos_sdk
+		local output_dir
+		local board_arg
+		if [ "${BL30_SELECT}" == "t5w_at301" ]; then
+			board_arg=at301_t962d4
+		else
+			board_arg=at309_t962d4
+		fi
+		source $work_dir/scripts/env.sh $board_arg aocpu && make MANIFEST_FILE="t5w_aocpu_rtos_sdk.xml" -C $work_dir &&    \
+			output_dir=$work_dir/output/$BOARD-$PRODUCT/freertos
+		target=$output_dir/bl30.bin
+		cp -arf $output_dir/freertos.bin $output_dir/bl30.bin
 	else
-		/bin/bash mk $soc
+		echo "Build bl30 from src_ao."
+		target="$1/bl30.bin"
+		cd $1
+		#export CROSS_COMPILE=${AARCH32_TOOL_CHAIN}
+		local soc=$3
+		if [ "$soc" == "gxtvbb" ]; then
+			soc="gxtvb"
+		fi
+		#make clean BOARD=$soc &> /dev/null
+		#make BOARD=$soc &> /dev/null
+		if [[ "${BL30_SELECT}" != "" ]]; then
+			/bin/bash mk ${BL30_SELECT}
+		else
+			/bin/bash mk $soc
+		fi
 	fi
+
 	if [ $? != 0 ]; then
 		cd ${MAIN_FOLDER}
 		echo "Error: Build bl30 failed... abort"
 		exit -1
 	fi
+	echo "Copy target from $target to $2"
 	cd ${MAIN_FOLDER}
 	cp ${target} $2 -f
 	echo "done"

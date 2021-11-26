f8386d4e12a52eaa14c5086edf057df4d3fcb577
Xiaohu.Huang <xiaohu.huang@amlogic.com>
2021-11-26 10:52:33 +0800

    scirpts: Add config the board and product by index number supported. [1/1]
    PD#SWPL-61630
    
    Problem:
    Add config the board and product by index number supported.
    
    Solution:
    Add config the board and product by index number supported.
    
    Verify:
    T5W_AT301/T5W_AT309
    
    Change-Id: I9c4ab5b33f79d803be7f6b8830cd19ebc473d178
    Signed-off-by: Xiaohu.Huang <xiaohu.huang@amlogic.com>

--- bl30/rtos_sdk/scripts/env.sh
+++ bl30/rtos_sdk/scripts/env.sh
@@ -11,6 +11,31 @@ usage()
 	echo ""
 }
 
+# $1: path
+# $2: depth
+choose()
+{
+	echo "Available $1s:"
+	ARRAY=($(find $PWD/$1s/ -mindepth $2 -maxdepth $2 -type d ! -name ".*" | xargs basename -a))
+	for i in "${!ARRAY[@]}";
+	do
+		echo -e "\t$i. ${ARRAY[$i]}"
+	done
+	read -p "Choose your $1: " CHOICE
+	# Determine whether it is a digital number
+	expr $CHOICE + 1 > /dev/null 2>&1
+	if [ $? -eq 0 ]; then
+		RESULT=${ARRAY[$CHOICE]}
+	else
+		RESULT=$CHOICE
+	fi
+	# Check RESULT
+	if [ -z "$(find $PWD/$1s/ -mindepth $2 -maxdepth $2 -type d -name $RESULT)" ]; then
+		echo "No such $1!"
+		return 1
+	fi
+}
+
 if [ -n "$1" ]; then
 	if [ $1 == "-h" ]; then
 		usage
@@ -22,14 +47,9 @@ else
 	unset BOARD
 fi
 if [ -z $BOARD ]; then
-	echo "Available boards:"
-	find $PWD/boards/ -mindepth 2 -maxdepth 2 -type d ! -name ".*" -exec basename {} \;
-	read -p "Choose your board: " BOARD
-	# Check BOARD
-	if [ -z "$(find $PWD/boards/ -mindepth 2 -maxdepth 2 -type d -name $BOARD)" ]; then
-		echo "No such board!"
-		return 1
-	fi
+	choose board 2
+	[ $? -ne 0 ] && return 1;
+	BOARD=$RESULT
 fi
 
 if [ -n "$2" ]; then
@@ -39,14 +59,9 @@ else
 	echo ""
 fi
 if [ -z $PRODUCT ]; then
-	echo "Available products:"
-	find $PWD/products/ -mindepth 1 -maxdepth 1 -type d ! -name ".*" -exec basename {} \;
-	read -p "Choose your product: " PRODUCT
-	# Check PRODUCT
-	if [ -z "$(find $PWD/products/ -mindepth 1 -maxdepth 1 -type d -name $PRODUCT)" ]; then
-		echo "No such product!"
-		return 1
-	fi
+	choose product 1
+	[ $? -ne 0 ] && return 1;
+	PRODUCT=$RESULT
 fi
 
 ARCH=`find -not -path "./output/*" -name "*$BOARD" -exec dirname {} \; | xargs basename`

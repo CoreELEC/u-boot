898a448c20a318e7f456112209d69aacbe346249
Kelvin Zhang <kelvin.zhang@amlogic.com>
2021-12-24 20:59:18 +0800

    drivers_aocpu: init drivers_aocpu [1/1]
    PD#SWPL-66844
    
    Problem:
    init drivers_aocpu
    
    Solution:
    init drivers_aocpu
    
    Verify:
    N/A
    
    Signed-off-by: Kelvin Zhang <kelvin.zhang@amlogic.com>
    Change-Id: Iadc845929b1cead1375024a8ad4723334bad4015

---
+++ bl30/rtos_sdk/drivers_aocpu/ddr/ddr.h
@@ -0,0 +1,11 @@
+#ifndef __DDR_H__
+#define __DDR_H__
+
+#include <stdint.h>
+
+void vDDR_suspend(uint32_t st_f);
+
+void vDDR_resume(uint32_t st_f);
+
+#endif
+
---
+++ bl30/rtos_sdk/drivers_aocpu/get_version/version.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "version.h"
+
+int version(void)
+{
+	printf("AOCPU image version='%s'\n", VERSION);
+
+	return 0;
+}
+

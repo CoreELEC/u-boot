8e91925e896610ed40f378fb441775ffe9a2717a
Xiaohu.Huang <xiaohu.huang@amlogic.com>
2022-01-13 15:02:51 +0800

    boards/riscv: Add function definiton for riscv. [1/1]
    PD#SWPL-68240
    
    Problem:
    Refactor for all.
    
    Solution:
    Add function definiton for riscv.
    
    Verify:
    N/A
    
    Change-Id: Iea2f6d5a0ce54ed1a4a9ab3e305bef0904af4c85
    Signed-off-by: Xiaohu.Huang <xiaohu.huang@amlogic.com>

--- bl30/rtos_sdk/boards/riscv/at309_t962d4/power.h
+++ bl30/rtos_sdk/boards/riscv/at309_t962d4/power.h
@@ -4,7 +4,12 @@
  * SPDX-License-Identifier: MIT
  */
 
+#ifndef __POWER_H__
+#define __POWER_H__
+
 extern void str_hw_init(void);
 extern void str_hw_disable(void);
 extern void str_power_on(int shutdown_flag);
 extern void str_power_off(int shutdown_flag);
+
+#endif

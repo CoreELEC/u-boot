9bbf3c88aa5a333a9f3df54ac7e9da37410344ae
Xiaohu.Huang <xiaohu.huang@amlogic.com>
2022-01-17 10:41:38 +0800

    soc/riscv: Instead of use Kconfig items to compiler flags. [1/1]
    PD#SWPL-68240
    
    Problem:
    Refactor for all.
    
    Solution:
    Instead of use Kconfig items to compiler flags.
    
    Verify:
    N/A
    
    Change-Id: If43d62932f542ee9d0466047cc79198b2f693fa4
    Signed-off-by: Xiaohu.Huang <xiaohu.huang@amlogic.com>

--- bl30/rtos_sdk/soc/riscv/t5d/soc.h
+++ bl30/rtos_sdk/soc/riscv/t5d/soc.h
@@ -6,28 +6,6 @@
 
 #ifndef __SOC_H
 #define __SOC_H
-/*
-#ifndef __ASM
-#include "FreeRTOSConfig.h"
-#include "riscv_const.h"
-#include "irq.h"
-#include "register.h"
-#endif
-
-#define SOC_ECLIC_NUM_INTERRUPTS 32
-#define SOC_TIMER_FREQ           configRTC_CLOCK_HZ
-#define SOC_ECLIC_CTRL_ADDR      0x0C000000UL
-#define SOC_TIMER_CTRL_ADDR      0x02000000UL
-#define SOC_PMP_BASE             0xff100000UL
-#define SOC_LOCAL_SRAM_BASE      0x10000000UL
-#define SRAM_BEGIN               SOC_LOCAL_SRAM_BASE
-#define SRAM_SIZE                (0x20000)//(96*1024)
-#define SRAM_END                 (SRAM_BEGIN + SRAM_SIZE)
-#define IO_BASE                  0xff000000UL
-#define IO_SIZE                  0x00100000
-#define IO_BEGIN                 (IO_BASE)
-#define IO_END                   (IO_BASE + IO_SIZE)
-*/
 
 #define SOC_PIC_NUM_INTERRUPTS 112
 #define SOC_PIC_CTRL_ADDR 0x03000000

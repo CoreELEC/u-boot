2215090b756380c286f0a6e52333dda02a87d631
Xiaohu.Huang <xiaohu.huang@amlogic.com>
2022-02-11 17:49:09 +0800

    scripts: Fixed the lost params check messages echo. [1/1]
    PD#SWPL-68240
    
    Problem:
    The params check messages lost.
    
    Solution:
    Use check command execute result to replace to set -e.
    
    Verify:
    N/A
    
    Change-Id: Idf727b16e26f053fb62cc29bd531a87e699b676e
    Signed-off-by: Xiaohu.Huang <xiaohu.huang@amlogic.com>

--- bl30/rtos_sdk/scripts/build_all.sh
+++ bl30/rtos_sdk/scripts/build_all.sh
@@ -7,7 +7,7 @@
 
 # usage:./scripts/build_all.sh at rtos sdk root dir
 
-set -e
+BUILD_COMBINATION="$PWD/scripts/build_combination.txt"
 
 # Build and upload document
 if [[ "$SUBMIT_TYPE" == "daily" ]]; then
@@ -15,17 +15,19 @@ if [[ "$SUBMIT_TYPE" == "daily" ]]; then
 	cd output/docs/html; find -type f -exec curl --ftp-create-dirs -T {} ftp://platform:platform@10.68.11.163:2222/Documents/Ecosystem/RTOS/rtos-sdk/{} \;; cd -
 fi
 
-BUILD_COMBINATION="$PWD/scripts/build_combination.txt"
-
 # Build all projects
 i=0
 while IFS= read -r LINE; do
 	[[ "$i" -ne 0 ]] && echo ""
 	i=$((i+1))
 	source scripts/env.sh $LINE
+	[ "$?" -ne 0 ] && echo "Failed to source scripts/env.sh!" && exit 1
 	make distclean
+	[ "$?" -ne 0 ] && echo "Failed to make distclean!" && exit 2
 	make
+	[ "$?" -ne 0 ] && echo "Failed to make!" && exit 3
 	if [[ "$SUBMIT_TYPE" == "daily" ]]; then
 		source scripts/scp.sh
+		[ "$?" -ne 0 ] && echo "Failed to source scripts/scp.sh!" && exit 4
 	fi
 done < "$BUILD_COMBINATION"

7d71ab11695fa8cfdc96b87f6a837f4828f49de4
Portisch <hugo.portisch@yahoo.de>
2022-02-12 22:46:47 +0100

    board: add Radxa Zero 2 w400_v1

---
+++ include/configs/radxa_zero2.h
@@ -0,0 +1,15 @@
+#ifndef __RADXAZERO_H__
+#define __RADXAZERO_H__
+
+#define CONFIG_DEVICE_PRODUCT		"radxa_zero2"
+#define ODROID_BOARD_UUID		"ddb8c3f6-d94d-4394-b633-3134139cc2e0"
+
+/* configs for CEC */
+#define CONFIG_CEC_OSD_NAME		"Radxa Zero 2"
+#define CONFIG_CEC_WAKEUP
+
+#include "odroid-g12-common.h"
+
+#undef ETHERNET_EXTERNAL_PHY
+
+#endif

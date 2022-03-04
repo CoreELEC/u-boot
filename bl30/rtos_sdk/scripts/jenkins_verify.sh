280afbe43d3f7c3c956cc8848f6a67d7f2ee79a5
kelvin.zhang <kelvin.zhang@amlogic.com>
2022-03-04 09:43:01 +0800

    jenkins.sh: update jenkins script [3/3]
    PD#SWPL-67525
    
    Problem:
    update jenkins script
    
    Solution:
    update jenkins script
    
    Verify:
    ad401_a113l
    
    Signed-off-by: kelvin.zhang <kelvin.zhang@amlogic.com>
    Change-Id: Ia6e61953022f8772e7f6340e1cb1b01d279d35dc

--- bl30/rtos_sdk/scripts/jenkins_verify.sh
+++ bl30/rtos_sdk/scripts/jenkins_verify.sh
@@ -29,15 +29,93 @@ MATCH_PATTERN="projects/"
 BRANCH=${BRANCH_NAME#*${MATCH_PATTERN}}
 WORK_DIR=$BUILDCHECK_BASE_PATH/$PROJECT_NAME/$BRANCH
 
+LAST_MANIFEST_FILE="manifest_last.xml"
+CURRENT_MANIFEST_FILE="manifest.xml"
+DIFF_MANIFEST_FILE="updates.xml"
+
 #rm -rf $WORK_DIR
 if [ ! -d "$WORK_DIR" ]; then
 	echo -e "\n======== Downloading source code ========"
 	mkdir -p $WORK_DIR
 	cd $WORK_DIR
 	repo init -u ${MANIFEST_URL} -b ${MANIFEST_BRANCH} --repo-url=git://scgit.amlogic.com/tools/repo.git --no-repo-verify
+else
+	echo -e "\n======== Syncing source code ========"
+	cd $WORK_DIR
+	repo forall -c git reset -q --hard origin/$BRANCH_NAME
+	repo manifest -r -o $LAST_MANIFEST_FILE
 fi
 
-export PROJECT_NAME BRANCH_NAME SUBMIT_TYPE WORK_DIR GIT_CHERRY_PICK
+repo sync -cq -j8
+repo forall -c git reset -q --hard origin/$BRANCH_NAME
+repo manifest -r -o $CURRENT_MANIFEST_FILE
+echo -e "======== Done ========\n"
 
-cd $WORK_DIR
-./scripts/verify.sh
+if [ -f $LAST_MANIFEST_FILE ] && [ -f $CURRENT_MANIFEST_FILE ]; then
+	comm -23 <(sort $LAST_MANIFEST_FILE) <(sort $CURRENT_MANIFEST_FILE) > $DIFF_MANIFEST_FILE
+
+	if [ -s $DIFF_MANIFEST_FILE ]; then
+		echo "======== Recent Changes ========"
+
+		while IFS= read -r line
+		do
+			keyline=`echo "$line" | grep 'name=.* path='`
+
+			for keyword in $keyline; do
+				[[ $keyword == path=* ]] && repo_path=`echo ${keyword#*path=} | sed 's/\"//g'`
+				[[ $keyword == name=* ]] && repo_name=`echo ${keyword#*name=} | sed 's/\"//g'`
+				[[ $keyword == revision=* ]] && repo_version=`echo ${keyword#*revision=} | sed 's/\"//g'`
+			done
+
+			if [ -d "$repo_path" ]; then
+				pushd $repo_path > /dev/null
+				echo -e "\nProject $repo_name"
+				git log $repo_version..HEAD
+				popd > /dev/null
+			fi
+		done < $DIFF_MANIFEST_FILE
+	else
+		echo -e "======== Nothing changed since last build ========"
+	fi
+	rm -f $DIFF_MANIFEST_FILE
+fi
+
+if [ -n "$GERRIT_PROJECT" ] && [ -n "$GERRIT_PATCHSET_NUMBER" ] && [ -n "$GERRIT_CHANGE_NUMBER" ]; then
+	echo -e "\n======== Applying Gerrit patch $GERRIT_CHANGE_NUMBER on Project $GERRIT_PROJECT ========"
+	keyline=`grep "name=\"$GERRIT_PROJECT\"" $CURRENT_MANIFEST_FILE`
+
+	for keyword in $keyline; do
+		if [[ $keyword == path=* ]]; then
+			repo_path=`echo ${keyword#*path=} | sed 's/\"//g'`
+			break;
+		fi
+	done
+
+	if [ -d "$repo_path" ]; then
+		pushd $repo_path > /dev/null
+		l2=${GERRIT_CHANGE_NUMBER: -2}
+		git fetch ssh://scgit.amlogic.com:29418/${GERRIT_PROJECT} refs/changes/${l2}/${GERRIT_CHANGE_NUMBER}/${GERRIT_PATCHSET_NUMBER}
+		git cherry-pick FETCH_HEAD
+		if [ "$?" -ne 0 ]; then
+			echo -e "========= Applying patch failed! =========\n"
+			exit 1
+		fi
+		popd > /dev/null
+	else
+		echo "No such directory! $repo_path"
+		exit 1
+	fi
+	echo -e "======== Done ========\n"
+fi
+
+echo "GIT_CHERRY_PICK $GIT_CHERRY_PICK"
+
+echo "========= Building all projects ========"
+./scripts/build_all.sh > build.log 2>&1
+if [ "$?" -eq 0 ]; then
+	echo "======== Done ========"
+else
+	cat build.log
+	echo -e "\nAborted!"
+	exit 1
+fi

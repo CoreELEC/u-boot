235de90e82fd8c1a1da7ec55d7a20763e4b763e0
Xiaohu.Huang <xiaohu.huang@amlogic.com>
2022-03-12 22:47:14 +0800

    boards/riscv: Modifications for A5's AOCPU supported. [1/1]
    PD#SWPL-67429
    
    Problem:
    Modifications for A5's AOCPU supported.
    
    Solution:
    Modifications for A5's AOCPU supported.
    
    Verify:
    av409_a113x2
    
    Change-Id: Ie0d2a1a8dbb24dd4436e2dc5edc85a96cf1596cc
    Signed-off-by: Xiaohu.Huang <xiaohu.huang@amlogic.com>

---
+++ bl30/rtos_sdk/boards/riscv/av400_a113x2/hw_business.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2021-2022 Amlogic, Inc. All rights reserved.
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#ifndef __hW_BUSINESS_H__
+#define __hW_BUSINESS_H__
+
+void hw_business_process(void);
+
+#endif
+
---
+++ bl30/rtos_sdk/boards/riscv/av400_a113x2/power.h
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2021-2022 Amlogic, Inc. All rights reserved.
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+extern void str_hw_init(void);
+extern void str_hw_disable(void);
+extern void str_power_on(int shutdown_flag);
+extern void str_power_off(int shutdown_flag);
+

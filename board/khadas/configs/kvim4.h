b49b4d9b4d0c288da2636f2e70c07d7d7c939c12
Peter Vicman <peter.vicman@gmail.com>
2022-07-25 12:01:21 +0200

    VIM4: show Khadas logo from first partition

--- board/khadas/configs/kvim4.h
+++ board/khadas/configs/kvim4.h
@@ -356,7 +356,7 @@
 			"run recovery_from_flash_base;"\
 			"\0"\
         "load_bmp_logo="\
-            "if load mmc 0:2 ${loadaddr} /usr/share/fenix/logo/logo.bmp || load mmc 1:2 ${loadaddr} /usr/share/fenix/logo/logo.bmp || load mmc 1:3 ${loadaddr} /usr/share/fenix/logo/logo.bmp; then "\
+            "if load mmc 0:1 ${loadaddr} boot-logo-1080.bmp.gz || load mmc 1:1 ${loadaddr} boot-logo-1080.bmp.gz; then "\
                 "bmp display ${loadaddr};"\
                 "bmp scale;"\
             "fi;"\

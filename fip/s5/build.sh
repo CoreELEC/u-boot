cbef3a4d1eba33363e888a3dec0d6dbbe79f61b4
Bo Lv <bo.lv@amlogic.com>
2022-08-05 14:38:28 +0800

    fip: device acs.bin need reduce size to 8K [1/1]
    PD#SWPL-85437
    
    Problem:
    device acs.bin need reduce size to 8K
    
    Solution:
    1. device acs.bin reduce size to 8K
    2. chip acs.bin reduce size to 2K
    3. update acpu-imagetool from hongjun
    commit(d23cb63967e25649f915935544d66114ea3bfefa)
    
    Verify:
    verified on z1
    
    Change-Id: I7e8d2f01e0c3c961fa6570b2c0a343771c4f4248
    Signed-off-by: Bo Lv <bo.lv@amlogic.com>

--- fip/s5/build.sh
+++ fip/s5/build.sh
@@ -85,10 +85,10 @@ function mk_bl2ex() {
 	echo "================================================================="
 	echo "image packing with acpu-imagetool for bl2 bl2e bl2x"
 
-	dd if=/dev/zero of=${payload}/bl2.bin.sto bs=144288 count=1
+	dd if=/dev/zero of=${payload}/bl2.bin.sto bs=183200 count=1
 	dd if=${output}/bl2.bin.sto of=${payload}/bl2.bin.sto conv=notrunc
 
-	dd if=/dev/zero of=${payload}/bl2.bin.usb bs=144288 count=1
+	dd if=/dev/zero of=${payload}/bl2.bin.usb bs=183200 count=1
 	dd if=${output}/bl2.bin.usb of=${payload}/bl2.bin.usb conv=notrunc
 
 	dd if=/dev/zero of=${payload}/bl2e.bin.sto bs=65536 count=1
@@ -190,19 +190,19 @@ function mk_bl2ex() {
 	chip_acs_size=`stat -c %s ${INPUT_PARAMS}/chip_acs.bin`
 	dev_acs_size=`stat -c %s ${INPUT_PARAMS}/device_acs.bin`
 
-	if [ $chip_acs_size -gt 8192 ]; then
-		echo "chip acs size exceed limit 8192, $chip_acs_size"
+	if [ $chip_acs_size -gt 2048 ]; then
+		echo "chip acs size exceed limit 2048, $chip_acs_size"
 		exit -1
 	else
-		dd if=/dev/zero of=${payload}/chip_acs.bin bs=8192 count=1
+		dd if=/dev/zero of=${payload}/chip_acs.bin bs=2048 count=1
 		dd if=${INPUT_PARAMS}/chip_acs.bin of=${payload}/chip_acs.bin conv=notrunc
 	fi
 
-	if [ $dev_acs_size -gt 28672 ]; then
-		echo "chip acs size exceed limit 28672, $dev_acs_size"
+	if [ $dev_acs_size -gt 8192 ]; then
+		echo "dev acs size exceed limit 8192, $dev_acs_size"
 		exit -1
 	else
-		dd if=/dev/zero of=${payload}/device_acs.bin bs=28672 count=1
+		dd if=/dev/zero of=${payload}/device_acs.bin bs=8192 count=1
 		dd if=${INPUT_PARAMS}/device_acs.bin of=${payload}/device_acs.bin conv=notrunc
 	fi
 
@@ -212,8 +212,7 @@ function mk_bl2ex() {
 			--infile-bl2x-payload=${payload}/bl2x.bin \
 			--infile-dvinit-params=${payload}/device_acs.bin \
 			--infile-csinit-params=${payload}/chip_acs.bin \
-			--infile-ddr-fwdata=${payload}/ddrfw_data.bin \
-			--scs-family=p1 \
+			--scs-family=${CUR_SOC} \
 			--outfile-bb1st=${output}/bb1st.sto.bin \
 			--outfile-blob-bl2e=${output}/blob-bl2e.sto.bin \
 			--outfile-blob-bl2x=${output}/blob-bl2x.bin
@@ -224,8 +223,7 @@ function mk_bl2ex() {
 			--infile-bl2x-payload=${payload}/bl2x.bin \
 			--infile-dvinit-params=${payload}/device_acs.bin \
 			--infile-csinit-params=${payload}/chip_acs.bin \
-			--infile-ddr-fwdata=${payload}/ddrfw_data.bin \
-			--scs-family=p1 \
+			--scs-family=${CUR_SOC} \
 			--outfile-bb1st=${output}/bb1st.usb.bin \
 			--outfile-blob-bl2e=${output}/blob-bl2e.usb.bin \
 			--outfile-blob-bl2x=${output}/blob-bl2x.bin
@@ -604,7 +602,7 @@ function cleanup() {
 function encrypt_step() {
 	dbg "encrypt: $@"
 	local ret=0
-	./${FIP_FOLDER}${CUR_SOC}/aml_encrypt_p1 $@
+	./${FIP_FOLDER}${CUR_SOC}/aml_encrypt_${CUR_SOC} $@
 	ret=$?
 	if [ 0 != $ret ]; then
 		echo "Err! aml_encrypt_${CUR_SOC} return $ret"

cb6e67c6fc6f197651dcf0dc4a09f5dedda53def
Bo Lv <bo.lv@amlogic.com>
2022-08-30 20:04:28 +0800

    RTOS_SDK: rtos need rename for board config [1/1]
    PD#SWPL-91088
    
    Problem:
    rtos need rename board config
    
    Solution:
    add config for rtos build
    
    Verify:
    s5 ax201
    
    Change-Id: I745c0704decc0fb18e9aedfcd9ce6c3311fabdd2
    Signed-off-by: Bo Lv <bo.lv@amlogic.com>

---
+++ bl30/rtos_sdk/boards/riscv/ax208_s928x/power.c
@@ -0,0 +1,266 @@
+/*
+ * Copyright (c) 2021-2022 Amlogic, Inc. All rights reserved.
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#include <stdio.h>
+#include "FreeRTOS.h"
+#include "common.h"
+#include "gpio.h"
+#include "ir.h"
+#include "soc.h"
+#include "suspend.h"
+#include "task.h"
+#include "gpio.h"
+#include "pwm.h"
+#include "pwm_plat.h"
+#include "keypad.h"
+#include "timer_source.h"
+#include "vad_suspend.h"
+#include "wakeup.h"
+#include "power.h"
+#include "mailbox-api.h"
+
+/* #define CONFIG_ETH_WAKEUP */
+
+#ifdef CONFIG_ETH_WAKEUP
+#include "interrupt_control.h"
+#include "eth.h"
+#endif
+static int vdd_ee;
+static int vdd_cpu;
+static TaskHandle_t vadTask;
+
+static struct IRPowerKey prvPowerKeyList[] = {
+	{ 0xef10fe01, IR_NORMAL }, /* ref tv pwr */
+	{ 0xba45bd02, IR_NORMAL }, /* small ir pwr */
+	{ 0xef10fb04, IR_NORMAL }, /* old ref tv pwr */
+	{ 0xf20dfe01, IR_NORMAL },
+	{ 0xe51afb04, IR_NORMAL },
+	{ 0x3ac5bd02, IR_CUSTOM },
+	{}
+};
+
+static void vIRHandler(struct IRPowerKey *pkey)
+{
+	uint32_t buf[4] = { 0 };
+
+	if (pkey->type == IR_NORMAL)
+		buf[0] = REMOTE_WAKEUP;
+	else if (pkey->type == IR_CUSTOM)
+		buf[0] = REMOTE_CUS_WAKEUP;
+
+	/* do sth below  to wakeup*/
+	STR_Wakeup_src_Queue_Send_FromISR(buf);
+};
+
+static void *xMboxVadWakeup(void *msg)
+{
+	uint32_t buf[4] = { 0 };
+
+	buf[0] = VAD_WAKEUP;
+	STR_Wakeup_src_Queue_Send(buf);
+
+	return NULL;
+}
+
+void str_hw_init(void)
+{
+	int ret;
+	/*enable device & wakeup source interrupt*/
+	vIRInit(MODE_HARD_NEC, GPIOD_5, PIN_FUNC1, prvPowerKeyList, ARRAY_SIZE(prvPowerKeyList),
+		vIRHandler);
+#ifdef CONFIG_ETH_WAKEUP
+	vETHInit(IRQ_ETH_PMT_NUM, eth_handler);
+#endif
+
+	ret = xInstallRemoteMessageCallbackFeedBack(AODSPA_CHANNEL, MBX_CMD_VAD_AWE_WAKEUP,
+									xMboxVadWakeup, 0);
+	if (ret == MBOX_CALL_MAX)
+		printf("mbox cmd 0x%x register fail\n", MBX_CMD_VAD_AWE_WAKEUP);
+
+	vBackupAndClearGpioIrqReg();
+	vKeyPadInit();
+	vGpioIRQInit();
+}
+
+void str_hw_disable(void)
+{
+	/*disable wakeup source interrupt*/
+	vIRDeint();
+#ifdef CONFIG_ETH_WAKEUP
+	vETHDeint();
+#endif
+	xUninstallRemoteMessageCallback(AODSPA_CHANNEL, MBX_CMD_VAD_AWE_WAKEUP);
+
+	vKeyPadDeinit();
+	vRestoreGpioIrqReg();
+}
+
+void str_power_on(int shutdown_flag)
+{
+	int ret;
+
+	(void)shutdown_flag;
+
+	/* open PWM clk */
+	REG32(CLKCTRL_PWM_CLK_EF_CTRL) |= (1 << 24) | (1 << 8);
+
+	/* set GPIOE_1 pinmux to pwm */
+	xPinmuxSet(GPIOE_1, PIN_FUNC1);
+
+	/* enable vddcpu PWM channel */
+	REG32(PWMEF_MISC_REG_AB) |= (1 << 1);
+
+	/***set vdd_cpu val***/
+	ret = vPwmMesonsetvoltage(VDDCPU_VOLT, vdd_cpu);
+	if (ret < 0) {
+		printf("VDD_CPU pwm set fail\n");
+		return;
+	}
+
+	/***power on vdd_cpu***/
+	ret = xGpioSetDir(GPIO_TEST_N, GPIO_DIR_OUT);
+	if (ret < 0) {
+		printf("vdd_cpu set gpio dir fail\n");
+		return;
+	}
+
+	ret = xGpioSetValue(GPIO_TEST_N, GPIO_LEVEL_HIGH);
+	if (ret < 0) {
+		printf("vdd_cpu set gpio val fail\n");
+		return;
+	}
+
+	/***set vdd_ee val***/
+	ret = vPwmMesonsetvoltage(VDDEE_VOLT, vdd_ee);
+	if (ret < 0) {
+		printf("VDD_EE pwm set fail\n");
+		return;
+	}
+
+	if (shutdown_flag) {
+		/***power on vcc_3.3v***/
+		ret = xGpioSetDir(GPIOD_2, GPIO_DIR_OUT);
+		if (ret < 0) {
+			printf("vcc_3.3v set gpio dir fail\n");
+			return;
+		}
+
+		ret = xGpioSetValue(GPIOD_2, GPIO_LEVEL_HIGH);
+		if (ret < 0) {
+			printf("vcc_3.3v gpio val fail\n");
+			return;
+		}
+	}
+
+	/***power on vcc_5v***/
+	ret = xGpioSetDir(GPIOD_6, GPIO_DIR_OUT);
+	if (ret < 0) {
+		printf("vcc_5v set gpio dir fail\n");
+		return;
+	}
+
+	ret = xGpioSetValue(GPIOD_6, GPIO_LEVEL_HIGH);
+	if (ret < 0) {
+		printf("vcc_5v gpio val fail\n");
+		return;
+	}
+
+	/*Wait 200ms for VDDCPU statble*/
+	vTaskDelay(pdMS_TO_TICKS(200));
+
+	printf("vdd_cpu on\n");
+}
+
+void str_power_off(int shutdown_flag)
+{
+	int ret;
+
+	(void)shutdown_flag;
+
+	/***power off vcc_5v***/
+	ret = xGpioSetDir(GPIOD_6, GPIO_DIR_OUT);
+	if (ret < 0) {
+		printf("vcc_5v set gpio dir fail\n");
+		return;
+	}
+
+	ret = xGpioSetValue(GPIOD_6, GPIO_LEVEL_LOW);
+	if (ret < 0) {
+		printf("vcc_5v gpio val fail\n");
+		return;
+	}
+
+	if (shutdown_flag) {
+		/***power off vcc_3.3v***/
+		ret = xGpioSetDir(GPIOD_2, GPIO_DIR_OUT);
+		if (ret < 0) {
+			printf("vcc_3.3v set gpio dir fail\n");
+			return;
+		}
+
+		ret = xGpioSetValue(GPIOD_2, GPIO_LEVEL_LOW);
+		if (ret < 0) {
+			printf("vcc_3.3v gpio val fail\n");
+			return;
+		}
+	}
+
+	/***set vdd_ee val***/
+	vdd_ee = vPwmMesongetvoltage(VDDEE_VOLT);
+	if (vdd_ee < 0) {
+		printf("vdd_EE pwm get fail\n");
+		return;
+	}
+
+	ret = vPwmMesonsetvoltage(VDDEE_VOLT, 720);
+	if (ret < 0) {
+		printf("vdd_EE pwm set fail\n");
+		return;
+	}
+
+	/***set vdd_cpu val***/
+	vdd_cpu = vPwmMesongetvoltage(VDDCPU_VOLT);
+	if (vdd_ee < 0) {
+		printf("VDD_CPU pwm get fail\n");
+		return;
+	}
+
+	/***power off vdd_cpu***/
+	ret = xGpioSetDir(GPIO_TEST_N, GPIO_DIR_OUT);
+	if (ret < 0) {
+		printf("vdd_cpu set gpio dir fail\n");
+		return;
+	}
+
+	ret = xGpioSetValue(GPIO_TEST_N, GPIO_LEVEL_LOW);
+	if (ret < 0) {
+		printf("vdd_cpu set gpio val fail\n");
+		return;
+	}
+
+	/* set GPIOE_1 pinmux to gpio */
+	xPinmuxSet(GPIOE_1, PIN_FUNC0);
+
+	/***set vddcpu pwm to input***/
+	ret = xGpioSetDir(GPIOE_1, GPIO_DIR_IN);
+	if (ret < 0) {
+		printf("GPIOE_1 set gpio dir fail\n");
+		return;
+	}
+
+	/*disable PWM CLK*/
+	REG32(CLKCTRL_PWM_CLK_EF_CTRL) &= ~(1 << 24);
+
+	/* disable PWM channel */
+	REG32(PWMEF_MISC_REG_AB) &= ~(1 << 1);
+
+	if (shutdown_flag) {
+		/* disable sar adc */
+		vKeyPadDeinit();
+	}
+
+	printf("vdd_cpu off\n");
+}
---
+++ bl30/rtos_sdk/boards/riscv/ax209_s928x/keypad.c
@@ -0,0 +1,91 @@
+/*
+ * Copyright (c) 2021-2022 Amlogic, Inc. All rights reserved.
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include "FreeRTOS.h"
+#include "ir.h"
+#include "soc.h"
+#include "keypad.h"
+#include "gpio.h"
+#include "saradc.h"
+#include "suspend.h"
+#include "vad_suspend.h"
+
+/*KEY ID*/
+#define GPIO_KEY_ID_POWER GPIOD_3
+//#define GPIO_KEY_ID_WIFI_WAKE GPIOX_7
+
+#define ADC_KEY_ID_MENU 520
+#define ADC_KEY_ID_VOL_DEC 521
+#define ADC_KEY_ID_VOL_INC 522
+#define ADC_KEY_ID_ESC 523
+#define ADC_KEY_ID_HOME 524
+
+static void vGpioKeyCallBack(struct xReportEvent event)
+{
+	uint32_t buf[4] = { 0 };
+#ifdef KEYPAD_USED
+	switch (event.ulCode) {
+	case GPIO_KEY_ID_WIFI_WAKE:
+		buf[0] = POWER_KEY_WAKEUP;
+		STR_Wakeup_src_Queue_Send_FromISR(buf);
+		wakeup_dsp();
+		break;
+	default:
+		break;
+	}
+#endif
+	printf("GPIO key event 0x%x, key code %d, responseTicks %d\n", event.event, event.ulCode,
+	       event.responseTime);
+}
+
+static void vAdcKeyCallBack(struct xReportEvent event)
+{
+	uint32_t buf[4] = { 0 };
+
+	switch (event.ulCode) {
+	case ADC_KEY_ID_MENU:
+		buf[0] = POWER_KEY_WAKEUP;
+		STR_Wakeup_src_Queue_Send_FromISR(buf);
+		wakeup_dsp();
+		break;
+	default:
+		break;
+	}
+
+	printf("ADC key event 0x%x, key code %d, responseTime %d\n", event.event, event.ulCode,
+	       event.responseTime);
+}
+
+struct xGpioKeyInfo gpioKeyInfo[] = {
+	/*
+	 * GPIO_KEY_INFO(GPIO_KEY_ID_POWER, HIGH, EVENT_SHORT,
+	 *		vGpioKeyCallBack, NULL),
+	 */
+//	GPIO_KEY_INFO(GPIO_KEY_ID_WIFI_WAKE, HIGH, EVENT_SHORT, vGpioKeyCallBack, NULL)
+
+};
+
+struct xAdcKeyInfo adcKeyInfo[] = {
+	ADC_KEY_INFO(ADC_KEY_ID_MENU, 2048, SARADC_CH2, EVENT_SHORT, vAdcKeyCallBack, NULL),
+};
+
+void vKeyPadInit(void)
+{
+	vCreateAdcKey(adcKeyInfo, sizeof(adcKeyInfo) / sizeof(struct xAdcKeyInfo));
+	vAdcKeyEnable();
+	vCreateGpioKey(gpioKeyInfo, sizeof(gpioKeyInfo) / sizeof(struct xGpioKeyInfo));
+	vGpioKeyEnable();
+}
+
+void vKeyPadDeinit(void)
+{
+	vAdcKeyDisable();
+	vDestoryAdcKey();
+	vGpioKeyDisable();
+	vDestoryGpioKey();
+}

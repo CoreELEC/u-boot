ecf521efeee32ae06d217b60fc2a267d419efb22
Kelvin Zhang <kelvin.zhang@amlogic.com>
2022-09-15 11:18:34 +0800

    build.sh: improve build_all.sh [1/1]
    PD#SWPL-67525
    
    Problem:
    improve build_all.sh
    
    Solution:
    improve build_all.sh
    
    Verify:
    ad401_a113l
    
    Signed-off-by: Kelvin Zhang <kelvin.zhang@amlogic.com>
    Change-Id: I3be15f6e244ea3189e2f21345d53e52063e1c088

--- bl30/rtos_sdk/scripts/build_all.sh
+++ bl30/rtos_sdk/scripts/build_all.sh
@@ -7,10 +7,14 @@
 
 [ -z "$BUILD_LOG" ] && BUILD_LOG="output/build.log"
 
+# Clear build.log
+cat <<EOF > $BUILD_LOG
+EOF
+
 source scripts/publish.sh
 
 if [[ "$SUBMIT_TYPE" == "daily" ]] || [[ "$SUBMIT_TYPE" == "release" ]]; then
-	echo "======== Building document ========" | tee $BUILD_LOG
+	echo "======== Building document ========" | tee -a $BUILD_LOG
 		make docs >> $BUILD_LOG 2>&1
 		if [ -d $LOCAL_DOC_PATH ]; then
 			pushd $LOCAL_DOC_PATH >/dev/null
@@ -25,10 +29,6 @@ if [[ "$SUBMIT_TYPE" == "daily" ]] || [[ "$SUBMIT_TYPE" == "release" ]]; then
 			echo "$LOCAL_DOC_PATH not exist!"
 		fi
 	echo -e "======== Done ========\n" | tee -a $BUILD_LOG
-else
-# Clear Kconfig
-cat <<EOF > $BUILD_LOG
-EOF
 fi
 
 echo "======== Building all projects ========" | tee -a $BUILD_LOG

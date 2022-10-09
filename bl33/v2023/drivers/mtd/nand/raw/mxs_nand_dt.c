CoreELEC/u-boot

commit d05b830a4212e9094cadc4290e4a2f334a79b379

MXS_NAND: Add clock support for iMX8
Since iMX8 has enabled clock uclass, we can parse the clocks from DTB
and enable them in GPMI driver.

Signed-off-by: Ye Li <ye.li@nxp.com>
Signed-off-by: Peng Fan <peng.fan@nxp.com>

bl33/v2023/drivers/mtd/nand/raw/mxs_nand_dt.c
@@ -2,6 +2,8 @@
  * NXP GPMI NAND flash driver (DT initialization)
  *
  * Copyright (C) 2018 Toradex
+ * Copyright 2019 NXP
+ *
  * Authors:
  * Stefan Agner <stefan.agner@toradex.com>
  *
@@ -14,6 +16,7 @@
 #include <linux/io.h>
 #include <linux/ioport.h>
 #include <linux/printk.h>
+#include <clk.h>
 
 #include <mxs_nand.h>
 
@@ -90,6 +93,72 @@ static int mxs_nand_dt_probe(struct udevice *dev)
 	info->use_minimum_ecc = dev_read_bool(dev, "fsl,use-minimum-ecc");
 
 	info->legacy_bch_geometry = dev_read_bool(dev, "fsl,legacy-bch-geometry");
+
+	if (IS_ENABLED(CONFIG_CLK) && IS_ENABLED(CONFIG_IMX8)) {
+		/* Assigned clock already set clock */
+		struct clk gpmi_clk;
+
+		ret = clk_get_by_name(dev, "gpmi_io", &gpmi_clk);
+		if (ret < 0) {
+			debug("Can't get gpmi io clk: %d\n", ret);
+			return ret;
+		}
+
+		ret = clk_enable(&gpmi_clk);
+		if (ret < 0) {
+			debug("Can't enable gpmi io clk: %d\n", ret);
+			return ret;
+		}
+
+		ret = clk_get_by_name(dev, "gpmi_apb", &gpmi_clk);
+		if (ret < 0) {
+			debug("Can't get gpmi_apb clk: %d\n", ret);
+			return ret;
+		}
+
+		ret = clk_enable(&gpmi_clk);
+		if (ret < 0) {
+			debug("Can't enable gpmi_apb clk: %d\n", ret);
+			return ret;
+		}
+
+		ret = clk_get_by_name(dev, "gpmi_bch", &gpmi_clk);
+		if (ret < 0) {
+			debug("Can't get gpmi_bch clk: %d\n", ret);
+			return ret;
+		}
+
+		ret = clk_enable(&gpmi_clk);
+		if (ret < 0) {
+			debug("Can't enable gpmi_bch clk: %d\n", ret);
+			return ret;
+		}
+
+		ret = clk_get_by_name(dev, "gpmi_apb_bch", &gpmi_clk);
+		if (ret < 0) {
+			debug("Can't get gpmi_apb_bch clk: %d\n", ret);
+			return ret;
+		}
+
+		ret = clk_enable(&gpmi_clk);
+		if (ret < 0) {
+			debug("Can't enable gpmi_apb_bch clk: %d\n", ret);
+			return ret;
+		}
+
+		/* this clock is used for apbh_dma, since the apbh dma does not support DM,
+		  * we optionally enable it here
+		  */
+		ret = clk_get_by_name(dev, "gpmi_apbh_dma", &gpmi_clk);
+		if (ret < 0) {
+			debug("Can't get gpmi_apbh_dma clk: %d\n", ret);
+		} else {
+			ret = clk_enable(&gpmi_clk);
+			if (ret < 0) {
+				debug("Can't enable gpmi_apbh_dma clk: %d\n", ret);
+			}
+		}
+	}
 
 	return mxs_nand_init_ctrl(info);
 }

commit 98c0d54cd7640829226737cd33062a5b31b9d404

mtd: mxs_nand: get the clock with the right name
Rename the gpmi_apb_bch clock name to gpmi_bch_apb, as you can find in
the device tree.

Fixes: commit d05b830a4212e ("MXS_NAND: Add clock support for iMX8")
Signed-off-by: Dario Binacchi <dario.binacchi@amarulasolutions.com>
Reviewed-by: Michael Trimarchi <michael@amarulasolutions.com>

bl33/v2023/drivers/mtd/nand/raw/mxs_nand_dt.c
@@ -132,15 +132,15 @@ static int mxs_nand_dt_probe(struct udevice *dev)
 			return ret;
 		}
 
-		ret = clk_get_by_name(dev, "gpmi_apb_bch", &gpmi_clk);
+		ret = clk_get_by_name(dev, "gpmi_bch_apb", &gpmi_clk);
 		if (ret < 0) {
-			debug("Can't get gpmi_apb_bch clk: %d\n", ret);
+			debug("Can't get gpmi_bch_apb clk: %d\n", ret);
 			return ret;
 		}
 
 		ret = clk_enable(&gpmi_clk);
 		if (ret < 0) {
-			debug("Can't enable gpmi_apb_bch clk: %d\n", ret);
+			debug("Can't enable gpmi_bch_apb clk: %d\n", ret);
 			return ret;
 		}
 	}
bcb84ff801c46b7384524a907fe8870375c78f57
xiaohu.huang <xiaohu.huang@amlogic.com>
2022-11-03 15:50:25 +0800

    libc/aml: Add symbols in AML libc for aml_isp module [2/2]
    PD#SWPL-100419
    
    Problem:
    add putchar function for arm64
    
    Solution:
    add putchar function for arm64
    
    Verify:
    N/A
    
    Change-Id: I270b56d1a26aec026a65124846207180594352d9
    Signed-off-by: xiaohu.huang <xiaohu.huang@amlogic.com>

--- bl30/rtos_sdk/lib/libc/aml/aml_putchar.c
+++ bl30/rtos_sdk/lib/libc/aml/aml_putchar.c
@@ -5,11 +5,19 @@
  */
 
 #include "aml_putchar.h"
+#if (1 == CONFIG_ARM64)
+#include "serial.h"
+#else
 #include "uart.h"
+#endif
 
 int putchar(int c)
 {
+#if (1 == CONFIG_ARM64)
+	vSerialPutChar(ConsoleSerial, c);
+#else
 	vUartPutc(c);
+#endif
 
 	return 0;
 }

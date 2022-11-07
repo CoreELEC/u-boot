f5db2193a40163b7cd3a78cbf8f3343eefd5564e
Portisch <hugo.portisch@yahoo.de>
2022-11-07 15:23:40 +0100

    gpio: add power user gpio support

--- bl30/src_ao/demos/amlogic/include/suspend.h
+++ bl30/src_ao/demos/amlogic/include/suspend.h
@@ -68,6 +68,7 @@ void vCLK_resume(uint32_t st_f);
 void vDSP_suspend(uint32_t st_f);
 void vDSP_resume(uint32_t st_f);
 int get_USB_Power_flag(void);
+uint32_t get_User_Gpio(void);
 extern void create_str_task(void);
 extern void STR_Start_Sem_Give_FromISR(void);
 extern void STR_Start_Sem_Give(void);

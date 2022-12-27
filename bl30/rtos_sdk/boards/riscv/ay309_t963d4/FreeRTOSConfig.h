983cba566bd7d644d934ac35ad6301757e1b2aad
bangzheng.liu <bangzheng.liu@amlogic.com>
2022-12-27 12:03:51 +0800

    T5M: change AOCPU internal timer frequency [1/1]
    PD#SWPL-106479
    
    Problem:
    AOCPU internal timer frequency 1M.
    
    Solution:
    Modify AOCPU internal timer frequency to 24M.
    
    Verify:
    Local test AOCPU schedule ok.
    
    Change-Id: I1efe7fc6ec1815978377b32b8bf126593c1130e3
    Signed-off-by: bangzheng.liu <bangzheng.liu@amlogic.com>

--- bl30/rtos_sdk/boards/riscv/ay309_t963d4/FreeRTOSConfig.h
+++ bl30/rtos_sdk/boards/riscv/ay309_t963d4/FreeRTOSConfig.h
@@ -19,7 +19,7 @@
 #define configUSE_PREEMPTION 1
 #define configUSE_PORT_OPTIMISED_TASK_SELECTION 0
 #define configUSE_TICKLESS_IDLE 0
-#define configCPU_CLOCK_HZ 1000000 //24000000
+#define configCPU_CLOCK_HZ 24000000
 #define configTICK_RATE_HZ 50
 #define configMAX_PRIORITIES 4
 #define configMINIMAL_STACK_SIZE 450

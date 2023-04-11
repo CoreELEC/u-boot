74bbc350c0a8647bd81aa001305b0528cb972cd0
Bo Lv <bo.lv@amlogic.com>
2023-04-11 20:32:09 +0800

    bl33: s1a bootloader bringup [5/6]
    PD#SWPL-118020
    
    Problem:
    s1a bootloader bringup
    
    Solution:
    add bl33 code for bringup
    
    Verify:
    z1
    
    Change-Id: I4e8e98bd9b50353961e817b0037a9edc26cc9c88
    Signed-off-by: Bo Lv <bo.lv@amlogic.com>

--- bl33/v2023/boot/image.c
+++ bl33/v2023/boot/image.c
@@ -245,11 +245,16 @@ int image_check_hcrc(const struct legacy_img_hdr *hdr)
 
 int image_check_dcrc(const struct legacy_img_hdr *hdr)
 {
+#ifdef CONFIG_PXP_EMULATOR
+	puts("PXP skip CRC...");
+	return 1;
+#else
 	ulong data = image_get_data(hdr);
 	ulong len = image_get_data_size(hdr);
 	ulong dcrc = crc32_wd(0, (unsigned char *)data, len, CHUNKSZ_CRC32);
 
 	return (dcrc == image_get_dcrc(hdr));
+#endif
 }
 
 /**

07e319c71ba9ef6848683b919ba1bbfef214f8ad
Feng Chen <feng.chen@amlogic.com>
2023-04-19 14:48:09 +0800

    spicc: a4: new spicc controller driver support [1/1]
    PD#SWPL-117794
    
    Problem:
    uboot v2023 bringup on a4.
    
    Solution:
    add new spicc controller support.
    
    Verify:
    a4_ba409
    
    Change-Id: Ic2b521c5707041a5c47cfe58295112c97512a4fd
    Signed-off-by: Feng Chen <feng.chen@amlogic.com>

--- bl33/v2023/cmd/spi.c
+++ bl33/v2023/cmd/spi.c
@@ -14,12 +14,24 @@
 #include <errno.h>
 #include <spi.h>
 
+#if CONFIG_IS_ENABLED(AMLOGIC_MODIFY)
+#include <linux/compat.h>
+#endif
 /*-----------------------------------------------------------------------
  * Definitions
  */
 
 #ifndef MAX_SPI_BYTES
 #   define MAX_SPI_BYTES 32	/* Maximum number of bytes we can handle */
+#endif
+
+#if CONFIG_IS_ENABLED(AMLOGIC_MODIFY)
+#ifndef CONFIG_DEFAULT_SPI_BUS
+#   define CONFIG_DEFAULT_SPI_BUS	0
+#endif
+#ifndef CONFIG_DEFAULT_SPI_MODE
+#   define CONFIG_DEFAULT_SPI_MODE	SPI_MODE_0
+#endif
 #endif
 
 /*
@@ -30,14 +42,31 @@ static unsigned int	cs;
 static unsigned int	mode;
 static unsigned int	freq;
 static int		bitlen;
+
+#if !CONFIG_IS_ENABLED(AMLOGIC_MODIFY)
 static uchar		dout[MAX_SPI_BYTES];
 static uchar		din[MAX_SPI_BYTES];
+#else
+static int		wordlen = 8;
+static unsigned int	xfer_flags;
+struct spi_slave	*g_slave;
+#define XFERS_MAX	8
+static uchar		*buf_list[XFERS_MAX];
+static int		buf_cnt;
+static uchar		*dout;
+static uchar		*din;
+#endif
 
 static int do_spi_xfer(int bus, int cs)
 {
 	struct spi_slave *slave;
 	int ret = 0;
-
+#if CONFIG_IS_ENABLED(AMLOGIC_MODIFY)
+	if (g_slave && !(xfer_flags & SPI_XFER_BEGIN)) {
+		slave = g_slave;
+		goto next_xfer;
+	}
+#endif
 #if CONFIG_IS_ENABLED(DM_SPI)
 	char name[30], *str;
 	struct udevice *dev;
@@ -58,11 +87,22 @@ static int do_spi_xfer(int bus, int cs)
 	}
 #endif
 
+#if CONFIG_IS_ENABLED(AMLOGIC_MODIFY)
+	g_slave = slave;
+	slave->wordlen = wordlen;
+#endif
 	ret = spi_claim_bus(slave);
 	if (ret)
 		goto done;
+#if	!CONFIG_IS_ENABLED(AMLOGIC_MODIFY)
 	ret = spi_xfer(slave, bitlen, dout, din,
-		       SPI_XFER_BEGIN | SPI_XFER_END);
+				   SPI_XFER_BEGIN | SPI_XFER_END);
+#else
+next_xfer:
+	ret = spi_xfer(slave, bitlen, dout, din, xfer_flags);
+	if (ret > 0)
+		return 0;
+#endif
 #if !CONFIG_IS_ENABLED(DM_SPI)
 	/* We don't get an error code in this case */
 	if (ret)
@@ -78,6 +118,12 @@ static int do_spi_xfer(int bus, int cs)
 		printf("\n");
 	}
 done:
+#if CONFIG_IS_ENABLED(AMLOGIC_MODIFY)
+	g_slave = 0;
+	for (int i = 0; i < buf_cnt; i++)
+		kfree(buf_list[i]);
+	buf_cnt = 0;
+#endif
 	spi_release_bus(slave);
 #if !CONFIG_IS_ENABLED(DM_SPI)
 	spi_free_slave(slave);
@@ -125,9 +171,30 @@ int do_spi(struct cmd_tbl *cmdtp, int flag, int argc, char *const argv[])
 				mode = dectoul(cp + 1, &cp);
 			if (*cp == '@')
 				freq = dectoul(cp + 1, &cp);
+#if CONFIG_IS_ENABLED(AMLOGIC_MODIFY)
+			if (*cp == '.')
+				wordlen = simple_strtoul(cp + 1, &cp, 10);
+			if (*cp == '.')
+				xfer_flags = simple_strtoul(cp + 1, &cp, 16);
+			else
+				xfer_flags = SPI_XFER_BEGIN | SPI_XFER_END;
+#endif
 		}
+#if !CONFIG_IS_ENABLED(AMLOGIC_MODIFY)
 		if (argc >= 3)
 			bitlen = dectoul(argv[2], NULL);
+#else
+		if (argc >= 3) {
+			bitlen = dectoul(argv[2], NULL);
+			dout = kcalloc(ALIGN(bitlen / 8, 8), 2, GFP_KERNEL);
+			if (!dout)
+				return -ENOMEM;
+			din = dout + ALIGN(bitlen / 8, 8);
+			buf_list[buf_cnt] = dout;
+			if (++buf_cnt >= XFERS_MAX)
+				xfer_flags |= SPI_XFER_END;
+		}
+#endif
 		if (argc >= 4) {
 			cp = argv[3];
 			for(j = 0; *cp; j++, cp++) {

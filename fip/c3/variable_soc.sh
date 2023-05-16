201ab99633a5d3286eb925de365c30614ca2aa9e
Meng yu <meng.yu@amlogic.com>
2023-05-16 15:59:18 +0800

    fip: add bl31/bl32 top directory [2/2]
    PD#SWPL-123810
    
    Problem:
    bl31/bl32 too many directories
    
    Solution:
    add bl31/bl32 top directory in bootloader
    
    Verify:
    compile pass in local
    
    Change-Id: I32ade2176e6a88d92ed9847cba5157ae391b5c12
    Signed-off-by: Meng yu <meng.yu@amlogic.com>

--- fip/c3/variable_soc.sh
+++ fip/c3/variable_soc.sh
@@ -24,8 +24,8 @@ declare -a BLX_SRC_FOLDER=("bl2/core"		\
 			   "bl2/ree"		\
 			   "bl2/ree"		\
 			   "bl2/tee"		\
-			   "bl31_1.3/src"	\
-			   "bl32_3.8/src"	\
+			   "bl31/bl31_1.3/src"	\
+			   "bl32/bl32_3.8/src"	\
 			   "NULL"		\
 			   "bl33")
 
@@ -34,8 +34,8 @@ declare -a BLX_BIN_FOLDER=("bl2/bin"		\
 			   "bl2/bin"		\
 			   "bl2/bin"		\
 			   "bl2/bin"		\
-			   "bl31_1.3/bin"	\
-			   "bl32_3.8/bin"	\
+			   "bl31/bl31_1.3/bin"	\
+			   "bl32/bl32_3.8/bin"	\
 			   "bl40/bin")
 
 if [ "y" == "${CONFIG_PXP_NO_SIGNED}" ]; then

979cf329cb90ae0a6ee597ae5e84d0d1cd188f9c
bangzheng.liu <bangzheng.liu@amlogic.com>
2023-05-24 17:15:19 +0800

    TXHD2: bl30 add txhd2 board be301, be309, be311 and be319 [1/1]
    PD#SWPL-125307
    
    Problem:
    bl30 txhd2 board is only txhd2_skt.
    
    Solution:
    bl30 add txhd2 board be301, be309, be311 and be319.
    
    Verify:
    local build all board pass.
    
    Change-Id: I683872dbe5d35827d9bd9fead4deae4a875e95ec
    Signed-off-by: bangzheng.liu <bangzheng.liu@amlogic.com>

---
+++ bl30/rtos_sdk/boards/riscv/be311_t950s/leds_plat.h
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2021-2022 Amlogic, Inc. All rights reserved.
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#ifndef _MESON_LEDS_PLAT_H_
+#define _MESON_LEDS_PLAT_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+/*
+ * pwm t5d leds config
+ */
+#include <leds_state.h>
+#include <pwm_plat.h>
+
+#define LED_BREATH_MAX_COUNT 4
+
+extern struct LedDevice MesonLeds[];
+
+enum led_id {
+	LED_ID_0 = 0,
+	LED_ID_MAX,
+};
+
+int32_t get_led_breath_len(uint32_t breath_id);
+int32_t vLedPinmuxInit(void);
+int32_t vLedPlatInit(int32_t **stickmem);
+
+#ifdef __cplusplus
+}
+#endif
+#endif /* _MESON_LED_PLAT_H_ */

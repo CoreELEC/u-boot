ec87243637a1b38b290d5e211cb6535cc6cef463
Bo Lv <bo.lv@amlogic.com>
2023-06-27 16:32:31 +0800

    bl33: 2023: refine board env [1/1]
    PD#SWPL-129410
    
    Problem:
    board env need refine
    
    Solution:
    fix it
    
    Verify:
    s4
    
    Change-Id: I14dc85ffb789f9c50bb74f1b944f9dd67490005c
    Signed-off-by: Bo Lv <bo.lv@amlogic.com>

--- bl33/v2023/board/amlogic/configs/s4_ap222.h
+++ bl33/v2023/board/amlogic/configs/s4_ap222.h
@@ -7,13 +7,12 @@
 #define __BOARD_CFG_H__
 
 #include <asm/amlogic/arch/cpu.h>
-#include <amlogic/base_env.h>
 
 /*
  * platform power init config
  */
 
-#define AML_VCCK_INIT_VOLTAGE	  799	    //VCCK power up voltage
+#define AML_VCCK_INIT_VOLTAGE	  1009	    //VCCK power up voltage
 #define AML_VDDEE_INIT_VOLTAGE    800       // VDDEE power up voltage
 /*Distinguish whether to use efuse to adjust vddee*/
 //#define CONFIG_PDVFS_ENABLE

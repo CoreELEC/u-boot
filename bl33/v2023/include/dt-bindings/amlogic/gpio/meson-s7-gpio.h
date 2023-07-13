61798f8c2a5e97bbce111057b9880f4bfba3c07f
Huqiang Qin <huqiang.qin@amlogic.com>
2023-07-13 10:30:04 +0800

    pinctrl: support gpio/pinmux for s7 [1/1]
    PD#SWPL-131121
    
    Problem:
    S7 PxP Bringup.
    
    Solution:
    S7 PxP Bringup.
    
    Verify:
    PxP
    
    Change-Id: I03ef1355d933f14abe5b4c748b8666ef1990689f
    Signed-off-by: Huqiang Qin <huqiang.qin@amlogic.com>

--- bl33/v2023/include/dt-bindings/amlogic/gpio/meson-s7-gpio.h
+++ bl33/v2023/include/dt-bindings/amlogic/gpio/meson-s7-gpio.h
@@ -1,103 +1,99 @@
 /* SPDX-License-Identifier: (GPL-2.0+ OR MIT) */
 /*
- * Copyright (c) 2023 Amlogic, Inc. All rights reserved.
+ * Copyright (c) 2019 Amlogic, Inc. All rights reserved.
  */
 
 #ifndef _DT_BINDINGS_MESON_S7_GPIO_H
 #define _DT_BINDINGS_MESON_S7_GPIO_H
 
-/* GPIOAO */
-#define GPIOAO_0			0
-#define GPIOAO_1			1
-#define GPIOAO_2			2
-#define GPIOAO_3			3
-#define GPIOAO_4			4
-#define GPIOAO_5			5
-#define GPIOAO_6			6
+#define	GPIOE_0		0
+#define	GPIOE_1		1
 
-#define GPIO_TEST_N			7
+#define	GPIOB_0		2
+#define	GPIOB_1		3
+#define	GPIOB_2		4
+#define	GPIOB_3		5
+#define	GPIOB_4		6
+#define	GPIOB_5		7
+#define	GPIOB_6		8
+#define	GPIOB_7		9
+#define	GPIOB_8		10
+#define	GPIOB_9		11
+#define	GPIOB_10	12
+#define	GPIOB_11	13
+#define	GPIOB_12	14
+#define	GPIOB_13	15
 
-/* GPIOE */
-#define GPIOE_0				0
-#define GPIOE_1				1
+#define	GPIOC_0		16
+#define	GPIOC_1		17
+#define	GPIOC_2		18
+#define	GPIOC_3		19
+#define	GPIOC_4		20
+#define	GPIOC_5		21
+#define	GPIOC_6		22
+#define	GPIOC_7		23
 
-/* GPIOD */
-#define GPIOD_0				2
-#define GPIOD_1				3
-#define GPIOD_2				4
-#define GPIOD_3				5
-#define GPIOD_4				6
-#define GPIOD_5				7
-#define GPIOD_6				8
-#define GPIOD_7				9
-#define GPIOD_8				10
-#define GPIOD_9				11
-#define GPIOD_10			12
-#define GPIOD_11			13
-#define GPIOD_12			14
-#define GPIOD_13			15
-#define GPIOD_14			16
-#define GPIOD_15			17
+#define	GPIOD_0		24
+#define	GPIOD_1		25
+#define	GPIOD_2		26
+#define	GPIOD_3		27
+#define	GPIOD_4		28
+#define	GPIOD_5		29
+#define	GPIOD_6		30
+#define	GPIOD_7		31
+#define	GPIOD_8		32
+#define	GPIOD_9		33
+#define	GPIOD_10	34
+#define	GPIOD_11	35
 
-/* GPIOB */
-#define GPIOB_0				18
-#define GPIOB_1				19
-#define GPIOB_2				20
-#define GPIOB_3				21
-#define GPIOB_4				22
-#define GPIOB_5				23
-#define GPIOB_6				24
-#define GPIOB_7				25
-#define GPIOB_8				26
-#define GPIOB_9				27
-#define GPIOB_10			28
-#define GPIOB_11			29
-#define GPIOB_12			30
-#define GPIOB_13			31
+#define	GPIOH_0		36
+#define	GPIOH_1		37
+#define	GPIOH_2		38
+#define	GPIOH_3		39
+#define	GPIOH_4		40
+#define	GPIOH_5		41
+#define	GPIOH_6		42
+#define	GPIOH_7		43
+#define	GPIOH_8 	44
+#define	GPIOH_9 	45
+#define	GPIOH_10	46
+#define	GPIOH_11	47
 
-/* GPIOX */
-#define GPIOX_0				32
-#define GPIOX_1				33
-#define GPIOX_2				34
-#define GPIOX_3				35
-#define GPIOX_4				36
-#define GPIOX_5				37
-#define GPIOX_6				38
-#define GPIOX_7				39
-#define GPIOX_8				40
-#define GPIOX_9				41
-#define GPIOX_10			42
-#define GPIOX_11			43
-#define GPIOX_12			44
-#define GPIOX_13			45
-#define GPIOX_14			46
-#define GPIOX_15			47
-#define GPIOX_16			48
-#define GPIOX_17			49
+#define	GPIOX_0		48
+#define	GPIOX_1		49
+#define	GPIOX_2		50
+#define	GPIOX_3		51
+#define	GPIOX_4		52
+#define	GPIOX_5		53
+#define	GPIOX_6		54
+#define	GPIOX_7		55
+#define	GPIOX_8		56
+#define	GPIOX_9		57
+#define	GPIOX_10	58
+#define	GPIOX_11	59
+#define	GPIOX_12	60
+#define	GPIOX_13	61
+#define	GPIOX_14	62
+#define	GPIOX_15	63
+#define	GPIOX_16	64
+#define	GPIOX_17	65
+#define	GPIOX_18	66
+#define	GPIOX_19	67
 
-/* GPIOT */
-#define GPIOT_0				50
-#define GPIOT_1				51
-#define GPIOT_2				52
-#define GPIOT_3				53
-#define GPIOT_4				54
-#define GPIOT_5				55
-#define GPIOT_6				56
-#define GPIOT_7				57
-#define GPIOT_8				58
-#define GPIOT_9				59
-#define GPIOT_10			60
-#define GPIOT_11			61
-#define GPIOT_12			62
-#define GPIOT_13			63
-#define GPIOT_14			64
-#define GPIOT_15			65
-#define GPIOT_16			66
-#define GPIOT_17			67
-#define GPIOT_18			68
-#define GPIOT_19			69
-#define GPIOT_20			70
-#define GPIOT_21			71
-#define GPIOT_22			72
+#define	GPIOZ_0		68
+#define	GPIOZ_1		69
+#define	GPIOZ_2		70
+#define	GPIOZ_3		71
+#define	GPIOZ_4		72
+#define	GPIOZ_5		73
+#define	GPIOZ_6		74
+#define	GPIOZ_7		75
+#define	GPIOZ_8		76
+#define	GPIOZ_9		77
+#define	GPIOZ_10	78
+#define	GPIOZ_11	79
+#define	GPIOZ_12	80
 
-#endif
+#define	GPIO_TEST_N	81
+
+#endif /* _DT_BINDINGS_MESON_S7_GPIO_H */

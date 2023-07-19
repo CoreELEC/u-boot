3b096795d719d6429b5f63e173172eec4cd8c059
bangzheng.liu <bangzheng.liu@amlogic.com>
2023-07-19 15:59:33 +0800

    S1A: bl30 add s1a board bg201, bg209 [1/1]
    PD#SWPL-132594
    
    Problem:
    bl30 s1a board is only s1a_skt.
    
    Solution:
    bl30 add s1a board bg201, bg209.
    
    Verify:
    local build all board pass.
    
    Change-Id: I4ec1a85037eecc577f2ee2eee7bc6717fe9117f8
    Signed-off-by: bangzheng.liu <bangzheng.liu@amlogic.com>

---
+++ bl30/rtos_sdk/boards/riscv/bg201_s805c1/power.h
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2021-2022 Amlogic, Inc. All rights reserved.
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#ifndef __POWER_H__
+#define __POWER_H__
+
+void str_hw_init(void);
+
+void str_hw_disable(void);
+
+void str_power_on(int shutdown_flag);
+
+void str_power_off(int shutdown_flag);
+
+void Bt_GpioIRQRegister(void);
+
+void Bt_GpioIRQFree(void);
+
+
+#endif
+

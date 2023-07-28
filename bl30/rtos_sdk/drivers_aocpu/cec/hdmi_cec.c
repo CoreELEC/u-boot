5c9e2e414b85cf76e6c121294208398d1611a1bb
hang cheng <hang.cheng@amlogic.com>
2023-07-28 12:05:57 +0800

    cec: enable function for txhd2 [3/3]
    PD#SWPL-133731
    
    Problem:
    need to enable CEC function for txhd2
    
    Solution:
    enable cec function for txhd2
    
    Verify:
    be301
    
    Change-Id: I7931029fff811367f5d744ff85550861077aeeec
    Signed-off-by: hang cheng <hang.cheng@amlogic.com>

--- bl30/rtos_sdk/drivers_aocpu/cec/hdmi_cec.c
+++ bl30/rtos_sdk/drivers_aocpu/cec/hdmi_cec.c
@@ -394,6 +394,39 @@ unsigned int cec_reg_tab[] = {
 };
 #endif
 
+#ifdef CEC_CHIP_SEL_TXHD2
+/* based on T5M, register table enum cec_reg_idx*/
+unsigned int cec_reg_tab[] = {
+	0xffff, /*AO_CEC_CLK_CNTL_REG0*/
+	0xffff, /*AO_CEC_CLK_CNTL_REG1*/
+	0xffff, /*AO_CEC_GEN_CNTL*/
+	0xffff, /*AO_CEC_RW_REG*/
+	0xffff, /*AO_CEC_INTR_MASKN*/
+	0xffff, /*AO_CEC_INTR_CLR*/
+	0xffff, /*AO_CEC_INTR_STAT*/
+
+	AO_CECB_CLK_CNTL_REG0,
+	AO_CECB_CLK_CNTL_REG1,
+	AO_CECB_GEN_CNTL,
+	AO_CECB_RW_REG,
+	AO_CECB_INTR_MASKN,
+	AO_CECB_INTR_CLR,
+	AO_CECB_INTR_STAT,
+
+	AO_DEBUG_REG0,
+	AO_DEBUG_REG1,
+
+	AO_CEC_STICKY_DATA0,
+	AO_CEC_STICKY_DATA1, /*port info return val to kernel*/
+	AO_CEC_STICKY_DATA2, /*port info return val to kernel*/
+	AO_CEC_STICKY_DATA3, /*not use*/
+	AO_CEC_STICKY_DATA4, /*not use*/
+	AO_CEC_STICKY_DATA5, /*not use*/
+	AO_CEC_STICKY_DATA6, /*not use*/
+	AO_CEC_STICKY_DATA7, /*not use*/
+};
+#endif
+
 #if CEC_REG_DEBUG
 static const char *const ceca_reg_name1[] = {
 	"CEC_TX_MSG_LENGTH", "CEC_TX_MSG_CMD",	  "CEC_TX_WRITE_BUF",  "CEC_TX_CLEAR_BUF",

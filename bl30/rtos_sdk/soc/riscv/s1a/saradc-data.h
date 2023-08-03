f8a48e43b0e262e451edf7d64897912e8817617a
Huqiang Qin <huqiang.qin@amlogic.com>
2023-08-03 16:41:06 +0800

    saradc: s1a: support saradc/keypad [1/1]
    PD#SWPL-134719
    
    Problem:
    S1A Silicon Bringup.
    
    Solution:
    S1A and TXHD2 have the same controller.
    
    Verify:
    S1A/BG201
    
    Change-Id: I23194f90054b7018c147e31ad6cc5b0d791083f6
    Signed-off-by: Huqiang Qin <huqiang.qin@amlogic.com>

--- bl30/rtos_sdk/soc/riscv/s1a/saradc-data.h
+++ bl30/rtos_sdk/soc/riscv/s1a/saradc-data.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021-2022 Amlogic, Inc. All rights reserved.
+ * Copyright (c) 2021-2023 Amlogic, Inc. All rights reserved.
  *
  * SPDX-License-Identifier: MIT
  */
@@ -12,13 +12,16 @@
 #define SAR_CLK_BASE		CLKCTRL_SAR_CLK_CTRL0
 #define SARADC_BASE		SAR_ADC_REG0
 
-#define REG11_VREF_EN_VALUE	0
-#define REG11_CMV_SEL_VALUE	0
-#define REG11_EOC_VALUE		1
+#define REG11_VREF_EN_VALUE	1
+#define REG11_CMV_SEL_VALUE	1
+#define REG11_EOC_VALUE		0
 
 /* s1a saradc interrupt num */
 #define SARADC_INTERRUPT_NUM	181
 
 #define SARADC_REG_NUM		(18 + 1)
+
+/* bandgap of gxlx3/txhd2/s1a is bit12 */
+#define SARADC_REG11_BANDGAP_EN_BIT 12
 
 #endif

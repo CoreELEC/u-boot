63c3db92369c26a8ad8c8087b1285657a538f60d
hang cheng <hang.cheng@amlogic.com>
2023-08-07 16:40:47 +0800

    cec: bringup for s1a [2/4]
    PD#SWPL-134993
    
    Problem:
    need to bringup cec function for s1a
    
    Solution:
    bringup cec function for s1a
    
    Verify:
    bg201
    
    Change-Id: I8dd80c9753434bcf8e8f794a61bf06b6b0ca41da
    Signed-off-by: hang cheng <hang.cheng@amlogic.com>

--- bl30/rtos_sdk/soc/riscv/s1a/cec-data.h
+++ bl30/rtos_sdk/soc/riscv/s1a/cec-data.h
@@ -4,23 +4,16 @@
  * SPDX-License-Identifier: MIT
  */
 
-#ifndef __CEC_S1A_DATA_H__
-#define __CEC_S1A_DATA_H__
+#ifndef __S1A_CEC_DATA_H__
+#define __S1A_CEC_DATA_H__
 
-/*s1a data define*/
-
-/*GPIOH_3 FUNC4:ceca, FUNC1:ceca FUNC2:cecb*/
-#define CEC_PIN_MX	GPIOH_3
-#define CEC_PIN_FUNC	PIN_FUNC2
-#define CEC_IP		1	/* 0: cec a, 1: cecb*/
+/* S1A data define */
+#define CEC_PIN_MX GPIOH_3
+#define CEC_PIN_FUNC PIN_FUNC1
+#define CEC_IP 1 /* 0: cec a, 1: cecb*/
 
 //reg table define
-//#define CEC_CHIP_SEL_T7
-//#define CEC_CHIP_SEL_T5
-//#define CEC_CHIP_SEL_SC2
 #define CEC_CHIP_SEL_S1A
 
-
-#define CEC_ON		1
+#define CEC_ON 1
 #endif
-

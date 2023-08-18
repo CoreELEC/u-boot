d656e53b7dfbcb7434de40a272b8ee0fa32a7d70
yuhua.lin <yuhua.lin@amlogic.com>
2023-08-18 10:42:38 +0800

    vpu: change rdarb reg for s7 [1/1]
    PD#SWPL-135793
    
    Problem:
    osd2 can not display
    
    Solution:
    change rdarb reg default
    
    Verify:
    s7
    
    Change-Id: I9d2a2931e00fdc29c1819a9e47db25eb3a9df5b1
    Signed-off-by: yuhua.lin <yuhua.lin@amlogic.com>

--- bl33/v2023/drivers/amlogic/media/common/vpu/vpu_power.c
+++ bl33/v2023/drivers/amlogic/media/common/vpu/vpu_power.c
@@ -74,9 +74,12 @@ void vpu_module_init_config(void)
 #ifdef VPU_RDARB_UGT_L2C1
 		vpu_vcbus_write(VPU_RDARB_UGT_L2C1, 0xf);
 #endif
-	} else {
+	} else if (vpu_conf.data->chip_type == VPU_CHIP_S7) {
+		/*S7 ONLY VPU0 READ*/
+		vpu_vcbus_write(VPU_RDARB_MODE_L2C1, 0x00000);
+	} else
 		vpu_vcbus_write(VPU_RDARB_MODE_L2C1, 0x900000);
-	}
+
 	vpu_vcbus_write(VPU_WRARB_MODE_L2C1, 0x20000);
 #ifdef CONFIG_AMLOGIC_TEE
 	if (vpu_conf.data->chip_type == VPU_CHIP_T3 ||

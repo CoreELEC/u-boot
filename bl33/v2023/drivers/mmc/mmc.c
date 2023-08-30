820f8e741a4155788817315a6527ba1b3225d553
jinbiao <jinbiao.ou@amlogic.com>
2023-08-30 03:35:48 +0000

    mmc: passing null pointer "mmc" to "mmc_init" [1/1]
    PD#SWPL-138064
    
    Problem:
    The mmc_init function may have access to a null pointer
    
    Solution:
    Add null pointer judgment
    
    Verify:
    none
    
    Change-Id: Icf864b90204be44035ccd3c64eb2c87d266af59f
    Signed-off-by: jinbiao <jinbiao.ou@amlogic.com>

--- bl33/v2023/drivers/mmc/mmc.c
+++ bl33/v2023/drivers/mmc/mmc.c
@@ -2998,6 +2998,10 @@ int mmc_init(struct mmc *mmc)
 {
 	int err = 0;
 	__maybe_unused ulong start;
+
+	if (!mmc)
+		return -ENODEV;
+
 #if CONFIG_IS_ENABLED(DM_MMC)
 	struct mmc_uclass_priv *upriv = dev_get_uclass_priv(mmc->dev);
 

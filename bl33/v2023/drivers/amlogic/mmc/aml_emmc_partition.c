155f3f1ce5a23267b073ad998311ef305bad0dae
jinbiao <jinbiao.ou@amlogic.com>
2023-11-27 02:46:06 +0000

    mmc: check whether the start lba of gpt_e is out of bounds. [1/1]
    PD#SWPL-147531
    
    Problem:
    The start lba of the gpt partition entry is
    not checked for out-of-bounds.
    
    Solution:
    check whether the start lba of gpt_e is out of bounds.
    
    Verify:
    s4_ap222
    
    Change-Id: I290cc007229258d7ed1e5387dc1660179060f769
    Signed-off-by: jinbiao <jinbiao.ou@amlogic.com>

--- bl33/v2023/drivers/amlogic/mmc/aml_emmc_partition.c
+++ bl33/v2023/drivers/amlogic/mmc/aml_emmc_partition.c
@@ -1869,6 +1869,12 @@ int check_gpt_part(struct blk_desc *dev_desc, void *buf)
 		}
 
 #endif
+		if (le64_to_cpu(gpt_e[i].starting_lba) > gpt_h->last_usable_lba) {
+			printf("gpt_e[%d].starting_lba: %llX > %llX, writing failed\n", i,
+			       le64_to_cpu(gpt_e[i].starting_lba),
+			       le64_to_cpu(gpt_h->last_usable_lba));
+			return 1;
+		}
 		if (le64_to_cpu(gpt_e[i].ending_lba) > gpt_h->last_usable_lba) {
 			printf("gpt_e[%d].ending_lba: %llX > %llX, reset it\n",
 			i, le64_to_cpu(gpt_e[i].ending_lba), le64_to_cpu(gpt_h->last_usable_lba));

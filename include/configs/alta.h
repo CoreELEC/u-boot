dc6727693b888b95d4b1094ae1c34e3a7eb084e2
Portisch <hugo.portisch@yahoo.de>
2024-01-05 07:36:45 +0100

    board: add Libre Computer Alta AML-A311D-CC

---
+++ include/configs/alta.h
@@ -0,0 +1,15 @@
+#ifndef __AML_A311D_CC_H__
+#define __AML_A311D_CC_H__
+
+#define CONFIG_DEVICE_PRODUCT		"alta"
+#define ODROID_BOARD_UUID		"c21e5ce2-f4b8-4279-97b8-70147276f608"
+
+/* configs for CEC */
+#define CONFIG_CEC_OSD_NAME		"Alta"
+#define CONFIG_CEC_WAKEUP
+
+#include "odroid-g12-common.h"
+
+#undef ETHERNET_EXTERNAL_PHY
+
+#endif

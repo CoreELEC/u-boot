be3343bb4725f661b715f722f7ac779c3f49bf28
bangzheng.liu <bangzheng.liu@amlogic.com>
2024-01-11 12:03:39 +0800

    S6: PXP Bringup Bootloader BL30 code portinig [2/3]
    PD#SWPL-153508
    
    Problem:
    S6 PXP Bringup Bootloader BL30.
    
    Solution:
    Code porting from s7d and ucode.
    
    Verify:
    Test in S6 PXP.
    
    Change-Id: Id593842328d9fd0656ae6f5a7307bf89572b362d
    Signed-off-by: bangzheng.liu <bangzheng.liu@amlogic.com>

---
+++ bl30/rtos_sdk/boards/riscv/s6_skt/hw_business.c
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2021-2022 Amlogic, Inc. All rights reserved.
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#include <stdio.h>
+#include "FreeRTOS.h"
+#include "queue.h"
+#include "semphr.h"
+#include "hw_business.h"
+#include "n200_eclic.h"
+#include "n200_func.h"
+#include "uart.h"
+#include "eth.h"
+#include "common.h"
+#include "riscv_encoding.h"
+#include "mailbox-api.h"
+#include "hdmi_cec.h"
+#include "stick_mem.h"
+#include "suspend.h"
+#include "vrtc.h"
+
+#define INT_TEST_NEST_DEPTH 6
+#define INT_TEST_GPIO_NUM 6
+#define INT_TEST_TASK_DELAY 50 // ms
+#define TASK_TEST_STACK_DEPTH 200
+
+//#define GPIO_INT_SOURCE(x) (SOC_PIC_INT_GPIO_BASE + x)
+
+/* Configure board type:
+ *   Board under test :        SIGNAL_BOARD_ENABLE     0
+ *   Signal generation board : SIGNAL_BOARD_ENABLE     1
+ */
+#define SIGNAL_BOARD_ENABLE 0
+
+#define INT_TEST_INT_WAVE_ENABLE 1
+
+#if INT_TEST_INT_WAVE_ENABLE
+#define INT_TEST_TIMER_PERIOD 500 // ms
+#define INT_TEST_INT_DELAY 10 // ms
+#else
+#define INT_TEST_TIMER_PERIOD 500 // ms
+#define INT_TEST_INT_DELAY 0x3ff // ms
+#endif
+
+#define INT_TEST_MAX_TIMER_PERIOD 100 // ms
+#define INT_TEST_MIN_TIMER_PERIOD 50 // ms
+#define INT_TEST_MUTE_TIMER_PERIOD 200 // ms
+
+void config_eclic_irqs(void)
+{
+	eclic_init(ECLIC_NUM_INTERRUPTS);
+	eclic_set_nlbits(0);
+}
+
+void hw_business_process(void)
+{
+	uint8_t i = 0;
+
+	config_eclic_irqs();
+	for (i = 0; i < 4; ++i)
+		printf("AOCPU_IRQ_SEL=0x%x\n", REG32(AOCPU_IRQ_SEL0 + i * 4));
+	vMbInit();
+	//	vCecCallbackInit(CEC_CHIP_SC2);
+	vRtcInit();
+	//rtc_init();
+	vETHMailboxCallback();
+	create_str_task();
+}
---
+++ bl30/rtos_sdk/boards/riscv/s6_skt/power.c
@@ -0,0 +1,244 @@
+/*
+ * Copyright (c) 2021-2022 Amlogic, Inc. All rights reserved.
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#include <stdio.h>
+#include "FreeRTOS.h"
+#include "common.h"
+#include "gpio.h"
+#include "ir.h"
+#include "eth.h"
+#include "soc.h"
+#include "suspend.h"
+#include "task.h"
+#include "gpio.h"
+#include "pwm.h"
+#include "pwm_plat.h"
+#include "keypad.h"
+#include "timer_source.h"
+#include "wakeup.h"
+#include "power.h"
+#include "mailbox-api.h"
+
+
+#include "hdmi_cec.h"
+static TaskHandle_t cecTask;
+
+#define VCC5V_GPIO	GPIOC_7
+#define VCC3V3_GPIO	GPIOD_10
+#define VDDCPU_A55_GPIO	GPIOD_3
+#define VDDCPU_A76_GPIO	GPIO_TEST_N
+
+static int vdd_ee;
+static int vdddos_npu_vpu;
+static TaskHandle_t vadTask;
+
+static struct IRPowerKey prvPowerKeyList[] = {
+	{ 0xef10fe01, IR_NORMAL }, /* ref tv pwr */
+	{ 0xba45bd02, IR_NORMAL }, /* small ir pwr */
+	{ 0xef10fb04, IR_NORMAL }, /* old ref tv pwr */
+	{ 0xf20dfe01, IR_NORMAL },
+	{ 0xe51afb04, IR_NORMAL },
+	{ 0x3ac5bd02, IR_CUSTOM },
+	{}
+};
+
+static void vIRHandler(struct IRPowerKey *pkey)
+{
+	uint32_t buf[4] = { 0 };
+
+	if (pkey->type == IR_NORMAL)
+		buf[0] = REMOTE_WAKEUP;
+	else if (pkey->type == IR_CUSTOM)
+		buf[0] = REMOTE_CUS_WAKEUP;
+
+	/* do sth below  to wakeup*/
+	STR_Wakeup_src_Queue_Send_FromISR(buf);
+};
+
+static void *xMboxVadWakeup(void *msg)
+{
+	uint32_t buf[4] = { 0 };
+
+	buf[0] = VAD_WAKEUP;
+	STR_Wakeup_src_Queue_Send(buf);
+
+	return NULL;
+}
+
+void str_hw_init(void)
+{
+	int ret;
+	/*enable device & wakeup source interrupt*/
+	vIRInit(MODE_HARD_NEC, GPIOD_5, PIN_FUNC1, prvPowerKeyList, ARRAY_SIZE(prvPowerKeyList),
+		vIRHandler);
+	vETHInit(0);
+
+	xTaskCreate(vCEC_task, "CECtask", configMINIMAL_STACK_SIZE,
+		    NULL, CEC_TASK_PRI, &cecTask);
+
+	//vBackupAndClearGpioIrqReg();
+	vGpioIRQInit();
+	vKeyPadInit();
+	//Bt_GpioIRQRegister();
+}
+
+void str_hw_disable(void)
+{
+	/*disable wakeup source interrupt*/
+	vIRDeint();
+	vETHDeint();
+
+	if (cecTask) {
+		vTaskDelete(cecTask);
+		cec_req_irq(0);
+	}
+	//Bt_GpioIRQFree();
+	vKeyPadDeinit();
+	vRestoreGpioIrqReg();
+}
+
+void str_power_on(int shutdown_flag)
+{
+	int ret;
+
+	(void)shutdown_flag;
+
+	/***power on A55 vdd_cpu***/
+	ret = xGpioSetDir(VDDCPU_A55_GPIO, GPIO_DIR_OUT);
+	if (ret < 0) {
+		printf("vdd_cpu set gpio dir fail\n");
+		return;
+	}
+
+	ret = xGpioSetValue(VDDCPU_A55_GPIO, GPIO_LEVEL_HIGH);
+	if (ret < 0) {
+		printf("vdd_cpu set gpio val fail\n");
+		return;
+	}
+
+	/***power on A76 vdd_cpu***/
+	ret = xGpioSetDir(VDDCPU_A76_GPIO, GPIO_DIR_OUT);
+	if (ret < 0) {
+		printf("vdd_cpu set gpio dir fail\n");
+		return;
+	}
+
+	ret = xGpioSetValue(VDDCPU_A76_GPIO, GPIO_LEVEL_HIGH);
+	if (ret < 0) {
+		printf("vdd_cpu set gpio val fail\n");
+		return;
+	}
+
+	/***set vdd_ee val***/
+	ret = vPwmMesonsetvoltage(VDDEE_VOLT, vdd_ee);
+	if (ret < 0) {
+		printf("VDD_EE pwm set fail\n");
+		return;
+	}
+
+	if (shutdown_flag) {
+		/***power on vcc_3.3v***/
+		ret = xGpioSetDir(VCC3V3_GPIO, GPIO_DIR_OUT);
+		if (ret < 0) {
+			printf("vcc_3.3v set gpio dir fail\n");
+			return;
+		}
+
+		ret = xGpioSetValue(VCC3V3_GPIO, GPIO_LEVEL_HIGH);
+		if (ret < 0) {
+			printf("vcc_3.3v gpio val fail\n");
+			return;
+		}
+	}
+
+	/***power on vcc_5v***/
+	ret = xGpioSetDir(VCC5V_GPIO, GPIO_DIR_IN);
+	if (ret < 0) {
+		printf("vcc_5v set gpio dir fail\n");
+		return;
+	}
+
+	/*Wait 20ms for VDDCPU statable*/
+	vTaskDelay(pdMS_TO_TICKS(20));
+
+	printf("vdd_cpu on\n");
+}
+
+void str_power_off(int shutdown_flag)
+{
+	int ret;
+
+	(void)shutdown_flag;
+
+	/***power off vcc_5v***/
+	ret = xGpioSetDir(VCC5V_GPIO, GPIO_DIR_OUT);
+	if (ret < 0) {
+		printf("vcc_5v set gpio dir fail\n");
+		return;
+	}
+
+	ret = xGpioSetValue(VCC5V_GPIO, GPIO_LEVEL_LOW);
+	if (ret < 0) {
+		printf("vcc_5v gpio val fail\n");
+		return;
+	}
+
+	if (shutdown_flag) {
+		/***power off vcc_3.3v***/
+		ret = xGpioSetDir(VCC3V3_GPIO, GPIO_DIR_OUT);
+		if (ret < 0) {
+			printf("vcc_3.3v set gpio dir fail\n");
+			return;
+		}
+
+		ret = xGpioSetValue(VCC3V3_GPIO, GPIO_LEVEL_LOW);
+		if (ret < 0) {
+			printf("vcc_3.3v gpio val fail\n");
+			return;
+		}
+	}
+
+	/***set vdd_ee val***/
+	vdd_ee = vPwmMesongetvoltage(VDDEE_VOLT);
+	if (vdd_ee < 0) {
+		printf("vdd_EE pwm get fail\n");
+		return;
+	}
+
+	ret = vPwmMesonsetvoltage(VDDEE_VOLT, 770);
+	if (ret < 0) {
+		printf("vdd_EE pwm set fail\n");
+		return;
+	}
+
+	/***power off A55 vdd_cpu***/
+	ret = xGpioSetDir(VDDCPU_A55_GPIO, GPIO_DIR_OUT);
+	if (ret < 0) {
+		printf("vdd_cpu set gpio dir fail\n");
+		return;
+	}
+
+	ret = xGpioSetValue(VDDCPU_A55_GPIO, GPIO_LEVEL_LOW);
+	if (ret < 0) {
+		printf("vdd_cpu set gpio val fail\n");
+		return;
+	}
+
+	/***power off A76 vdd_cpu***/
+	ret = xGpioSetDir(VDDCPU_A76_GPIO, GPIO_DIR_OUT);
+	if (ret < 0) {
+		printf("vdd_cpu set gpio dir fail\n");
+		return;
+	}
+
+	ret = xGpioSetValue(VDDCPU_A76_GPIO, GPIO_LEVEL_LOW);
+	if (ret < 0) {
+		printf("vdd_cpu set gpio val fail\n");
+		return;
+	}
+
+	printf("Power down done.\n");
+}

633c651eb3c48261a4693e308dfb338e769f8b2c
benlong.zhou <benlong.zhou@amlogic.com>
2024-01-12 15:25:22 +0800

    bayside: enable device-key [1/1]
    PD#SWPL-152858
    
    Problem:
    s905y5r board device-key is enabled
    
    Solution:
    enable device-key
    
    Verify:
    t223
    
    Change-Id: Icb4dee2c0e5a14b0215fc700e4c24ec58aa1dfe2
    Signed-off-by: benlong.zhou <benlong.zhou@amlogic.com>

--- bl33/v2023/board/amlogic/configs/s7_t223.h
+++ bl33/v2023/board/amlogic/configs/s7_t223.h
@@ -309,7 +309,7 @@ defined(CONFIG_STORE_COMPATIBLE)
 
 /* unify build for generate encrypted bootloader "u-boot.bin.encrypt" */
 #define CONFIG_AML_CRYPTO_UBOOT   1
-//#define CONFIG_AML_SIGNED_UBOOT   1
+#define CONFIG_AML_SIGNED_UBOOT   1
 /* unify build for generate encrypted kernel image
  * SRC : "board/amlogic/(board)/boot.img"
  * DST : "fip/boot.img.encrypt"

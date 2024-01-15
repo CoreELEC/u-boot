405ee064eeb2f4d2a300e9f38a76d37b3c574d21
bowen cheng <bowen.cheng@amlogic.com>
2024-01-15 17:28:05 +0800

    cec: s7 cec bringup [3/3]
    PD#SWPL-153915
    
    Problem:
    enable mbox cec task
    
    Solution:
    enable mbox cec task
    
    Verify:
    bh201_s905y5
    
    Change-Id: If9874b2465b435cfe2329941348648c45e7466cd
    Signed-off-by: bowen cheng <bowen.cheng@amlogic.com>

--- bl30/rtos_sdk/boards/riscv/bh201_s905y5/hw_business.c
+++ bl30/rtos_sdk/boards/riscv/bh201_s905y5/hw_business.c
@@ -63,7 +63,7 @@ void hw_business_process(void)
 	for (i = 0; i < 4; ++i)
 		printf("AOCPU_IRQ_SEL=0x%x\n", REG32(AOCPU_IRQ_SEL0 + i * 4));
 	vMbInit();
-	//	vCecCallbackInit(CEC_CHIP_SC2);
+	vCecCallbackInit(CEC_CHIP_S7);
 	vRtcInit();
 	//rtc_init();
 	vETHMailboxCallback();

e458bc9c6acb9cb306cc365ad4f302d662a7c24f
yanmei.yang <yanmei.yang@amlogic.com>
2024-02-29 17:15:18 +0800

    yocto: CF1 disable hdmi bestmode for zapper  [1/1]
    PD#SWPL-151522
    
    Problem:
    disable hdmi bestmode for zapper
    
    Solution:
    disable hdmi bestmode for zapper
    
    Verify:
      Local
    
    Signed-off-by: yanmei.yang <yanmei.yang@amlogic.com>
    Change-Id: I2984afb9f66c48c6f6d9bd3cbacd5462adf312e0

--- bl33/v2023/board/amlogic/configs/s1a_bg201.h
+++ bl33/v2023/board/amlogic/configs/s1a_bg201.h
@@ -70,6 +70,7 @@
 		"hdmimode=1080p60hz\0" \
 		"colorattribute=444,8bit\0"\
 		"cvbsmode=576cvbs\0" \
+		"is.bestmode=false\0" \
 	"vout_init=enable\0" \
 		"display_width=1920\0" \
 		"display_height=1080\0" \

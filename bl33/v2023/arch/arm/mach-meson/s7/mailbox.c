b3d8b14832be58902efae33a24b91e72d74bfb9a
Yao Jie <jie.yao@amlogic.com>
2024-02-29 21:26:42 +0800

    bl33: S7 add aocpu alive detection mechanism [1/2]
    PD#SWPL-155399
    
    Problem:
    CPU would hang when bl31/bl33/kernel communicate with
    bl30/AOCPU that we could not confirm the reason.
    
    Solution:
    Add aocpu alive detection mechanism.
    
    Verify:
    S7-BH201
    
    Change-Id: Ic02730e66a434c5d6c2d320d7e0739cf732c9ef3
    Signed-off-by: Yao Jie <jie.yao@amlogic.com>

--- bl33/v2023/arch/arm/mach-meson/s7/mailbox.c
+++ bl33/v2023/arch/arm/mach-meson/s7/mailbox.c
@@ -52,6 +52,43 @@ static inline void mbclean(u32 to, long count)
 	}
 }
 
+/* Return if AOCPU is alive by checking AOCPU tick count */
+static u32 is_aocpu_alive(void)
+{
+	u32 aocpu_tick_count_1;
+	u32 aocpu_tick_count_2;
+	u32 ret;
+
+	aocpu_tick_count_1 = aml_readl32(AOCPU_TICK_CNT_RD_ADDR);
+	printf("%s: aocpu_tick_count_1 = %x\n", __func__, aocpu_tick_count_1);
+	/* Wait for 50 msec to let tick count update */
+	mdelay(50);
+	aocpu_tick_count_2 = aml_readl32(AOCPU_TICK_CNT_RD_ADDR);
+	printf("%s: aocpu_tick_count_2 = %x\n", __func__, aocpu_tick_count_2);
+	if (aocpu_tick_count_1 == aocpu_tick_count_2)
+		ret = 0;
+	else
+		ret = 1;
+
+	return ret;
+}
+
+/* Return timerE value (seconds) */
+static u32 read_timer_e(void)
+{
+	unsigned long long time64 = 0, time_high = 0;
+	u32 time = 0;
+
+	/*timeE high+low, first read low, second read high*/
+	time64 = aml_readl32(TIMERE_LOW_REG);
+	time_high = aml_readl32(TIMERE_HIG_REG);
+	time64 += (time_high << 32);
+	time64 = time64 / 1000000;
+	time = (u32)time64;
+
+	return time;
+}
+
 int mhu_get_addr(u32 chan, u32 *mboxset, u32 *mboxsts,
 		 u32 *mboxwr, u32 *mboxrd,
 		 u32 *mboxirqclr, u32 *mboxid)
@@ -113,13 +150,41 @@ void mhu_message_send(u32 mboxset, u32 command, u32 size)
 	aml_writel32(mbox_cmd, mboxset);
 }
 
-u32 mhu_message_wait(u32 mboxsts)
+u32 mhu_message_wait(u32 mboxset, u32 mboxsts, u32 command, u32 size)
 {
 	/* Wait for response from other core */
 	u32 response;
+	u32 time_stamp_start = 0;
+	u32 time_stamp_end = 0;
+	u32 time_elapse = 0;
+	u32 retry_cnt = 2;
+	u32 aocpu_alive;
+	u32 mbox_cmd;
 
-	while ((response = aml_readl32(mboxsts)))
-		;
+	time_stamp_start = read_timer_e();
+	while ((response = aml_readl32(mboxsts))) {
+		time_stamp_end = read_timer_e();
+		time_elapse = time_stamp_end - time_stamp_start;
+		/* Set mailbox wait time out: 1 second */
+		if (time_elapse > 1) {
+			printf("%s: warning: mailbox wait time out, send fail!\n", __func__);
+			aocpu_alive = is_aocpu_alive();
+			if (aocpu_alive)
+				printf("%s: aocpu is alive!\n", __func__);
+			else
+				printf("%s: aocpu is not alive!\n", __func__);
+
+			if (retry_cnt == 0)
+				break;
+			retry_cnt--;
+
+			printf("%s: mailbox send retry\n", __func__);
+			mbox_cmd = MHU_CMD_BUILD(command, size + MHU_DATA_OFFSET);
+			aml_writel32(0, mboxset);
+			aml_writel32(mbox_cmd, mboxset);
+			time_stamp_start = read_timer_e();
+		}
+	}
 
 	return response;
 }
@@ -161,7 +226,7 @@ int scpi_send_data(u32 chan, u32 command,
 	if (sendmessage && sendsize != 0)
 		mhu_build_payload(mboxwr, sendmessage, sendsize);
 	mhu_message_send(mboxset, command, sendsize);
-	mhu_message_wait(mboxsts);
+	mhu_message_wait(mboxset, mboxsts, command, sendsize);
 	if (revmessage && revsize != 0)
 		mhu_get_payload(mboxwr, mboxrd, revmessage, revsize);
 	mhu_message_end(mboxwr, mboxirqclr, mboxid);

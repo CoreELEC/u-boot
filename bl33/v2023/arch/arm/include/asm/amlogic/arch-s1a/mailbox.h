f599a22ff9987d131ac4a10c27f2806ed2ec53fb
bangzheng.liu <bangzheng.liu@amlogic.com>
2024-03-01 16:39:22 +0800

    bl33: support bl30 print control when use acs [1/1]
    PD#SWPL-158030
    
    Problem:
    bl30 uart print would be disabled by acs
    
    Solution:
    add bl30 print control
    
    Verify:
    s7_bh201
    
    Change-Id: Ic610e05897c441b0dab59d8906ddb2821ccf23bd
    Signed-off-by: bangzheng.liu <bangzheng.liu@amlogic.com>

--- bl33/v2023/arch/arm/include/asm/amlogic/arch-s1a/mailbox.h
+++ bl33/v2023/arch/arm/include/asm/amlogic/arch-s1a/mailbox.h
@@ -49,6 +49,7 @@
 #define CMD_TEST		0x6
 #define CMD_GET_STICK_REBOOT_FLAG	0x40
 #define CMD_LED_INFO            0xF7
+#define CMD_SET_BL30_PRINT	0x6A
 
 /*******************************************************************************
  * Mssage composition

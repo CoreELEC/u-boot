20fe54621313a929d209f4c5ef84f20185d92ddc
Huqiang Qin <huqiang.qin@amlogic.com>
2024-03-07 17:05:04 +0800

    pinctrl: t5m: support gpio/pinmux [1/1]
    PD#SWPL-159501
    
    Problem:
    T5M U-Boot 2023 Bringup.
    
    Solution:
    T5M U-Boot 2023 Bringup.
    
    Verify:
    T5M/AY301
    
    Change-Id: I3773c802d23963d6c00871a2100addee75c96bf9
    Signed-off-by: Huqiang Qin <huqiang.qin@amlogic.com>

--- bl33/v2023/board/amlogic/t5m_ay301/t5m_ay301.c
+++ bl33/v2023/board/amlogic/t5m_ay301/t5m_ay301.c
@@ -136,7 +136,7 @@ int board_init(void)
 {
 	printf("board init\n");
 
-	run_command("gpio set GPIO_TEST_N0", 0);
+	run_command("gpio set GPIO_TEST_N_0", 0);
 	run_command("gpio clr GPIOD_11", 0);
 	aml_set_bootsequence(0);
 	//Please keep try usb boot first in board_init, as other init before usb may cause burning failure

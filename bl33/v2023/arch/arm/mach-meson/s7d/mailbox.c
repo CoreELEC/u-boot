6b4f65145ecb95d6e22bde239ae15be4322da52d
Yao Jie <jie.yao@amlogic.com>
2024-04-03 17:59:27 +0800

    bl33: S7D add aocpu alive detection mechanism [1/1]
    PD#SWPL-163760
    
    Problem:
    CPU would hang when bl31/bl33/kernel communicate with
    bl30/AOCPU that we could not confirm the reason.
    
    Solution:
    Add aocpu alive detection mechanism.
    
    Verify:
    S7D-BM209
    
    Change-Id: I77f2037a9f8247601564857eb7a1d6361e893f9e
    Signed-off-by: Yao Jie <jie.yao@amlogic.com>

--- bl33/v2023/arch/arm/mach-meson/s7d/mailbox.c
+++ bl33/v2023/arch/arm/mach-meson/s7d/mailbox.c
@@ -14,11 +14,11 @@
 #define aml_writel32(value, reg)	writel(value, reg)
 #define aml_readl32(reg)		readl(reg)
 
-static inline void mbwrite(uint32_t to, void *from, long count)
+static inline void mbwrite(u32 to, void *from, long count)
 {
 	int i = 0;
-	int len = count / 4 + (count % 4);
-	uint32_t *p = from;
+	int len = count / 4 + ((count % 4) ? 1 : 0);
+	u32 *p = from;
 
 	while (len > 0) {
 		aml_writel32(p[i], to + (4 * i));
@@ -27,10 +27,23 @@ static inline void mbwrite(uint32_t to, void *from, long count)
 	}
 }
 
-static inline void mbclean(uint32_t to, long count)
+static inline void mbread(void *to, u32 from, long count)
 {
 	int i = 0;
-	int len = count / 4 + (count % 4);
+	int len = count / 4 + ((count % 4) ? 1 : 0);
+	u32 *p = to;
+
+	while (len > 0) {
+		p[i] = aml_readl32(from + (4 * i));
+		len--;
+		i++;
+	}
+}
+
+static inline void mbclean(u32 to, long count)
+{
+	int i = 0;
+	int len = count / 4 + ((count % 4) ? 1 : 0);
 
 	while (len > 0) {
 		aml_writel32(0, to + (4 * i));
@@ -39,9 +52,46 @@ static inline void mbclean(uint32_t to, long count)
 	}
 }
 
-int mhu_get_addr(uint32_t chan, uint32_t *mboxset, uint32_t *mboxsts,
-		 uintptr_t *mboxpl, uint32_t *mboxwr, uint32_t *mboxrd,
-		 uint32_t *mboxirqclr, uint32_t *mboxid)
+/* Return if AOCPU is alive by checking AOCPU tick count */
+static u32 is_aocpu_alive(void)
+{
+	u32 aocpu_tick_count_1;
+	u32 aocpu_tick_count_2;
+	u32 ret;
+
+	aocpu_tick_count_1 = aml_readl32(AOCPU_TICK_CNT_RD_ADDR);
+	printf("%s: aocpu_tick_count_1 = %x\n", __func__, aocpu_tick_count_1);
+	/* Wait for 50 msec to let tick count update */
+	mdelay(50);
+	aocpu_tick_count_2 = aml_readl32(AOCPU_TICK_CNT_RD_ADDR);
+	printf("%s: aocpu_tick_count_2 = %x\n", __func__, aocpu_tick_count_2);
+	if (aocpu_tick_count_1 == aocpu_tick_count_2)
+		ret = 0;
+	else
+		ret = 1;
+
+	return ret;
+}
+
+/* Return timerE value (seconds) */
+static u32 read_timer_e(void)
+{
+	unsigned long long time64 = 0, time_high = 0;
+	u32 time = 0;
+
+	/*timeE high+low, first read low, second read high*/
+	time64 = aml_readl32(TIMERE_LOW_REG);
+	time_high = aml_readl32(TIMERE_HIG_REG);
+	time64 += (time_high << 32);
+	time64 = time64 / 1000000;
+	time = (u32)time64;
+
+	return time;
+}
+
+int mhu_get_addr(u32 chan, u32 *mboxset, u32 *mboxsts,
+		 u32 *mboxwr, u32 *mboxrd,
+		 u32 *mboxirqclr, u32 *mboxid)
 {
 	int ret = 0;
 
@@ -62,14 +112,14 @@ int mhu_get_addr(uint32_t chan, uint32_t *mboxset, uint32_t *mboxsts,
 	return ret;
 }
 
-void mhu_message_start(uint32_t mboxsts)
+void mhu_message_start(u32 mboxsts)
 {
 	/* Make sure any previous command has finished */
 	while (aml_readl32(mboxsts) != 0)
 		;
 }
 
-void mhu_build_payload(uintptr_t mboxpl, uint32_t mboxwr, void *message, uint32_t size)
+void mhu_build_payload(u32 mboxwr, void *message, u32 size)
 {
 	if (size > (MHU_PAYLOAD_SIZE - MHU_DATA_OFFSET)) {
 		printf("bl33: scpi send input size error\n");
@@ -80,7 +130,7 @@ void mhu_build_payload(uintptr_t mboxpl, uint32_t mboxwr, void *message, uint32_
 	mbwrite(mboxwr + MHU_DATA_OFFSET, message, size);
 }
 
-void mhu_get_payload(uintptr_t mboxpl, uint32_t mboxwr, void *message, uint32_t size)
+void mhu_get_payload(u32 mboxwr, u32 mboxrd, void *message, u32 size)
 {
 	if (size > (MHU_PAYLOAD_SIZE - MHU_DATA_OFFSET)) {
 		printf("bl33: scpi send input size error\n");
@@ -88,65 +138,97 @@ void mhu_get_payload(uintptr_t mboxpl, uint32_t mboxwr, void *message, uint32_t 
 	}
 	if (size == 0)
 		return;
-	printf("bl33: scpi no support get revmessage\n");
+	mbread(message, mboxrd + MHU_DATA_OFFSET, size);
+	mbclean(mboxwr, MHU_PAYLOAD_SIZE);
 }
 
-void mhu_message_send(uint32_t mboxset, uint32_t command, uint32_t size)
+void mhu_message_send(u32 mboxset, u32 command, u32 size)
 {
-	uint32_t mbox_cmd;
+	u32 mbox_cmd;
 
 	mbox_cmd = MHU_CMD_BUILD(command, size + MHU_DATA_OFFSET);
 	aml_writel32(mbox_cmd, mboxset);
 }
 
-uint32_t mhu_message_wait(uint32_t mboxsts)
+u32 mhu_message_wait(u32 mboxset, u32 mboxsts, u32 command, u32 size)
 {
-	/* Wait for response from HIFI */
-	uint32_t response;
+	/* Wait for response from other core */
+	u32 response;
+	u32 time_stamp_start = 0;
+	u32 time_stamp_end = 0;
+	u32 time_elapse = 0;
+	u32 retry_cnt = 2;
+	u32 aocpu_alive;
+	u32 mbox_cmd;
 
-	while ((response = aml_readl32(mboxsts)))
-		;
+	time_stamp_start = read_timer_e();
+	while ((response = aml_readl32(mboxsts))) {
+		time_stamp_end = read_timer_e();
+		time_elapse = time_stamp_end - time_stamp_start;
+		/* Set mailbox wait time out: 1 second */
+		if (time_elapse > 1) {
+			printf("%s: warning: mailbox wait time out, send fail!\n", __func__);
+			aocpu_alive = is_aocpu_alive();
+			if (aocpu_alive)
+				printf("%s: aocpu is alive!\n", __func__);
+			else
+				printf("%s: aocpu is not alive!\n", __func__);
+
+			if (retry_cnt == 0)
+				break;
+			retry_cnt--;
+
+			printf("%s: mailbox send retry\n", __func__);
+			mbox_cmd = MHU_CMD_BUILD(command, size + MHU_DATA_OFFSET);
+			aml_writel32(0, mboxset);
+			aml_writel32(mbox_cmd, mboxset);
+			time_stamp_start = read_timer_e();
+		}
+	}
 
 	return response;
 }
 
-void mhu_message_end(uintptr_t mboxpl, uint32_t mboxwr, uint32_t mboxirqclr, uint32_t mboxid)
+void mhu_message_end(u32 mboxwr, u32 mboxirqclr, u32 mboxid)
 {
+	/* Clear all datas in mailbox buffer */
 	mbclean(mboxwr, MHU_PAYLOAD_SIZE);
-	/* Clear any response we got by writing all ones to the CLEAR register */
+	/* Clear irq_clr */
 	aml_writel32(MHU_ACK_MASK(mboxid), mboxirqclr);
 }
 
 void mhu_init(void)
 {
 	aml_writel32(0xffffffffu, REE2AO_CLR_ADDR);
-	printf("[BL33] mhu init done -v2\n");
+	printf("[BL33] mhu init done fifo-v2\n");
 }
 
-int scpi_send_data(uint32_t chan, uint32_t command,
-		   void *sendmessage, uint32_t sendsize, void *revmessage, uint32_t revsize)
+int scpi_send_data(u32 chan, u32 command,
+		   void *sendmessage, u32 sendsize,
+		   void *revmessage, u32 revsize)
 {
-	uint32_t mboxset = 0;
-	uint32_t mboxsts = 0;
-	uintptr_t mboxpl = 0;
-	uint32_t mboxwr = 0;
-	uint32_t mboxrd = 0;
-	uint32_t mboxirq = 0;
-	uint32_t mboxid = 0;
+	u32 mboxset = 0;
+	u32 mboxsts = 0;
+	u32 mboxwr = 0;
+	u32 mboxrd = 0;
+	u32 mboxirqclr = 0;
+	u32 mboxid = 0;
 	int ret = 0;
 
-	ret = mhu_get_addr(chan, &mboxset, &mboxsts, &mboxpl, &mboxwr, &mboxrd, &mboxirq, &mboxid);
+	ret = mhu_get_addr(chan, &mboxset, &mboxsts,
+			   &mboxwr, &mboxrd,
+			   &mboxirqclr, &mboxid);
 	if (ret) {
 		printf("bl33: mhu get addr fail\n");
 		return ret;
 	}
 	mhu_message_start(mboxsts);
 	if (sendmessage && sendsize != 0)
-		mhu_build_payload(mboxpl, mboxwr, sendmessage, sendsize);
+		mhu_build_payload(mboxwr, sendmessage, sendsize);
 	mhu_message_send(mboxset, command, sendsize);
-	mhu_message_wait(mboxsts);
+	mhu_message_wait(mboxset, mboxsts, command, sendsize);
 	if (revmessage && revsize != 0)
-		mhu_get_payload(mboxpl, mboxrd, revmessage, revsize);
-	mhu_message_end(mboxpl, mboxwr, mboxirq, mboxid);
+		mhu_get_payload(mboxwr, mboxrd, revmessage, revsize);
+	mhu_message_end(mboxwr, mboxirqclr, mboxid);
 	return ret;
 }

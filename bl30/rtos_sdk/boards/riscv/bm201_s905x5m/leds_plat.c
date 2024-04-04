7faa11f48f477ff31aab70d15f1021c62051c6a7
Junyi Zhao <junyi.zhao@amlogic.com>
2024-04-04 14:39:05 +0800

    SoC S7d: led: add led driver [1/1]
    PD#SWPL-163018
    
    Problem:
    there is no led driver
    
    Solution:
    add led driver
    
    Verify:
    s7d bm201
    
    Change-Id: I755d4b7e94ca72ab77afe042c2116b104dbc84da
    Signed-off-by: Junyi Zhao <junyi.zhao@amlogic.com>

--- bl30/rtos_sdk/boards/riscv/bm201_s905x5m/leds_plat.c
+++ bl30/rtos_sdk/boards/riscv/bm201_s905x5m/leds_plat.c
@@ -33,7 +33,8 @@ struct LedDevice MesonLeds[] = {
 		.id = LED_ID_0,
 		.type = LED_TYPE_PWM,
 		.name = "sys_led",
-		.hardware_id = MESON_PWM_C,
+		.hardware_id = MESON_PWM_INVALID,
+		.pwm_chip_id = PWM_G,
 		.polarity = LED_POLARITY_POSITIVE,
 		.breathtime = 0,
 	},
@@ -69,5 +70,5 @@ int32_t vLedPlatInit(int32_t **stickmem)
 int32_t vLedPinmuxInit(void)
 {
 	/* set pinmux */
-	return xPinmuxSet(GPIOD_8, PIN_FUNC3);
+	return xPinmuxSet(GPIODV_6, PIN_FUNC2);
 }

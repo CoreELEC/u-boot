0b8fa6631173a004bc078dac9dc78b309bda705c
xiangyang.yan <xiangyang.yan@amlogic.com>
2024-04-09 15:40:18 +0800

    eth: S7D 2023 inphy enable [1/1]
    PD#SWPL-163036
    
    Problem:
    S7D eth 2023 bringup
    
    Solution:
    S7D eth 2023 bringup
    
    Verify:
    S905X5M-BM201-DDR4#003
    
    Change-Id: I52832942bfdf04ee264f391da908b1e0d0cd5a6c
    Signed-off-by: xiangyang.yan <xiangyang.yan@amlogic.com>

--- bl33/v2023/arch/arm/mach-meson/s7d/eth.c
+++ bl33/v2023/arch/arm/mach-meson/s7d/eth.c
@@ -44,6 +44,7 @@
 #define ANACTRL_PLL_GATE_DIS 0xffffffff
 #endif
 
+#define AML_ETH_PLL_STS  0x40
 #define AML_ETH_PLL_CTL0 0x44
 #define AML_ETH_PLL_CTL1 0x48
 #define AML_ETH_PLL_CTL2 0x4C
@@ -56,6 +57,7 @@
 #define AML_ETH_PHY_CNTL0 0x80
 #define AML_ETH_PHY_CNTL1 0x84
 #define AML_ETH_PHY_CNTL2 0x88
+
 
 enum {
 	/* chip num */
@@ -83,11 +85,9 @@ void setup_tx_amp(struct udevice *dev)
 }
 static void setup_internal_phy(struct udevice *dev)
 {
-	int phy_cntl1 = 0;
 	int mc_val = 0;
 	int chip_num = 0;
-	unsigned int pll_val[3] = {0};
-	unsigned int analog_val[3] = {0};
+	int reg_val = 0;
 	int rtn = 0;
 	struct resource eth_top, eth_cfg;
 	struct clk rmii_clk;
@@ -103,11 +103,6 @@ static void setup_internal_phy(struct udevice *dev)
 		printf("miss stmmacethrmii\n");
 	}
 
-	phy_cntl1 = dev_read_u32_default(dev, "phy_cntl1", 4);
-	if (phy_cntl1 < 0) {
-		printf("miss phy_cntl1\n");
-	}
-
 	mc_val = dev_read_u32_default(dev, "mc_val", 4);
 	if (mc_val < 0) {
 		printf("miss mc_val\n");
@@ -120,24 +115,6 @@ static void setup_internal_phy(struct udevice *dev)
 	}
 	printf("chip num %d\n", chip_num);
 
-	rtn = dev_read_u32_array(dev, "pll_val", pll_val, ARRAY_SIZE(pll_val));
-	if (rtn < 0) {
-		printf("miss pll_val\n");
-	}
-
-	dev_read_u32_array(dev, "analog_val", analog_val, ARRAY_SIZE(analog_val));
-	if (rtn < 0) {
-		printf("miss analog_val\n");
-	}
-
-	for (int i = 0;i <3; i++) {
-		debug("pll_val 0x%08x\n", pll_val[i]);
-	}
-
-	for (int i = 0;i <3; i++) {
-		debug("analog_val 0x%08x\n", analog_val[i]);
-	}
-
 	rtn = dev_read_resource_byname(dev, "eth_top", &eth_top);
 	if (rtn) {
 		printf("can't get eth_top resource(ret = %d)\n", rtn);
@@ -147,40 +124,45 @@ static void setup_internal_phy(struct udevice *dev)
 	if (rtn) {
 		printf("can't get eth_cfg resource(ret = %d)\n", rtn);
 	}
-//	printf("wzh eth_top 0x%x eth_cfg 0x%x \n", eth_top.start, eth_cfg.start);
 
 	setup_tx_amp(dev);
-	/*top*/
-//	setbits_le32(ETHTOP_CNTL0, mc_val);
+
+	/* configure eth_top */
+	printf("mc_val=0x%x\n", mc_val);
 	setbits_le32(eth_top.start, mc_val);
-	/*pll*/
-	writel(readl(eth_cfg.start + AML_ETH_PLL_CTL3) & 0xfffffffe, eth_cfg.start + AML_ETH_PLL_CTL3);
+
+	/* configure pll */
+	// set AML_ETH_PLL_CTL3[ETH_PLL_BIAS_EN,ETH_PLL_RSTN] to [0,0]
+	writel(readl(eth_cfg.start + AML_ETH_PLL_CTL3) & 0xfffffffc, eth_cfg.start + AML_ETH_PLL_CTL3);
 	writel(0x00c091a2, eth_cfg.start + AML_ETH_PLL_CTL0);
 	writel(0x01111140, eth_cfg.start + AML_ETH_PLL_CTL1);
-	writel(readl(eth_cfg.start + AML_ETH_PLL_CTL3)|0x1, eth_cfg.start + AML_ETH_PLL_CTL3);
+	// set AML_ETH_PLL_CTL3[ETH_PLL_BIAS_EN,ETH_PLL_RSTN] to [1,0]
+	writel(readl(eth_cfg.start + AML_ETH_PLL_CTL3) | 0x00000002, eth_cfg.start + AML_ETH_PLL_CTL3);
 	udelay(200);
-	writel(readl(eth_cfg.start + AML_ETH_PLL_CTL3)|0x3, eth_cfg.start + AML_ETH_PLL_CTL3);
+	// set AML_ETH_PLL_CTL3[ETH_PLL_BIAS_EN,ETH_PLL_RSTN] to [1,1]
+	writel(readl(eth_cfg.start + AML_ETH_PLL_CTL3) | 0x00000003, eth_cfg.start + AML_ETH_PLL_CTL3);
 	udelay(200);
 	writel(0x00e091a2, eth_cfg.start + AML_ETH_PLL_CTL0);
-	udelay(200);
+	udelay(800);
+	reg_val = readl(eth_cfg.start + AML_ETH_PLL_STS);
+	/* check eth_mppll_lock_dig */
+	if (!(reg_val & 0x40000000))
+	{
+		printf("pll config failed\n");
+	}
 	writel(0x00007423, eth_cfg.start + AML_ETH_PLL_CTL7);
-
-	/*s7 analog amp*/
 	writel(0x00004001, eth_cfg.start + AML_ETH_PLL_CTL6);
 	writel(0x84001580, eth_cfg.start + AML_ETH_PLL_CTL5);
-	writel(0x88800003, eth_cfg.start + AML_ETH_PLL_CTL3);
+	writel((readl(eth_cfg.start + AML_ETH_PLL_CTL3) & 0x0000ffff) | 0x85800000, eth_cfg.start + AML_ETH_PLL_CTL3);
 
-	/*ctrl*/
-	/*config phyid should between  a 0~0xffffffff*/
-	/*please don't use 44000181, this has been used by internal phy*/
-	writel(0x33010180, eth_cfg.start + AML_ETH_PHY_CNTL0);
-
-	/* Initialize ephy control */
-	/*use_phy_smi | use_phy_ip | co_clkin from eth_phy_top*/
+	/* configure phy control */
+	/* config phyid should between  a 0~0xffffffff */
+	/* please don't use 44000181, this has been used by internal phy */
+	writel(0x33000180, eth_cfg.start + AML_ETH_PHY_CNTL0);
 	writel(0x34147, eth_cfg.start + AML_ETH_PHY_CNTL1);
 	writel(0x260, eth_cfg.start + AML_ETH_PHY_CNTL2);
-	writel(0x74147 | 0x200000, eth_cfg.start + AML_ETH_PHY_CNTL1);
-	udelay(200);
+	writel(0x42074147 | 0x200000, eth_cfg.start + AML_ETH_PHY_CNTL1);
+
 }
 
 static void setup_external_phy(struct udevice *dev)
@@ -283,6 +265,7 @@ void __iomem *DM_network_interface_setup(struct udevice *dev)
 	return 0;
 }
 
+#if 0
 static unsigned int return_write_val(struct phy_device *phy_dev, int rd_addr)
 {
 	int rd_data;
@@ -317,13 +300,55 @@ static unsigned int phy_tst_write(struct phy_device *phy_dev, unsigned int wr_ad
 	return 0;
 }
 
+
+
+static unsigned int phy_tst_read(struct phy_device *phy_dev, unsigned int rd_addr)
+{
+	unsigned int rd_data_hi;
+	unsigned int rd_data = 0;
+
+	/*init*/
+	phy_write(phy_dev, MDIO_DEVAD_NONE, 20, 0x0000);
+	phy_write(phy_dev, MDIO_DEVAD_NONE, 20, 0x0400);
+	phy_write(phy_dev, MDIO_DEVAD_NONE, 20, 0x0000);
+	phy_write(phy_dev, MDIO_DEVAD_NONE, 20, 0x0400);
+
+	if (rd_addr <= 31) {
+		phy_write(phy_dev, MDIO_DEVAD_NONE, 20,
+			((1 << 15) | (1 << 10) | ((rd_addr & 0x1f) << 5)));
+
+		rd_data = phy_read(phy_dev, MDIO_DEVAD_NONE, 21);
+		rd_data_hi = phy_read(phy_dev, MDIO_DEVAD_NONE, 22);
+		rd_data = ((rd_data_hi & 0xffff) << 16) | rd_data;
+		printf("read tstcntl phy [reg_%d] 0x%x\n", rd_addr, rd_data);
+	} else {
+		printf("Invalid parameter\n");
+	}
+
+	return rd_data;
+}
+#endif
+
 void DM_network_interface_setup_final(struct phy_device *phydev)
 {
+	#if 0
+	unsigned int reg_val;
+
 	if (internal_phy) {
 		phy_tst_write(phydev, 0x18, 0x8);
 		phy_tst_write(phydev, 0x16, 0x8400);
 		phy_tst_write(phydev, 0x15, 0x4408);
 	}
+
+	reg_val = phy_tst_read(phydev, 20);
+	printf("A3_CONFIG=0x%X\n", reg_val);
+	reg_val = phy_tst_read(phydev, 21);
+	printf("A4_CONFIG=0x%X\n", reg_val);
+	reg_val = phy_tst_read(phydev, 22);
+	printf("A5_CONFIG=0x%X\n", reg_val);
+	reg_val = phy_tst_read(phydev, 23);
+	printf("A6_CONFIG=0x%X\n", reg_val);
+	#endif
 }
 
 #endif

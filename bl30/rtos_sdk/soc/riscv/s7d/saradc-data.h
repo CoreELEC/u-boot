10c7dfe0b009a570ddd2e7acc1f7c97391c1c4da
Huqiang Qin <huqiang.qin@amlogic.com>
2024-04-10 14:50:04 +0800

    SoC s7d : saradc : support saradc [1/1]
    PD#SWPL-163676
    
    Problem:
    S7D Silicon Bringup.
    
    Solution:
    Add a new SARADC driver.
    
    Verify:
    S7D/BM201
    
    Change-Id: Iab1d5ba729bdbdda250327e7bb40c109ceb1b5a7
    Signed-off-by: Huqiang Qin <huqiang.qin@amlogic.com>

--- bl30/rtos_sdk/soc/riscv/s7d/saradc-data.h
+++ bl30/rtos_sdk/soc/riscv/s7d/saradc-data.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021-2022 Amlogic, Inc. All rights reserved.
+ * Copyright (c) 2021-2024 Amlogic, Inc. All rights reserved.
  *
  * SPDX-License-Identifier: MIT
  */
@@ -12,13 +12,9 @@
 #define SAR_CLK_BASE CLKCTRL_SAR_CLK_CTRL0
 #define SARADC_BASE SAR_ADC_REG0
 
-#define REG11_VREF_EN_VALUE 0
-#define REG11_CMV_SEL_VALUE 0
-#define REG11_EOC_VALUE 1
-
 /* s7d saradc interrupt num */
 #define SARADC_INTERRUPT_NUM 181
 
-#define SARADC_REG_NUM (18 + 1)
+#define SARADC_REG_NUM (21 + 1)
 
 #endif

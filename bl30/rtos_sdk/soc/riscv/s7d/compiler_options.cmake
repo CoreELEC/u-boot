9125283fd6c8ed8f8c526292d71ced2cbfa4ca1b
shijie.xiong <shijie.xiong@amlogic.com>
2024-05-08 16:06:51 +0800

    SoC common : soc: RISC-V compilation toolchain modification [2/4]
    PD#SWPL-166566
    
    Problem:
    Different Versions of Toolchain Required Based on Chip Configuration
    
    Solution:
    Modified the Process of Finding RISC-V Toolchain
    
    Verify:
    N/A
    
    Change-Id: I7b83b1a7fe8d8d92e8a0b7405d4a044b7d713cbe
    Signed-off-by: shijie.xiong <shijie.xiong@amlogic.com>

--- bl30/rtos_sdk/soc/riscv/s7d/compiler_options.cmake
+++ bl30/rtos_sdk/soc/riscv/s7d/compiler_options.cmake
@@ -3,3 +3,5 @@
 # SPDX-License-Identifier: MIT
 
 #add compiler options at here which is associated with soc.
+
+set(ENV{TOOLCHAIN_PATH} /opt/riscv-none-gcc/8.2.0-2.2-20190521-0004)
--- bl30/rtos_sdk/soc/riscv/t3x/compiler_options.cmake
+++ bl30/rtos_sdk/soc/riscv/t3x/compiler_options.cmake
@@ -3,3 +3,5 @@
 # SPDX-License-Identifier: MIT
 
 #add compiler options at here which is associated with soc.
+
+set(ENV{TOOLCHAIN_PATH} /opt/riscv-none-gcc/7.2.0-4-20180606-1631)

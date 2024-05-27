5688de8d5fa4371611942e40d700a6210942030f
Bo Lv <bo.lv@amlogic.com>
2024-05-27 15:02:34 +0800

    fip: s6: run device sign boot flow [1/1]
    PD#SWPL-170220
    
    Problem:
    run signature boot flow
    
    Solution:
    fix it
    
    Verify:
    s6
    
    Change-Id: I9ecff7abc4ae35e968f7cde22c903a7c9b2c5a08
    Signed-off-by: Bo Lv <bo.lv@amlogic.com>

--- fip/s6/bin/device-vendor-scs-signing.sh
+++ fip/s6/bin/device-vendor-scs-signing.sh
@@ -131,7 +131,7 @@ function mk_uboot() {
 	rm -f ${file_info_cfg}
 	mv -f ${file_info_cfg}.sha256 ${file_info_cfg}
 
-	dd if=${file_info_cfg} of=${bootloader} bs=512 seek=404 conv=notrunc status=none
+	dd if=${file_info_cfg} of=${bootloader} bs=512 seek=540 conv=notrunc status=none
 
 	if [ ${storage_type_suffix} == ".sto" ]; then
 		total_size=$[total_size+512]

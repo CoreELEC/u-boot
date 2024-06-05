78b081e05b177e85e9f8a6d6dc89160b126a9f83
Huqiang Qin <huqiang.qin@amlogic.com>
2024-06-05 14:17:03 +0800

    gpio: s7: push-pull the TEST_N output high to make VDDCPU_EN more stable [1/1]
    PD#SWPL-171290
    
    Problem:
    Reference HW recommended modifications.
    
    Solution:
    Push-pull the TEST_N output high to make VDDCPU_EN more stable.
    
    Verify:
    S7/BH201
    
    Change-Id: I292fe8f1233d1af97553e5bd2f05cc3dd2e9d03a
    Signed-off-by: Huqiang Qin <huqiang.qin@amlogic.com>

--- bl33/v2023/board/amlogic/s7_t223/firmware/timing.c
+++ bl33/v2023/board/amlogic/s7_t223/firmware/timing.c
@@ -160,6 +160,9 @@ bl2_reg_t __bl2_reg[] __attribute__ ((section(".generic_param"))) = {
 /* gpio/pinmux/pwm init */
 register_ops_t __bl2_ops_reg[MAX_REG_OPS_ENTRIES]
 __attribute__ ((section(".misc_param"))) = {
+	/* Push-pull the TEST_N output high to make VDDCPU_EN more stable */
+	{ PADCTRL_TESTN_O,         (0x1 << 0),    (0x1 << 0), 0, 0, 0 },
+	{ PADCTRL_TESTN_OEN,       (0x0 << 0),    (0x1 << 0), 0, 0, 0 },
 	/* config vmin_ft value */
 	{ 0, VMIN_SS_VALUE, 0xffffffff, 0, BL2_INIT_STAGE_VMIN_FLAG_1, 0 },
 	{ 0, VMIN_TT_VALUE, 0xffffffff, 0, BL2_INIT_STAGE_VMIN_FLAG_2, 0 },

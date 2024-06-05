38d92f73c25da2ac17b367f1c7fa2ba4b13985ce
Zelong Dong <zelong.dong@amlogic.com>
2024-06-05 11:07:52 +0000

    bl33: disable GPIOH_3 pull_en [1/1]
    PD#SWPL-164140
    
    Problem:
    disable GPIOH_3 pull_en
    
    Solution:
    disable GPIOH_3 pull_en
    
    Verify:
    s7d_bm202
    
    Change-Id: I8527dd978be07f137bdf310961711d9d19aa4ddb
    Signed-off-by: Zelong Dong <zelong.dong@amlogic.com>

--- bl33/v2023/board/amlogic/s7d_bm201/firmware/timing.c
+++ bl33/v2023/board/amlogic/s7d_bm201/firmware/timing.c
@@ -175,6 +175,7 @@ __attribute__ ((section(".misc_param"))) = {
 	{ PADCTRL_PIN_MUX_REGI,	   (0x3 << 4),	  (0xf << 4), 0, 0, 0 },
 	{ PADCTRL_GPIOD_PULL_UP,   (0x1 << 2),	  (0x1 << 2), 0, 0, 0 },
 	{ PWM_TEE_ONLY_J,          (0x1 << 0),	  (0xffffffff << 0), 0, 0, 0 },
+	/* disable GPIOH_3 Pull */
 	{ PADCTRL_GPIOH_PULL_EN,   (0x0 << 3),    (0x1 << 3), 0, 0, 0 },
 #ifdef CONFIG_NOVERBOSE_BUILD
 	/* use acs flag to disable uart print in each blx
--- bl33/v2023/board/amlogic/s7d_bm202/firmware/timing.c
+++ bl33/v2023/board/amlogic/s7d_bm202/firmware/timing.c
@@ -175,6 +175,8 @@ __attribute__ ((section(".misc_param"))) = {
 	{ PADCTRL_PIN_MUX_REGI,	   (0x3 << 4),	  (0xf << 4), 0, 0, 0 },
 	{ PADCTRL_GPIOD_PULL_UP,   (0x1 << 2),	  (0x1 << 2), 0, 0, 0 },
 	{ PWM_TEE_ONLY_J,          (0x1 << 0),	  (0xffffffff << 0), 0, 0, 0 },
+	/* disable GPIOH_3 Pull */
+	{ PADCTRL_GPIOH_PULL_EN,   (0x0 << 3),    (0x1 << 3), 0, 0, 0 },
 #ifdef CONFIG_NOVERBOSE_BUILD
 	/* use acs flag to disable uart print in each blx
 	 * reg must be UART_B_WFIFO, flags: 1 --> disable uart print, 0: enable

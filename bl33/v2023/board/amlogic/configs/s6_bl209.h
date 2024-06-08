4a3a32f0bdbd3e642dc1b686b9175a6495db0e39
Bo Lv <bo.lv@amlogic.com>
2024-06-08 15:01:34 +0800

    bl33: s6: silicon bringup [1/1]
    PD#SWPL-170220
    
    Problem:
    s6 silicon bringup
    
    Solution:
    s6 silicon bringup
    
    Verify:
    s6
    
    Change-Id: I0994cb8c92fb0fb09478feae630159ddf2ca7da4
    Signed-off-by: Bo Lv <bo.lv@amlogic.com>

--- bl33/v2023/board/amlogic/configs/s6_bl209.h
+++ bl33/v2023/board/amlogic/configs/s6_bl209.h
@@ -46,6 +46,10 @@
 #define AML_ADC_POWER_KEY_CHAN   2  /*channel range: 0-7*/
 #define AML_ADC_POWER_KEY_VAL    0  /*sample value range: 0-1023*/
 
+/* AVB */
+#define CONFIG_AML_AVB2_ANTIROLLBACK 1
+#define CONFIG_AVB_VERIFY 1
+
 /* Bootloader Control Block function
    That is used for recovery and the bootloader to talk to each other
 */
@@ -55,6 +59,12 @@
 
 /* args/envs */
 #define CONFIG_SYS_MAXARGS  64
+
+/* AVB */
+#define CONFIG_AML_AVB2_ANTIROLLBACK 1
+#define CONFIG_AVB_VERIFY 1
+#define CONFIG_SUPPORT_EMMC_RPMB 1
+#define CONFIG_AML_DEV_ID 1
 
 //for common env list, please maintain it in board/amlogic/env/linux.env
 #define CONFIG_EXTRA_ENV_SETTINGS \
@@ -88,7 +98,7 @@
 	"dv_fw_dir=/reserved/firmware/dovi_fw.bin\0"\
 	"hdr_policy=0\0"\
 	"frac_rate_policy=1\0"\
-	"board=s7d_skt\0"
+	"board=s6_bl209\0"
 
 #ifndef CONFIG_PXP_EMULATOR
 #define CONFIG_PREBOOT  \
@@ -102,7 +112,7 @@
 #else
 #define CONFIG_PREBOOT  "echo preboot"
 #endif
-#define CONFIG_ENV_IS_NOWHERE  1
+#define CONFIG_ENV_IS_NOWHERE  0
 //#define CONFIG_ENV_SIZE   (64 * 1024)
 #define CONFIG_FIT 1
 #define CONFIG_OF_LIBFDT 1

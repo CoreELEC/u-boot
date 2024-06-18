38673c93c7ca0c44eccd2b3af2a80b8e3ad8de88
ruofei.zhao <ruofei.zhao@amlogic.com>
2024-06-18 17:40:49 +0800

    hdmitx21: update hdmitx phy para [1/2]
    PD#SWPL-172957
    
    Problem:
    update hdmitx phy para
    
    Solution:
    update hdmitx phy para
    
    Verify:
    S6-BL201
    
    Change-Id: Ib14684235e6e7f01952864c17ab34b7884a7d8f3
    Signed-off-by: ruofei.zhao <ruofei.zhao@amlogic.com>

--- bl33/v2023/arch/arm/mach-meson/s6/hdmitx_hw.c
+++ bl33/v2023/arch/arm/mach-meson/s6/hdmitx_hw.c
@@ -340,20 +340,20 @@ do { \
 	case HDMI_PHYPARA_6G:	/* 5.94/4.5/3.7Gbps */
 	case HDMI_PHYPARA_4p5G:
 	case HDMI_PHYPARA_3p7G:
-		hd21_write_reg(ANACTRL_HDMIPHY_CTRL0, 0x8003a8fb);
-		hd21_write_reg(ANACTRL_HDMIPHY_CTRL5, 0x1555);
+		hd21_write_reg(ANACTRL_HDMIPHY_CTRL0, 0x8003cafb);
+		hd21_write_reg(ANACTRL_HDMIPHY_CTRL5, 0x2555);
 		hd21_write_reg(ANACTRL_HDMIPHY_CTRL3, 0x4ef001);
 		break;
 	case HDMI_PHYPARA_3G:	/* 2.97Gbps */
 		hd21_write_reg(ANACTRL_HDMIPHY_CTRL0, 0x800380dd);
-		hd21_write_reg(ANACTRL_HDMIPHY_CTRL5, 0x1555);
+		hd21_write_reg(ANACTRL_HDMIPHY_CTRL5, 0x2555);
 		hd21_write_reg(ANACTRL_HDMIPHY_CTRL3, 0x4ef001);
 		break;
 	case HDMI_PHYPARA_270M:	/* 1.485Gbps, and below */
 	case HDMI_PHYPARA_DEF:
 	default:
-		hd21_write_reg(ANACTRL_HDMIPHY_CTRL0, 0x820380a0);
-		hd21_write_reg(ANACTRL_HDMIPHY_CTRL5, 0x1555);
+		hd21_write_reg(ANACTRL_HDMIPHY_CTRL0, 0x82038088);
+		hd21_write_reg(ANACTRL_HDMIPHY_CTRL5, 0x2555);
 		hd21_write_reg(ANACTRL_HDMIPHY_CTRL3, 0x4ef001);
 		break;
 	}

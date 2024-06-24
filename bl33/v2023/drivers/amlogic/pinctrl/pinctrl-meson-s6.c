58b376ff7cf492a6900789840968f79f2d899cf9
Qianggui Song <qianggui.song@amlogic.com>
2024-06-24 08:18:31 +0000

    pinctrl: fix some pins definition [1/1]
    PD#SWPL-172940
    
    Problem:
    definition of some pins are wrong.
    
    Solution:
    porting fixing from kernel
    
    Verify:
    s6_bl201
    
    Change-Id: I2fa93a7b23c1ea6f514cc2ea694645b51e9da785
    Signed-off-by: Qianggui Song <qianggui.song@amlogic.com>

--- bl33/v2023/drivers/amlogic/pinctrl/pinctrl-meson-s6.c
+++ bl33/v2023/drivers/amlogic/pinctrl/pinctrl-meson-s6.c
@@ -207,7 +207,7 @@ static const unsigned int tsin_b_clk_c_pins[]			= { GPIOC_0 };
 static const unsigned int tsin_b_sop_c_pins[]			= { GPIOC_1 };
 static const unsigned int tsin_b_valid_c_pins[]			= { GPIOC_2 };
 static const unsigned int tsin_b_din0_c_pins[]			= { GPIOC_3 };
-static const unsigned int tsin_b_d3_c_pins[]			= { GPIOC_4 };
+static const unsigned int tdm_d3_c_pins[]			= { GPIOC_4 };
 
 /* GPIOC func7 */
 static const unsigned int iso7816_clk_c0_pins[]			= { GPIOC_0 };
@@ -500,8 +500,8 @@ static const unsigned int ir_remote_out_a_pins[]		= { GPIOA_15 };
 /* GPIOA func4 */
 static const unsigned int iso7816_clk_a_pins[]			= { GPIOA_0 };
 static const unsigned int iso7816_data_a_pins[]			= { GPIOA_1 };
-static const unsigned int i2c5_scl_a14_pins[]			= { GPIOA_14 };
-static const unsigned int i2c5_sda_a15_pins[]			= { GPIOA_15 };
+static const unsigned int i2c5_sda_a14_pins[]			= { GPIOA_14 };
+static const unsigned int i2c5_scl_a15_pins[]			= { GPIOA_15 };
 
 /* GPIOA func5 */
 static const unsigned int eth_link_led_a_pins[]			= { GPIOA_14 };
@@ -812,7 +812,7 @@ static struct meson_pmx_group meson_s6_periphs_groups[] = {
 	GROUP(tsin_b_sop_c,		6),
 	GROUP(tsin_b_valid_c,		6),
 	GROUP(tsin_b_din0_c,		6),
-	GROUP(tsin_b_d3_c,		6),
+	GROUP(tdm_d3_c,			6),
 
 	/* GPIOC func7 */
 	GROUP(iso7816_clk_c0,		7),
@@ -1105,8 +1105,8 @@ static struct meson_pmx_group meson_s6_periphs_groups[] = {
 	/* GPIOA func4 */
 	GROUP(iso7816_clk_a,		4),
 	GROUP(iso7816_data_a,		4),
-	GROUP(i2c5_scl_a14,		4),
-	GROUP(i2c5_sda_a15,		4),
+	GROUP(i2c5_sda_a14,		4),
+	GROUP(i2c5_scl_a15,		4),
 
 	/* GPIOA func5 */
 	GROUP(eth_link_led_a,		5),
@@ -1186,7 +1186,7 @@ static const char * const i2c4_groups[] = {
 
 static const char * const i2c5_groups[] = {
 	"i2c5_scl_z", "i2c5_sda_z", "i2c5_scl_a10", "i2c5_sda_a11",
-	"i2c5_scl_a14", "i2c5_sda_a15"
+	"i2c5_sda_a14", "i2c5_scl_a15"
 };
 
 static const char * const ir_remote_groups[] = {
@@ -1268,7 +1268,7 @@ static const char * const tsin_groups[] = {
 	"tsin_a_sop_d", "tsin_a_din0_d", "tsin_a_clk_d", "tsin_a_valid_d",
 	"tsin_a_clk_c", "tsin_a_sop_c", "tsin_a_valid_c", "tsin_a_din0_c",
 	"tsin_b_clk_c", "tsin_b_sop_c", "tsin_b_valid_c", "tsin_b_din0_c",
-	"tsin_b_d3_c", "tsin_b_din0_x0", "tsin_b_sop_x1", "tsin_b_valid_x2",
+	"tsin_b_din0_x0", "tsin_b_sop_x1", "tsin_b_valid_x2",
 	"tsin_b_clk_x3", "tsin_b_sop_x8", "tsin_b_valid_x9", "tsin_b_din0_x10",
 	"tsin_b_clk_x11", "tsin_a_din0_z", "tsin_a_sop_z", "tsin_a_valid_z",
 	"tsin_a_clk_z", "tsin_b_sop_z4", "tsin_b_valid_z5", "tsin_b_din0_z6",
@@ -1293,7 +1293,7 @@ static const char * const tdm_groups[] = {
 	"tdm_d9_z", "tdm_d10_z", "tdm_d11_z", "tdm_d2_a",
 	"tdm_d3_a", "tdm_d5_a", "tdm_d6_a", "tdm_d7_a", "tdm_fs2_a",
 	"tdm_sclk2_a", "tdm_sclk1_a", "tdm_fs1_a", "tdm_d8_a", "tdm_d9_a",
-	"tdm_d10_a14", "tdm_d11_a15", "tdm_d10_a7", "tdm_d11_a8"
+	"tdm_d10_a14", "tdm_d11_a15", "tdm_d10_a7", "tdm_d11_a8", "tdm_d3_c"
 };
 
 static const char * const mclk_groups[] = {
@@ -1376,6 +1376,10 @@ static const char * const eth_groups[] = {
 	"eth_link_led_a", "eth_act_led_a"
 };
 
+static const char * const hdmitx_groups[] = {
+	"hdmitx_sda", "hdmitx_scl", "hdmitx_hpd_in"
+};
+
 static struct meson_pmx_func meson_s6_periphs_functions[] = {
 	FUNCTION(gpio_periphs),
 	FUNCTION(uart_a),
@@ -1418,7 +1422,8 @@ static struct meson_pmx_func meson_s6_periphs_functions[] = {
 	FUNCTION(eth),
 	FUNCTION(mic_mute),
 	FUNCTION(clk_32k_in),
-	FUNCTION(ao_cec)
+	FUNCTION(ao_cec),
+	FUNCTION(hdmitx)
 };
 
 static struct meson_bank meson_s6_periphs_banks[] = {

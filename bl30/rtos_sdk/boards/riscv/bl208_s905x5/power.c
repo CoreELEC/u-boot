d8302aba849bbd6ec8fae210f9141b99fb8f5072
Zhu Lv <zhu.lv@amlogic.com>
2024-07-22 02:32:41 +0000

    SoC: S6: add bt wakeup sample [1/1]
    PD#SWPL-178249
    
    Problem:
    bt wakeup fail
    
    Solution:
    add bt wakeup sample
    
    Verify:
    S6
    
    Change-Id: Ia97eb47d1c894b21d773a4556fea144764200a89
    Signed-off-by: Zhu Lv <zhu.lv@amlogic.com>

--- bl30/rtos_sdk/boards/riscv/bl208_s905x5/power.c
+++ bl30/rtos_sdk/boards/riscv/bl208_s905x5/power.c
@@ -18,7 +18,9 @@
 #include "pwm_plat.h"
 #include "keypad.h"
 #include "timer_source.h"
-#include "wakeup.h"
+#if CONFIG_WIFI_BT_WAKE
+#include "wifi_bt_wake.h"
+#endif
 #include "power.h"
 #include "mailbox-api.h"
 #include "board_common.h"
@@ -85,7 +87,10 @@ void str_hw_init(void)
 	vBackupAndClearGpioIrqReg();
 	vGpioIRQInit();
 	vKeyPadInit();
-	//Bt_GpioIRQRegister();
+
+#if CONFIG_WIFI_BT_WAKE
+	wifi_bt_wakeup_init();
+#endif
 }
 
 void str_hw_disable(void)
@@ -98,7 +103,11 @@ void str_hw_disable(void)
 		vTaskDelete(cecTask);
 		cec_req_irq(0);
 	}
-	//Bt_GpioIRQFree();
+
+#if CONFIG_WIFI_BT_WAKE
+	wifi_bt_wakeup_deinit();
+#endif
+
 	vKeyPadDeinit();
 	vRestoreGpioIrqReg();
 }

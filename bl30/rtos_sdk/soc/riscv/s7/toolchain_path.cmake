1b0fbaf39b3718913f03c942ea6bea074097a1d8
shijie.xiong <shijie.xiong@amlogic.com>
2024-07-26 15:37:03 +0800

    SoC common : soc: add compiler path settings. [2/2]
    PD#SWPL-168549
    
    Problem:
    Compiler architecture adjustments causing failure to
    properly recognize compiler version.
    
    Solution:
    Add compiler path settings code.
    
    Verify:
    N/A
    
    Change-Id: I283962cbd6f157b9c7241b0e82228c5d7413ddfa
    Signed-off-by: shijie.xiong <shijie.xiong@amlogic.com>

---
+++ bl30/rtos_sdk/soc/riscv/s7/toolchain_path.cmake
@@ -0,0 +1,6 @@
+# Copyright (c) 2021-2022 Amlogic, Inc. All rights reserved.
+
+# SPDX-License-Identifier: MIT
+
+# Add the toolchain path here
+set(ENV{TOOLCHAIN_PATH} /opt/riscv-none-gcc/7.2.0-4-20180606-1631)

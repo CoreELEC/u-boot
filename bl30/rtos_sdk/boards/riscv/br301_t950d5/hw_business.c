cf8efe91ae47a0e1cd406388ec8e980e63df9fd8
bangzheng.liu <bangzheng.liu@amlogic.com>
2024-07-26 12:42:36 +0800

    SoC t6d: bl30 add board br301, br309 [1/1]
    PD#SWPL-179190
    
    Problem:
    bl30 t6d board is only t6d_skt.
    
    Solution:
    add board br301_t950d5, br309_t950d5.
    
    Verify:
    local build all board pass.
    
    Change-Id: Ife02859d978d5ca8496ff5461d13bb13e2e29563
    Signed-off-by: bangzheng.liu <bangzheng.liu@amlogic.com>

---
+++ bl30/rtos_sdk/boards/riscv/br301_t950d5/hw_business.c
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2021-2022 Amlogic, Inc. All rights reserved.
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#include <stdio.h>
+#include "hw_business.h"
+#include "uart.h"
+#include "eth.h"
+#include "common.h"
+#include "mailbox-api.h"
+#include "hdmi_cec.h"
+#include "suspend.h"
+#include "vrtc.h"
+#include "hwspinlock.h"
+
+void hw_business_process(void)
+{
+	vMbInit();
+	//vCecCallbackInit(CEC_CHIP_T6D);
+	vRtcInit();
+	//rtc_init();
+	vETHMailboxCallback();
+	create_str_task();
+#ifdef CONFIG_HWSPINLOCK
+	vHwLockInit(HW_SPIN_LOCK0, 0);
+#endif
+}

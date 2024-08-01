61d712f48e34c8975c83f819a4a47e7107e0e051
Lawrence Mok <lawrence.mok@amlogic.com>
2024-08-01 22:13:26 +0000

    cmd: s6: add efuse_obj encrypted data write command [1/1]
    PD#SWPL-180100
    
    Problem:
    Need to support writing encrypted data from uboot
    
    Solution:
    add efuse_obj enc_set FIELD DATA command
    
    Verify:
    run efuse_obj enc_set FIELD DATA on s7
    
    Change-Id: I5e3ec43484d169da8d20e76d5784d3620177cbaa
    Signed-off-by: Lawrence Mok <lawrence.mok@amlogic.com>

--- bl33/v2023/arch/arm/include/asm/amlogic/arch-s6/efuse.h
+++ bl33/v2023/arch/arm/include/asm/amlogic/arch-s6/efuse.h
@@ -138,6 +138,7 @@ uint32_t meson_efuse_obj_read(uint32_t obj_id, uint8_t *buff, uint32_t *size);
 uint32_t efuse_obj_write(uint32_t obj_id, char *name, uint8_t *buff, uint32_t size);
 uint32_t efuse_obj_read(uint32_t obj_id, char *name, uint8_t *buff, uint32_t *size);
 uint32_t efuse_obj_set_data(char *name, char *data);
+uint32_t efuse_obj_set_enc_data(char *name, char *data);
 uint32_t efuse_obj_set_license(char *name);
 uint32_t efuse_obj_set_enc_data(char *name, char *data);
 uint32_t efuse_obj_lock(char *name);

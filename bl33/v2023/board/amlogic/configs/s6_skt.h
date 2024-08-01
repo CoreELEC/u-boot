dff45da16d236d3828ab8afdd400365f09f7749a
chao.zhang <chao.zhang@amlogic.com>
2024-08-01 16:09:33 +0800

    boot voltage: set ee/ddr same voltage [1/1]
    PD#SWPL-180839
    
    Problem:
    ee and ddr need same voltage of s6
    
    Solution:
    set ddr/ee to 750mv
    
    Verify:
    s6
    
    Change-Id: If811c2e60a01c914605bbf5ee039a0c8dad7f975
    Signed-off-by: chao.zhang <chao.zhang@amlogic.com>

--- bl33/v2023/board/amlogic/configs/s6_skt.h
+++ bl33/v2023/board/amlogic/configs/s6_skt.h
@@ -14,9 +14,9 @@
 
 #define AML_VCCK_INIT_VOLTAGE	  950	    //VCCK power up voltage
 #define AML_VDDEE_INIT_VOLTAGE    800       // VDDEE power up voltage
-#define AML_VDDDDR_INIT_VOLTAGE    780       // VDDDDR power up voltage
+#define AML_VDDDDR_INIT_VOLTAGE    800       // VDDDDR power up voltage
 /*Distinguish whether to use efuse to adjust vddee*/
-#define CONFIG_PDVFS_ENABLE
+//#define CONFIG_PDVFS_ENABLE
 
 /* SMP definitions */
 #define CPU_RELEASE_ADDR		secondary_boot_func

fee8f03037160527655e66d62cc3e7367ea3e4f3
Feng Chen <feng.chen@amlogic.com>
2024-08-05 17:38:53 +0800

    spinand: F35SQA001G QPL qualify [1/1]
    PD#SWPL-148920
    
    Problem:
    STB QPL qualify
    
    Solution:
    add F35SQA001G to flash id table.
    
    Verify:
    S1A_BG201
    
    Change-Id: I0ea126d6004da388fe2e0b5d09e596799801ad59
    Signed-off-by: Feng Chen <feng.chen@amlogic.com>

--- bl33/v2023/drivers/amlogic/mtd/nand/spi/core.c
+++ bl33/v2023/drivers/amlogic/mtd/nand/spi/core.c
@@ -40,6 +40,8 @@
 #include <amlogic/storage.h>
 #endif
 
+extern const struct spinand_manufacturer foresee_spinand_manufacturer;
+
 /* SPI NAND index visible in MTD names */
 static int spi_nand_idx;
 #ifdef CONFIG_AML_STORAGE
@@ -904,6 +906,7 @@ static const struct spinand_manufacturer *spinand_manufacturers[] = {
 	&micron_spinand_manufacturer,
 	&toshiba_spinand_manufacturer,
 	&winbond_spinand_manufacturer,
+	&foresee_spinand_manufacturer,
 };
 
 static int spinand_manufacturer_detect(struct spinand_device *spinand)

0e8296d5592f815520f24f924105491ba284d4f9
bowen cheng <bowen.cheng@amlogic.com>
2024-08-12 14:50:44 +0800

    SoC t6d : cec: t6d bl30 cec bringup [1/3]
    PD#SWPL-181291
    
    Problem:
    t6d bl30 cec bringup
    
    Solution:
    t6d bl30 cec bringup
    
    Verify:
    t6d
    
    Change-Id: Id4ad346526c93522e943457c6e647457bd19367f
    Signed-off-by: bowen cheng <bowen.cheng@amlogic.com>

--- bl30/rtos_sdk/soc/riscv/t6d/cec-data.h
+++ bl30/rtos_sdk/soc/riscv/t6d/cec-data.h
@@ -7,7 +7,7 @@
 #ifndef __T6D_CEC_DATA_H__
 #define __T6D_CEC_DATA_H__
 
-#define CEC_PIN_MX GPIOW_16
+#define CEC_PIN_MX GPIOW_12
 #define CEC_PIN_FUNC PIN_FUNC1
 #define CEC_IP 1 /* 0: cec a, 1: cecb*/
 

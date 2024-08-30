e6a382cbadc99014fee9d53842313e0a660d52c1
wenbo.wang <wenbo.wang@amlogic.com>
2024-08-30 11:24:57 +0800

    t6d: BAUDRATE need to befixed  [1/2]
    PD#SWPL-182343
    
    Problem:
    BAUDRATE need to be fixed
    
    Solution:
    CONFIG_BAUDRATE=115200
    
    Verify:
    t6d
    
    Change-Id: Ia850fc16c7036d750a7c2809d4a315e562e90ade
    Signed-off-by: wenbo.wang <wenbo.wang@amlogic.com>

--- bl33/v2023/board/amlogic/configs/t6d_br301.h
+++ bl33/v2023/board/amlogic/configs/t6d_br301.h
@@ -64,7 +64,6 @@
 #define CONFIG_EXTRA_ENV_SETTINGS \
 	"uart_base=0xfe078400\0"\
 	"usb_burning=" CONFIG_USB_TOOL_ENTRY "\0"\
-	"baudrate=115200\0"\
 	"model_name=FHD2HDMI\0"\
 	"panel_type=lvds_0\0"\
 	"model_name=FHD2HDMI\0" \

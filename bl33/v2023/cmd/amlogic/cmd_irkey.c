889fda4c0e56391efe92b71a00a57fe0353dc848
Zelong Dong <zelong.dong@amlogic.com>
2024-09-06 08:35:50 +0000

    bl33: ir: support mbox for some func [1/1]
    PD#SWPL-182441
    
    Problem:
    need to support mbox for some func
    
    Solution:
    1. get wakeup framecode
    2. enable/disable debug log
    3. set wakeup key table
    4. enable/disable bl30 ir
    5. get the last powerkey
    
    Verify:
    t6d_br301
    
    Change-Id: I0092b96e345584254d13132397e582a053125567
    Signed-off-by: Zelong Dong <zelong.dong@amlogic.com>

--- bl33/v2023/cmd/amlogic/cmd_irkey.c
+++ bl33/v2023/cmd/amlogic/cmd_irkey.c
@@ -9,9 +9,20 @@
 #include <command.h>
 #include <errno.h>
 #include <misc.h>
+#include <asm/amlogic/arch/mailbox.h>
 
 #define KEY_PARAMS_NUM  10
 #define OTHER_PARAMS_NUM 1
+
+#define MAX_KEY_NUM 16
+
+enum IrMboxCmdType {
+	IR_MBOX_CMD_SET_WAKEUP_LIST,
+	IR_MBOX_CMD_SET_DEBUG_LOG,
+	IR_MBOX_CMD_GET_WAKEUP_KEY,
+	IR_MBOX_CMD_SET_STATUS,
+	IR_MBOX_CMD_GET_PREBOOT_KEY
+};
 
 extern u32 get_time(void);
 
@@ -68,8 +79,49 @@ static int do_irkey(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
 	return -EINVAL;
 }
 
+static int do_irkey_wakeup(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
+{
+	char *endp;
+	u32 data[MAX_KEY_NUM + 3];
+	u32 framecode, ret;
+	char str[16] = {0};
+	u8 i;
+
+	if (argc < 2)
+		return CMD_RET_USAGE;
+
+	for (i = 1; i < argc; i++) {
+		data[i - 1] = simple_strtoul(argv[i], &endp, 0);
+		if (*argv[i] == 0 || *endp != 0)
+			return -EINVAL;
+	}
+
+	ret = scpi_send_data(AOCPU_REE_CHANNEL, CMD_SET_IR_STATE, data,
+			     sizeof(data), &framecode, sizeof(framecode));
+	if (ret != 0)
+		printf("CMD_SET_IR_STATE failed\n");
+
+	snprintf(str, sizeof(str), "0x%x", framecode);
+	if (data[0] == IR_MBOX_CMD_GET_WAKEUP_KEY)
+		env_set("ir_wakeup_key", str);
+	else if (data[0] == IR_MBOX_CMD_GET_PREBOOT_KEY)
+		env_set("ir_preboot_key", str);
+
+	return 0;
+}
+
 /*Max key arguments: 10*/
 U_BOOT_CMD(irkey, (KEY_PARAMS_NUM + OTHER_PARAMS_NUM + 1), 0, do_irkey,
 	"irkey <timeout> <key1> ...<keyN> - maximum value of N: 10",
 	NULL
+);
+
+U_BOOT_CMD(irkey_wakeup, (MAX_KEY_NUM + 3 + 1), 0, do_irkey_wakeup,
+	"irkey_wakeup",
+	"0 <key_num> <reason_bitmap> <key0> <key1> ... <key15>\n"
+	"			- set wakeup table\n"
+	"irkey_wakeup 1 <1|0>	- enable/disable bl30 ir debug log\n"
+	"irkey_wakeup 2		- get ir_wakeup_key\n"
+	"irkey_wakeup 3 <1|0>	- enable/disable bl30 ir\n"
+	"irkey_wakeup 4		- get ir_preboot_key\n"
 );

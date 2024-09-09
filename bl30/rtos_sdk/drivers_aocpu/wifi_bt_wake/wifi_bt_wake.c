a6ba0aaff757e0054885e713a50d6c7056323969
zuorong hong <zuorong.hong@amlogic.com>
2024-09-09 06:16:03 +0000

    SoC common: bt: fix wakeup_special func false wakeup [1/1]
    PD#SWPL-183713
    
    Problem:
    wakeup_special false wakeup when netflix resume.
    
    Solution:
    fix code logic,wakeup_special delay run.
    
    Verify:
    loki
    
    Change-Id: Icffcf7b8bc41af6640b1116c37b1ae8f11e50f4e
    Signed-off-by: zuorong hong <zuorong.hong@amlogic.com>

--- bl30/rtos_sdk/drivers_aocpu/wifi_bt_wake/wifi_bt_wake.c
+++ bl30/rtos_sdk/drivers_aocpu/wifi_bt_wake/wifi_bt_wake.c
@@ -45,6 +45,7 @@ do { \
 
 #define ONE_TICK_MS (1000 / configTICK_RATE_HZ)
 #define BT_DELAY_INTERVAL 40
+#define BT_WAKEUP_SPECIAL_DELAY 1000
 #define WIFI_DELAY_INTERVAL (ONE_TICK_MS * 2)
 
 #ifdef WIFI_WAKE_CFG
@@ -86,11 +87,11 @@ static void wakeup_special_handle(uint16_t gpio_en, uint16_t gpio_wake, uint32_t
 }
 
 static void wakeup_task_init(TaskFunction_t task_callback, const char * const name,
-	TaskHandle_t * const task)
+	TaskHandle_t * const task, void * const pvParameters)
 {
 	int32_t ret;
 
-	ret = xTaskCreate(task_callback, name, configMINIMAL_STACK_SIZE, NULL, 3, task);
+	ret = xTaskCreate(task_callback, name, configMINIMAL_STACK_SIZE, pvParameters, 3, task);
 	if (ret != pdPASS)
 		INFO("%s:create failed", name);
 	else
@@ -154,6 +155,7 @@ static void wifi_wakeup_task(void *args)
 wakeup:
 		INFO("wifi wakeup");
 		STR_Wakeup_src_Queue_Send(&key_val);
+
 suspend:
 		vTaskSuspend(wifi_task);
 	}
@@ -162,6 +164,8 @@ suspend:
 
 #ifdef BT_WAKE_CFG
 static TaskHandle_t bt_task;
+static TaskHandle_t bt_special_handle;
+
 
 static void vBTWakeup(void)
 {
@@ -169,14 +173,28 @@ static void vBTWakeup(void)
 	xTaskResumeFromISR(bt_task);
 }
 
+/* For some modules, bt_wake_host level has been low since entering suspend */
+static void bt_special_task(void *args)
+{
+	unsigned int *p_flag = (unsigned int *)args;
+
+	vTaskDelay(pdMS_TO_TICKS(BT_WAKEUP_SPECIAL_DELAY));
+	INFO("p_flag = %d ", *p_flag);
+	if (!(*p_flag))
+		wakeup_special_handle(BT_EN, BT_WAKE_HOST, BT_WAKEUP);
+	else
+		INFO("found trigger falling");
+	vTaskSuspend(bt_special_handle);
+}
+
 static void bt_wakeup_task(void *args)
 {
-	uint32_t key_val, flag;
+	uint32_t key_val, flag = 0;
 
 	UNUSED(args);
-	wakeup_special_handle(BT_EN, BT_WAKE_HOST, BT_WAKEUP);
 	xGpioSetDir(BT_WAKE_HOST, GPIO_DIR_IN);
 	xRequestGpioIRQ(BT_WAKE_HOST, vBTWakeup, IRQF_TRIGGER_FALLING);
+	wakeup_task_init(bt_special_task, "bt_wakeup_special", &bt_special_handle, (void *)(&flag));
 
 	while (1) {
 		vTaskSuspend(bt_task);
@@ -218,11 +236,11 @@ static void bt_wakeup_task(void *args)
 void wifi_bt_wakeup_init(void)
 {
 #ifdef WIFI_WAKE_CFG
-	wakeup_task_init(wifi_wakeup_task, "wifi_wakeup", &wifi_task);
+	wakeup_task_init(wifi_wakeup_task, "wifi_wakeup", &wifi_task, NULL);
 #endif
 
 #ifdef BT_WAKE_CFG
-	wakeup_task_init(bt_wakeup_task, "bt_wakeup", &bt_task);
+	wakeup_task_init(bt_wakeup_task, "bt_wakeup", &bt_task, NULL);
 #endif
 }
 
@@ -233,6 +251,7 @@ void wifi_bt_wakeup_deinit(void)
 #endif
 
 #ifdef BT_WAKE_CFG
+	vTaskDelete(bt_special_handle);
 	wakeup_task_deinit(bt_task, BT_WAKE_HOST);
 #endif
 }

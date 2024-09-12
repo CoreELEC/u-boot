5d9ec195bb1b51643703b76368fe021d2d1ab838
bangzheng.liu <bangzheng.liu@amlogic.com>
2024-09-12 16:33:13 +0800

    SoC s6: bl30 support DSP suspend resume with ffv [1/2]
    PD#SWPL-184689
    
    Problem:
    DSP needs to support low power mode with ffv.
    
    Solution:
    Add mbox callback for DSP to AOCPU.
    
    Verify:
    S6
    
    Change-Id: I7cf18a6d6541af0fd1b523cc30012280062f50a1
    Signed-off-by: bangzheng.liu <bangzheng.liu@amlogic.com>

---
+++ bl30/rtos_sdk/soc/riscv/s6/dsp.c
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2021-2022 Amlogic, Inc. All rights reserved.
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#include "soc.h"
+#include "dsp.h"
+#include "suspend.h"
+#include "mailbox-api.h"
+
+extern uint32_t suspend_flag;
+static void *xMboxVadWakeup(void *msg)
+{
+	*(uint32_t *)msg = suspend_flag;
+	uint32_t buf[4] = {0};
+
+	buf[0] = VAD_WAKEUP;
+	STR_Wakeup_src_Queue_Send(buf);
+
+	return NULL;
+}
+
+void vDSPVadWakeupInit(void)
+{
+	int ret;
+
+	ret = xInstallRemoteMessageCallbackFeedBack(AODSPA_CHANNEL,
+						    MBX_CMD_VAD_AWE_WAKEUP,
+						    xMboxVadWakeup,
+						    1);
+	if (ret == MBOX_CALL_MAX)
+		printf("mbox cmd 0x%x register fail\n", MBX_CMD_VAD_AWE_WAKEUP);
+}
+
+void vDSPVadWakeupDeinit(void)
+{
+	xUninstallRemoteMessageCallback(AODSPA_CHANNEL, MBX_CMD_VAD_AWE_WAKEUP);
+}
+

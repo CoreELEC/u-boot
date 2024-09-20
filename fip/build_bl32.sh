2da44508710ddc226949b4f6e069e68f8236395d
Pengguang Zhu <pengguang.zhu@amlogic.com>
2024-09-20 12:14:02 +0800

    fip: correct the build command for bl32 [1/1]
    PD#SWPL-177792
    
    Problem:
    the bl32 build command for s805x3 is incorrect
    
    Solution:
    correct the build command
    
    Verify:
    S7
    
    Change-Id: I9ec666d8a2ef01b9349741c196aa5f5d6824d6ef
    Signed-off-by: Pengguang Zhu <pengguang.zhu@amlogic.com>

--- fip/build_bl32.sh
+++ fip/build_bl32.sh
@@ -6,7 +6,18 @@ function build_bl32() {
 	local target2="$1/bl32.bin"
 	# $1: src_folder, $2: bin_folder, $3: soc
 	cd $1
-	/bin/bash build.sh $3 ${CONFIG_CAS}
+	if [ $CONFIG_CHIPSET_NAME == "s905w2" ]; then
+		eval 'CFG_NO_RPMB=y CFG_RSV_MEM_8M=y ./build.sh $3 ${CONFIG_CAS}'
+	elif [ $CONFIG_CHIPSET_NAME == "s805x2" ]; then
+		eval 'CFG_RSV_MEM_20M=y ./build.sh $3 ${CONFIG_CAS}'
+	elif [ $CONFIG_CHIPSET_NAME == "s805c1" || $CONFIG_CHIPSET_NAME == "s805c1eng" ]; then
+		eval 'CFG_NO_RPMB=y ./build.sh $3 ${CONFIG_CAS}'
+	elif [ $CONFIG_CHIPSET_NAME == "s805x3" ]; then
+		echo CONFIG_CHIPSET_NAME is $CONFIG_CHIPSET_NAME
+		eval 'CFG_RSV_MEM_16M_WITH_DYN_SHM=y ./build.sh $3 ${CONFIG_CAS}'
+	else
+		/bin/bash build.sh $3 ${CONFIG_CAS}
+	fi
 	if [ $? != 0 ]; then
 		cd ${MAIN_FOLDER}
 		echo "Error: Build bl32 failed... abort"

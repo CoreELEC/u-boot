de6339e9b57f68e4c052212e6499055fb6286dc5
bangzheng.liu <bangzheng.liu@amlogic.com>
2024-10-10 15:52:03 +0800

    SoC t6w: PXP Bringup Bootloader BL30 code portinig [1/3]
    PD#SWPL-188177
    
    Problem:
    T6W PXP Bringup Bootloader BL30.
    
    Solution:
    Code porting from t6d and ucode.
    
    Verify:
    Test in T6W PXP preload mode.
    
    Change-Id: I600717d15e9e742e1e9d628a1f0723fe64245965
    Signed-off-by: bangzheng.liu <bangzheng.liu@amlogic.com>

---
+++ bl30/rtos_sdk/soc/riscv/t6w/i2c_plat.h
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2021-2022 Amlogic, Inc. All rights reserved.
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+/*
+ * platform related header file
+ */
+#ifndef _MESON_I2C_PLAT_H_
+#define _MESON_I2C_PLAT_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+#include "FreeRTOS.h"
+#include <common.h>
+#include <meson_i2c.h>
+
+#define CLKCTRL_SYS_CLK_EN0_REG0                   ((0x0011  << 2) + 0xfe000000)
+#define CLKCTRL_SYS_CLK_EN0_REG1                   ((0x0012  << 2) + 0xfe000000)
+#define CLKCTRL_SYS_CLK_EN0_REG2                   ((0x0013  << 2) + 0xfe000000)
+#define CLKCTRL_SYS_CLK_EN0_REG3                   ((0x0014  << 2) + 0xfe000000)
+
+struct xMesonI2cPlatdata i2c_plat_data[] = {
+	{ 0, 0xfe066000, 3, 15, 100000, MESON_I2C_CLK_RATE,
+						CLKCTRL_SYS_CLK_EN0_REG1, 30 }, /* i2c A */
+	{ 1, 0xfe068000, 3, 15, 100000, MESON_I2C_CLK_RATE,
+						CLKCTRL_SYS_CLK_EN0_REG1, 31 }, /* i2c B */
+	{ 2, 0xfe06a000, 3, 15, 100000, MESON_I2C_CLK_RATE,
+						CLKCTRL_SYS_CLK_EN0_REG2, 0 }, /* i2c C */
+	{ 3, 0xfe06c000, 3, 15, 100000, MESON_I2C_CLK_RATE,
+						CLKCTRL_SYS_CLK_EN0_REG2, 1 }, /* i2c D */
+	{ 4, 0xfe06e000, 3, 15, 100000, MESON_I2C_CLK_RATE,
+						CLKCTRL_SYS_CLK_EN0_REG2, 2 }, /* i2c E */
+};
+
+#ifdef __cplusplus
+}
+#endif
+#endif /* _MESON_I2C_PLAT_H_ */

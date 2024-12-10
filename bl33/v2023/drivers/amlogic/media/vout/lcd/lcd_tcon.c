103801a7c85539ca65dc7c2414c9cf90fc9314e5
huijie.huang <huijie.huang@amlogic.com>
2024-12-10 16:16:54 +0800

    lcd: tcon reduce memory usage [1/2]
    PD#SWPL-195903
    
    Problem:
    Need to reduce tcon memory usage
    
    Solution:
    Tcon reduce memory usage
    
    Verify:
    T6D
    
    Change-Id: Iacd4b9a440982440cb443a6a8ba9422c61d052f3
    Signed-off-by: huijie.huang <huijie.huang@amlogic.com>

--- bl33/v2023/drivers/amlogic/media/vout/lcd/lcd_tcon.c
+++ bl33/v2023/drivers/amlogic/media/vout/lcd/lcd_tcon.c
@@ -1672,6 +1672,11 @@ static struct lcd_tcon_axi_mem_cfg_s axi_mem_cfg_tbl_txhd2[] = {
 	{ TCON_AXI_MEM_TYPE_DEMURA, 0x00100000, 0x19b, 0 },  /* 1M */
 };
 
+static struct lcd_tcon_axi_mem_cfg_s axi_mem_cfg_tbl_t6d[] = {
+	{ TCON_AXI_MEM_TYPE_OD,     0x00200000, 0x261, 0 },  /* 2M */
+	{ TCON_AXI_MEM_TYPE_DEMURA, 0x00100000, 0x19b, 0 },  /* 1M */
+};
+
 static struct lcd_tcon_dma_ops_s lcd_tcon_dma_ops_t5m = {
 	.status = 0,
 	.addr_list = NULL,
@@ -1858,8 +1863,8 @@ static struct lcd_tcon_config_s tcon_data_t6d = {
 
 	.axi_bank = LCD_TCON_AXI_BANK_T6D,
 
-	.rsv_mem_size    = 0x00502840,
-	.axi_size        = 0x00500000, /* 5M*/
+	.rsv_mem_size    = 0x00302840,
+	.axi_size        = 0x00300000, /* 3M*/
 	.bin_path_size   = 0x00002800, /* 10K */
 	.secure_cfg_size = 0x00000040, /* 64byte */
 	.vac_size        = 0,
@@ -1867,8 +1872,8 @@ static struct lcd_tcon_config_s tcon_data_t6d = {
 	.demura_lut_size = 0,
 	.acc_lut_size    = 0,
 
-	.axi_tbl_len = ARRAY_SIZE(axi_mem_cfg_tbl_txhd2),
-	.axi_mem_cfg_tbl = axi_mem_cfg_tbl_txhd2,
+	.axi_tbl_len = ARRAY_SIZE(axi_mem_cfg_tbl_t6d),
+	.axi_mem_cfg_tbl = axi_mem_cfg_tbl_t6d,
 	.lut_dma_ops = &lcd_tcon_dma_ops_t6d,
 
 	.tcon_init_table_pre_proc = lcd_tcon_init_table_pre_proc,

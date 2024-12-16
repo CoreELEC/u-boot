ee9a4f8a2ec30b91df8bc54f051eb95b96ca2a48
Hangyu Li <hangyu.li@amlogic.com>
2024-12-16 18:18:51 +0800

    fip: Optimize BL32 AMFC compression flag [1/1]
    PD#SWPL-184752
    
    Problem:
    Optimize AMFC compression flag
    
    Solution:
    T6D remains the same as before, other chips optimize AMFC compression flag.
    
    Verify:
    t6d
    
    Change-Id: I4be627b6bdd60415ac655195e161cafc16c14662
    Signed-off-by: Hangyu Li <hangyu.li@amlogic.com>

--- fip/amfc_compress.sh
+++ fip/amfc_compress.sh
@@ -22,7 +22,11 @@ mv -f $INPUT $COMPRESS_ORG
 ${TOOLBIN} ${COMPRESS_ORG} -9 -o ${COMPRESS_ZSTD}
 bin_org_size=`stat -c %s ${COMPRESS_ORG}`
 bin_zstd_size=`stat -c %s ${COMPRESS_ZSTD}`
-printf "%s" "@ZSTD" >  ${AMFC_ZSTD_HDR}
+if [ "$SOC" == "t6d" ]; then
+    printf "%s" "@ZSTD" > "${AMFC_ZSTD_HDR}"
+else
+    printf "%s" "ZSTD" > "${AMFC_ZSTD_HDR}"
+fi
 
 printf "%02x%02x%02x%02x" $[(bin_org_size) & 0xff] \
 $[((bin_org_size) >> 8) & 0xff] $[((bin_org_size) >> 16) & 0xff] \
